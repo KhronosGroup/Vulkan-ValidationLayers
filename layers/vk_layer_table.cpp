56661efba87006829f42cbd406960ae0279e27ac
Mark Lobodzinski <mark@lunarg.com>
2018-06-25 13:32:32 -0600

    layers: Remove unused functions in vk_layer_table
    The deleted source supplied old-style dispatch-table handling
    support. All dispatch table handling has been moved the the new-
    style support, making this unnecessary.

--- layers/vk_layer_table.cpp
+++ layers/vk_layer_table.cpp
@@ -22,57 +22,7 @@
 #include "vk_dispatch_table_helper.h"
 #include "vulkan/vk_layer.h"
 #include "vk_layer_table.h"
-static device_table_map tableMap;
-static instance_table_map tableInstanceMap;
 
-// Map lookup must be thread safe
-VkLayerDispatchTable *device_dispatch_table(void *object) {
-    dispatch_key key = get_dispatch_key(object);
-    device_table_map::const_iterator it = tableMap.find((void *)key);
-    assert(it != tableMap.end() && "Not able to find device dispatch entry");
-    return it->second;
-}
-
-VkLayerInstanceDispatchTable *instance_dispatch_table(void *object) {
-    dispatch_key key = get_dispatch_key(object);
-    instance_table_map::const_iterator it = tableInstanceMap.find((void *)key);
-    assert(it != tableInstanceMap.end() && "Not able to find instance dispatch entry");
-    return it->second;
-}
-
-void destroy_dispatch_table(device_table_map &map, dispatch_key key) {
-    device_table_map::const_iterator it = map.find((void *)key);
-    if (it != map.end()) {
-        delete it->second;
-        map.erase(it);
-    }
-}
-
-void destroy_dispatch_table(instance_table_map &map, dispatch_key key) {
-    instance_table_map::const_iterator it = map.find((void *)key);
-    if (it != map.end()) {
-        delete it->second;
-        map.erase(it);
-    }
-}
-
-void destroy_device_dispatch_table(dispatch_key key) { destroy_dispatch_table(tableMap, key); }
-
-void destroy_instance_dispatch_table(dispatch_key key) { destroy_dispatch_table(tableInstanceMap, key); }
-
-VkLayerDispatchTable *get_dispatch_table(device_table_map &map, void *object) {
-    dispatch_key key = get_dispatch_key(object);
-    device_table_map::const_iterator it = map.find((void *)key);
-    assert(it != map.end() && "Not able to find device dispatch entry");
-    return it->second;
-}
-
-VkLayerInstanceDispatchTable *get_dispatch_table(instance_table_map &map, void *object) {
-    dispatch_key key = get_dispatch_key(object);
-    instance_table_map::const_iterator it = map.find((void *)key);
-    assert(it != map.end() && "Not able to find instance dispatch entry");
-    return it->second;
-}
 
 VkLayerInstanceCreateInfo *get_chain_info(const VkInstanceCreateInfo *pCreateInfo, VkLayerFunction func) {
     VkLayerInstanceCreateInfo *chain_info = (VkLayerInstanceCreateInfo *)pCreateInfo->pNext;
@@ -90,57 +40,4 @@ VkLayerDeviceCreateInfo *get_chain_info(const VkDeviceCreateInfo *pCreateInfo, V
     }
     assert(chain_info != NULL);
     return chain_info;
-}
-
-/* Various dispatchable objects will use the same underlying dispatch table if they
- * are created from that "parent" object. Thus use pointer to dispatch table
- * as the key to these table maps.
- *    Instance -> PhysicalDevice
- *    Device -> CommandBuffer or Queue
- * If use the object themselves as key to map then implies Create entrypoints have to be intercepted
- * and a new key inserted into map */
-VkLayerInstanceDispatchTable *initInstanceTable(VkInstance instance, const PFN_vkGetInstanceProcAddr gpa, instance_table_map &map) {
-    VkLayerInstanceDispatchTable *pTable;
-    dispatch_key key = get_dispatch_key(instance);
-    instance_table_map::const_iterator it = map.find((void *)key);
-
-    if (it == map.end()) {
-        pTable = new VkLayerInstanceDispatchTable;
-        map[(void *)key] = pTable;
-    } else {
-        return it->second;
-    }
-
-    layer_init_instance_dispatch_table(instance, pTable, gpa);
-
-    // Setup func pointers that are required but not externally exposed.  These won't be added to the instance dispatch table by
-    // default.
-    pTable->GetPhysicalDeviceProcAddr = (PFN_GetPhysicalDeviceProcAddr)gpa(instance, "vk_layerGetPhysicalDeviceProcAddr");
-
-    return pTable;
-}
-
-VkLayerInstanceDispatchTable *initInstanceTable(VkInstance instance, const PFN_vkGetInstanceProcAddr gpa) {
-    return initInstanceTable(instance, gpa, tableInstanceMap);
-}
-
-VkLayerDispatchTable *initDeviceTable(VkDevice device, const PFN_vkGetDeviceProcAddr gpa, device_table_map &map) {
-    VkLayerDispatchTable *pTable;
-    dispatch_key key = get_dispatch_key(device);
-    device_table_map::const_iterator it = map.find((void *)key);
-
-    if (it == map.end()) {
-        pTable = new VkLayerDispatchTable;
-        map[(void *)key] = pTable;
-    } else {
-        return it->second;
-    }
-
-    layer_init_device_dispatch_table(device, pTable, gpa);
-
-    return pTable;
-}
-
-VkLayerDispatchTable *initDeviceTable(VkDevice device, const PFN_vkGetDeviceProcAddr gpa) {
-    return initDeviceTable(device, gpa, tableMap);
 }
