f42ced6a4c310b6d3fa3f6ecf540e3bae088ed14
Mark Lobodzinski <mark@lunarg.com>
2018-07-02 12:59:27 -0600

    layers: Verify dev extension APIs enabled for GDPA
    Object Tracker must return NULL for any device API whose parent
    extension has not been enabled during CreateDevice.

--- layers/object_tracker_utils.cpp
+++ layers/object_tracker_utils.cpp
@@ -845,6 +845,11 @@ VKAPI_ATTR VkResult VKAPI_CALL CreateDevice(VkPhysicalDevice physicalDevice, con
     device_data->report_data = layer_debug_utils_create_device(phy_dev_data->report_data, *pDevice);
     layer_init_device_dispatch_table(*pDevice, &device_data->device_dispatch_table, fpGetDeviceProcAddr);
 
+    // Save pCreateInfo device extension list for GetDeviceProcAddr()
+    for (uint32_t extn = 0; extn < pCreateInfo->enabledExtensionCount; extn++) {
+        device_data->device_extension_set.insert(pCreateInfo->ppEnabledExtensionNames[extn]);
+    }
+
     // Add link back to physDev
     device_data->physical_device = physicalDevice;
     device_data->instance = phy_dev_data->instance;
@@ -1392,11 +1397,14 @@ VKAPI_ATTR PFN_vkVoidFunction VKAPI_CALL GetPhysicalDeviceProcAddr(VkInstance in
 }
 
 VKAPI_ATTR PFN_vkVoidFunction VKAPI_CALL GetDeviceProcAddr(VkDevice device, const char *funcName) {
+    layer_data *device_data = GetLayerDataPtr(get_dispatch_key(device), layer_data_map);
+    if (!ApiParentExtensionEnabled(funcName, device_data->device_extension_set)) {
+        return nullptr;
+    }
     const auto item = name_to_funcptr_map.find(funcName);
     if (item != name_to_funcptr_map.end()) {
         return reinterpret_cast<PFN_vkVoidFunction>(item->second);
     }
-    layer_data *device_data = GetLayerDataPtr(get_dispatch_key(device), layer_data_map);
     if (!device_data->device_dispatch_table.GetDeviceProcAddr) return NULL;
     return device_data->device_dispatch_table.GetDeviceProcAddr(device, funcName);
 }
