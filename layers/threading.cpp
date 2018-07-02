c2e7a5cff52e47ac140dc28e79871b922aa82812
Mark Lobodzinski <mark@lunarg.com>
2018-07-02 13:15:17 -0600

    layers: Verify dev extension APIs enabled for GDPA
    Threading layer must return NULL for any device API whose parent
    extension has not been enabled during CreateDevice.

--- layers/threading.cpp
+++ layers/threading.cpp
@@ -170,6 +170,10 @@ VKAPI_ATTR VkResult VKAPI_CALL CreateDevice(VkPhysicalDevice gpu, const VkDevice
     // Setup device dispatch table
     my_device_data->device_dispatch_table = new VkLayerDispatchTable;
     layer_init_device_dispatch_table(*pDevice, my_device_data->device_dispatch_table, fpGetDeviceProcAddr);
+    // Save pCreateInfo device extension list for GetDeviceProcAddr()
+    for (uint32_t extn = 0; extn < pCreateInfo->enabledExtensionCount; extn++) {
+        my_device_data->device_extension_set.insert(pCreateInfo->ppEnabledExtensionNames[extn]);
+    }
 
     my_device_data->report_data = layer_debug_utils_create_device(my_instance_data->report_data, *pDevice);
     return result;
@@ -258,12 +262,14 @@ VKAPI_ATTR VkResult VKAPI_CALL EnumerateDeviceExtensionProperties(VkPhysicalDevi
 VKAPI_ATTR PFN_vkVoidFunction VKAPI_CALL GetPhysicalDeviceProcAddr(VkInstance instance, const char *funcName);
 
 VKAPI_ATTR PFN_vkVoidFunction VKAPI_CALL GetDeviceProcAddr(VkDevice device, const char *funcName) {
+    layer_data *device_data = GetLayerDataPtr(get_dispatch_key(device), layer_data_map);
+    if (!ApiParentExtensionEnabled(funcName, device_data->device_extension_set)) {
+        return nullptr;
+    }
     const auto item = name_to_funcptr_map.find(funcName);
     if (item != name_to_funcptr_map.end()) {
         return reinterpret_cast<PFN_vkVoidFunction>(item->second);
     }
-
-    layer_data *device_data = GetLayerDataPtr(get_dispatch_key(device), layer_data_map);
     auto &table = device_data->device_dispatch_table;
     if (!table->GetDeviceProcAddr) return nullptr;
     return table->GetDeviceProcAddr(device, funcName);
