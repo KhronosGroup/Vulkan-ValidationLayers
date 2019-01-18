938c1b25345dd1731872afdf3d0bffd6ba295386
Jasper St. Pierre <jstpierre@nvidia.com>
2019-01-18 13:55:20 -0800

    vk_validation_stats: Update Copyright year
    The header file was updated, but this script was not.

--- scripts/vk_validation_stats.py
+++ scripts/vk_validation_stats.py
@@ -479,8 +479,8 @@ class OutputDatabase:
 /*
  * Vulkan
  *
- * Copyright (c) 2016-2018 Google Inc.
- * Copyright (c) 2016-2018 LunarG, Inc.
+ * Copyright (c) 2016-2019 Google Inc.
+ * Copyright (c) 2016-2019 LunarG, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
