e6fe3d141c3cedd017dd4043dca38b4e7e98f08d
Locke <locke@lunarg.com>
2019-03-04 16:10:45 -0700

    tests: Fixed NumSamplesMismatch unexpected
    Change-Id: Ic4467533697c5d564dd882c82fac238b0d34fea0

--- tests/layer_validation_tests.cpp
+++ tests/layer_validation_tests.cpp
@@ -16885,6 +16885,8 @@ TEST_F(VkLayerTest, NumSamplesMismatch) {
     pipe.AddShader(&fs);
     pipe.AddDefaultColorAttachment();
     pipe.SetMSAA(&pipe_ms_state_ci);
+
+    m_errorMonitor->SetUnexpectedError("VUID-VkGraphicsPipelineCreateInfo-subpass-00757");
     pipe.CreateVKPipeline(pipeline_layout.handle(), renderPass());
 
     m_commandBuffer->begin();
