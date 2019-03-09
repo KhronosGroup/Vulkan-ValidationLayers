349d495d5a8603639fbf05408ee14a85597a8021
Mark Lobodzinski <mark@lunarg.com>
2019-03-08 18:19:48 -0700

    layers: Remove layer_data refs from GpuVal
    Change-Id: I66f74a40f7390197b9c86b4c0cfeb1daf3f1d28a

--- layers/core_validation.h
+++ layers/core_validation.h
@@ -617,34 +617,30 @@ class CoreChecks : public ValidationObject {
                                       VkPhysicalDeviceFeatures* supported_features);
     void GpuPostCallRecordCreateDevice();
     void GpuPreCallRecordDestroyDevice();
-    void GpuPreCallRecordFreeCommandBuffers(layer_data* dev_data, uint32_t commandBufferCount,
-                                            const VkCommandBuffer* pCommandBuffers);
-    bool GpuPreCallCreateShaderModule(layer_data* dev_data, const VkShaderModuleCreateInfo* pCreateInfo,
-                                      const VkAllocationCallbacks* pAllocator, VkShaderModule* pShaderModule,
-                                      uint32_t* unique_shader_id, VkShaderModuleCreateInfo* instrumented_create_info,
+    void GpuPreCallRecordFreeCommandBuffers(uint32_t commandBufferCount, const VkCommandBuffer* pCommandBuffers);
+    bool GpuPreCallCreateShaderModule(const VkShaderModuleCreateInfo* pCreateInfo, const VkAllocationCallbacks* pAllocator,
+                                      VkShaderModule* pShaderModule, uint32_t* unique_shader_id,
+                                      VkShaderModuleCreateInfo* instrumented_create_info,
                                       std::vector<unsigned int>* instrumented_pgm);
-    bool GpuPreCallCreatePipelineLayout(layer_data* device_data, const VkPipelineLayoutCreateInfo* pCreateInfo,
-                                        const VkAllocationCallbacks* pAllocator, VkPipelineLayout* pPipelineLayout,
-                                        std::vector<VkDescriptorSetLayout>* new_layouts,
+    bool GpuPreCallCreatePipelineLayout(const VkPipelineLayoutCreateInfo* pCreateInfo, const VkAllocationCallbacks* pAllocator,
+                                        VkPipelineLayout* pPipelineLayout, std::vector<VkDescriptorSetLayout>* new_layouts,
                                         VkPipelineLayoutCreateInfo* modified_create_info);
-    void GpuPostCallCreatePipelineLayout(layer_data* device_data, VkResult result);
+    void GpuPostCallCreatePipelineLayout(VkResult result);
     void GpuPostCallQueueSubmit(VkQueue queue, uint32_t submitCount, const VkSubmitInfo* pSubmits, VkFence fence);
-    void GpuPreCallValidateCmdWaitEvents(layer_data* dev_data, VkPipelineStageFlags sourceStageMask);
+    void GpuPreCallValidateCmdWaitEvents(VkPipelineStageFlags sourceStageMask);
     std::vector<safe_VkGraphicsPipelineCreateInfo> GpuPreCallRecordCreateGraphicsPipelines(
-        layer_data* dev_data, VkPipelineCache pipelineCache, uint32_t count, const VkGraphicsPipelineCreateInfo* pCreateInfos,
+        VkPipelineCache pipelineCache, uint32_t count, const VkGraphicsPipelineCreateInfo* pCreateInfos,
         const VkAllocationCallbacks* pAllocator, VkPipeline* pPipelines, std::vector<std::unique_ptr<PIPELINE_STATE>>& pipe_state);
-    void GpuPostCallRecordCreateGraphicsPipelines(layer_data* dev_data, const uint32_t count,
-                                                  const VkGraphicsPipelineCreateInfo* pCreateInfos,
+    void GpuPostCallRecordCreateGraphicsPipelines(const uint32_t count, const VkGraphicsPipelineCreateInfo* pCreateInfos,
                                                   const VkAllocationCallbacks* pAllocator, VkPipeline* pPipelines);
-    void GpuPreCallRecordDestroyPipeline(layer_data* dev_data, const VkPipeline pipeline);
-    void GpuAllocateValidationResources(layer_data* dev_data, const VkCommandBuffer cmd_buffer, VkPipelineBindPoint bind_point);
+    void GpuPreCallRecordDestroyPipeline(const VkPipeline pipeline);
+    void GpuAllocateValidationResources(const VkCommandBuffer cmd_buffer, VkPipelineBindPoint bind_point);
     void AnalyzeAndReportError(GLOBAL_CB_NODE* cb_node, VkQueue queue, uint32_t draw_index, uint32_t* const debug_output_buffer);
     void ProcessInstrumentationBuffer(VkQueue queue, GLOBAL_CB_NODE* cb_node);
     void SubmitBarrier(VkQueue queue);
-    bool GpuInstrumentShader(layer_data* dev_data, const VkShaderModuleCreateInfo* pCreateInfo, std::vector<unsigned int>& new_pgm,
+    bool GpuInstrumentShader(const VkShaderModuleCreateInfo* pCreateInfo, std::vector<unsigned int>& new_pgm,
                              uint32_t* unique_shader_id);
-    void ReportSetupProblem(const layer_data* dev_data, VkDebugReportObjectTypeEXT object_type, uint64_t object_handle,
-                            const char* const specific_message);
+    void ReportSetupProblem(VkDebugReportObjectTypeEXT object_type, uint64_t object_handle, const char* const specific_message);
 
     // Buffer Validation Functions
     template <class OBJECT, class LAYOUT>
