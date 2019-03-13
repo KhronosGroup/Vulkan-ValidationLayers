10455f67faf1bdcf4c9d878a22f6fe2164cb5036
Mark Lobodzinski <mark@lunarg.com>
2019-03-13 15:03:35 -0600

    tests: Fail LVTs encountering unexpected errors
    Change-Id: I1e007f77799fcc5696917fb4069dda8e176b97a3

--- tests/layer_validation_tests.cpp
+++ tests/layer_validation_tests.cpp
@@ -388,6 +388,16 @@ class ErrorMonitor {
             for (const auto desired_msg : desired_message_strings_) {
                 ADD_FAILURE() << "Did not receive expected error '" << desired_msg << "'";
             }
+        } else if (GetOtherFailureMsgs().size() > 0) {
+            // Fail test case for any unexpected errors
+#if defined(ANDROID) && defined(VALIDATION_APK)
+            // This will get unexpected errors into the adb log
+            for (auto msg : other_messages_) {
+                __android_log_print(ANDROID_LOG_INFO, "VulkanLayerValidationTests", "[ UNEXPECTED_ERR ] '%s'", msg.c_str());
+            }
+#else
+            ADD_FAILURE() << "Received unexpected error(s).";
+#endif
         }
         Reset();
     }
@@ -399,6 +409,16 @@ class ErrorMonitor {
             for (const auto msg : failure_message_strings_) {
                 ADD_FAILURE() << "Expected to succeed but got error: " << msg;
             }
+        } else if (GetOtherFailureMsgs().size() > 0) {
+            // Fail test case for any unexpected errors
+#if defined(ANDROID) && defined(VALIDATION_APK)
+            // This will get unexpected errors into the adb log
+            for (auto msg : other_messages_) {
+                __android_log_print(ANDROID_LOG_INFO, "VulkanLayerValidationTests", "[ UNEXPECTED_ERR ] '%s'", msg.c_str());
+            }
+#else
+            ADD_FAILURE() << "Received unexpected error(s).";
+#endif
         }
         Reset();
     }
