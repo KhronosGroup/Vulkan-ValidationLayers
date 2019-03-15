7f7d424ec2dda383e0a541aa692ad8cec01dcb0a
Mark Lobodzinski <mark@lunarg.com>
2019-03-15 14:26:31 -0600

    tests: Skip BufferDeviceAddressDisabled test
    The feature needed for the BufferDeviceAddressEXTDisabled test is
    not supported by the MockICD.
    
    Change-Id: I6c1101a78d21177dfbfd936ef0bcb27a634aaeaa

--- tests/layer_validation_tests.cpp
+++ tests/layer_validation_tests.cpp
@@ -37775,6 +37775,11 @@ TEST_F(VkLayerTest, BufferDeviceAddressEXTDisabled) {
         }
     }
 
+    if (DeviceIsMockICD() || DeviceSimulation()) {
+        printf("%s MockICD does not support this feature, skipping tests\n", kSkipPrefix);
+        return;
+    }
+
     PFN_vkGetPhysicalDeviceFeatures2KHR vkGetPhysicalDeviceFeatures2KHR =
         (PFN_vkGetPhysicalDeviceFeatures2KHR)vkGetInstanceProcAddr(instance(), "vkGetPhysicalDeviceFeatures2KHR");
     ASSERT_TRUE(vkGetPhysicalDeviceFeatures2KHR != nullptr);
