8f6d80e14f9c0de46af9e4e84ae0b24ede10989b
Mark Lobodzinski <mark@lunarg.com>
2019-03-18 12:46:31 -0600

    tests: Fix FBMixedSamplesNV test
    Needed to check for sampleRateShading feature.
    
    Change-Id: I44a9af6c60b2ec80efef228e49f492cc64455fa8

--- tests/layer_validation_tests.cpp
+++ tests/layer_validation_tests.cpp
@@ -36098,6 +36098,13 @@ TEST_F(VkLayerTest, FramebufferMixedSamplesNV) {
         return;
     }
 
+    VkPhysicalDeviceFeatures device_features = {};
+    ASSERT_NO_FATAL_FAILURE(GetPhysicalDeviceFeatures(&device_features));
+    if (VK_TRUE != device_features.sampleRateShading) {
+        printf("%s Test requires unsupported sampleRateShading feature.\n", kSkipPrefix);
+        return;
+    }
+
     ASSERT_NO_FATAL_FAILURE(InitState());
     ASSERT_NO_FATAL_FAILURE(InitRenderTarget());
 
