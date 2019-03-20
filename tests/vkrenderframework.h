adb4f6cc9050e55b78ee304c68943f5689afcdac
Locke <locke@lunarg.com>
2019-03-20 10:14:04 -0600

    tests: Initialize swapchain
    Change-Id: Ic1158f4cd4e537420e067f060bb284b79de9ea28

--- tests/vkrenderframework.h
+++ tests/vkrenderframework.h
@@ -89,6 +89,9 @@ class VkRenderFramework : public VkTestFramework {
     VkFramebuffer framebuffer() { return m_framebuffer; }
     void InitViewport(float width, float height);
     void InitViewport();
+    bool InitSwapchain(float width, float height);
+    bool InitSwapchain();
+    void DestroySwapchain();
     void InitRenderTarget();
     void InitRenderTarget(uint32_t targets);
     void InitRenderTarget(VkImageView *dsBinding);
@@ -125,6 +128,8 @@ class VkRenderFramework : public VkTestFramework {
     VkRenderPass m_renderPass;
     VkRenderPassCreateInfo renderPass_info_ = {};
     VkFramebuffer m_framebuffer;
+    VkSurfaceKHR m_surface;
+    VkSwapchainKHR m_swapchain;
     std::vector<VkViewport> m_viewports;
     std::vector<VkRect2D> m_scissors;
     float m_lineWidth;
