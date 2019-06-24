54a67c4eebd73deb3a7ef8987acca3372c7dfc64
Mark Lobodzinski <mark@lunarg.com>
2019-06-24 14:35:21 -0600

    corechecks: Print usage in hex instead of decimal
    A couple of error messages in desc_sets.cpp were outputting USAGE
    flags as decimal, though the message made it appear as if they were
    hex.
    
    Change-Id: Id5933e166a254c1a37df0f7c1f49b99360f9954c

--- layers/descriptor_sets.cpp
+++ layers/descriptor_sets.cpp
@@ -1474,9 +1474,9 @@ bool CoreChecks::ValidateImageUpdate(VkImageView image_view, VkImageLayout image
     }
     if (error_usage_bit) {
         std::stringstream error_str;
-        error_str << "ImageView (" << report_data->FormatHandle(image_view).c_str() << ") with usage mask 0x" << usage
-                  << " being used for a descriptor update of type " << string_VkDescriptorType(type) << " does not have "
-                  << error_usage_bit << " set.";
+        error_str << "ImageView (" << report_data->FormatHandle(image_view).c_str() << ") with usage mask " << std::hex
+                  << std::showbase << usage << " being used for a descriptor update of type " << string_VkDescriptorType(type)
+                  << " does not have " << error_usage_bit << " set.";
         *error_msg = error_str.str();
         return false;
     }
@@ -1924,7 +1924,7 @@ bool cvdescriptorset::ValidateBufferUsage(BUFFER_STATE const *buffer_node, VkDes
     }
     if (error_usage_bit) {
         std::stringstream error_str;
-        error_str << "Buffer (" << buffer_node->buffer << ") with usage mask 0x" << usage
+        error_str << "Buffer (" << buffer_node->buffer << ") with usage mask " << std::hex << std::showbase << usage
                   << " being used for a descriptor update of type " << string_VkDescriptorType(type) << " does not have "
                   << error_usage_bit << " set.";
         *error_msg = error_str.str();
