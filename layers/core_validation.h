86bcb3a3858043efa537be84a1ce70b76ce92952
unknown <locke@lunarg.com>
2019-07-11 13:05:36 -0600

    layers: Run setLayout in aliasing images
    Change-Id: I1054304cc51af73b66b275f07787118c17efd74c

--- layers/core_validation.h
+++ layers/core_validation.h
@@ -1487,6 +1487,9 @@ class CoreChecks : public ValidationStateTracker {
 
     void TransitionImageLayouts(CMD_BUFFER_STATE* cb_state, uint32_t memBarrierCount, const VkImageMemoryBarrier* pImgMemBarriers);
 
+    void RecordTransitionImageLayout(CMD_BUFFER_STATE* cb_state, const IMAGE_STATE* image_state,
+                                     const VkImageMemoryBarrier& mem_barrier);
+
     void TransitionFinalSubpassLayouts(CMD_BUFFER_STATE* pCB, const VkRenderPassBeginInfo* pRenderPassBegin,
                                        FRAMEBUFFER_STATE* framebuffer_state);
 
