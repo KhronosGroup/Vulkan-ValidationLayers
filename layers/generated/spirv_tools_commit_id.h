865471c9b0f57d8d5afef12b7a4c1188f838fff9
Mike Schuchardt <mikes@lunarg.com>
2019-07-11 08:11:47 -0700

    scripts: Update copyrights
    Change-Id: I7dbf24883b90bd54425c6b64592130d8f3b80012

--- layers/generated/spirv_tools_commit_id.h
+++ layers/generated/spirv_tools_commit_id.h
@@ -3,10 +3,10 @@
 
 /***************************************************************************
  *
- * Copyright (c) 2015-2017 The Khronos Group Inc.
- * Copyright (c) 2015-2017 Valve Corporation
- * Copyright (c) 2015-2017 LunarG, Inc.
- * Copyright (c) 2015-2017 Google Inc.
+ * Copyright (c) 2015-2019 The Khronos Group Inc.
+ * Copyright (c) 2015-2019 Valve Corporation
+ * Copyright (c) 2015-2019 LunarG, Inc.
+ * Copyright (c) 2015-2019 Google Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
