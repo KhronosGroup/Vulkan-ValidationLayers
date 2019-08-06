a3e6e64f60aa54e153b2b66e0ff3bb82ba02fd52
Jason Macnak <jmacnak@nvidia.com>
2019-08-06 14:46:08 -0700

    layers: Fix AS scratch buffer memory check
    I mistakenly thought that scratch_buffer_state->binding.size was the
    size of the underlying VkDeviceMemory allocation and that the offset
    within the underlying memory needed to be calculated.

--- layers/core_validation.cpp
+++ layers/core_validation.cpp
@@ -7141,7 +7141,7 @@ bool CoreChecks::PreCallValidateCmdBuildAccelerationStructureNV(VkCommandBuffer 
         }
         if (scratch_buffer_state != nullptr && dst_as_state != nullptr &&
             dst_as_state->update_scratch_memory_requirements.memoryRequirements.size >
-                (scratch_buffer_state->binding.size - (scratch_buffer_state->binding.offset + scratchOffset))) {
+                (scratch_buffer_state->binding.size - scratchOffset)) {
             skip |= log_msg(report_data, VK_DEBUG_REPORT_ERROR_BIT_EXT, VK_DEBUG_REPORT_OBJECT_TYPE_COMMAND_BUFFER_EXT,
                             HandleToUint64(commandBuffer), "VUID-vkCmdBuildAccelerationStructureNV-update-02492",
                             "vkCmdBuildAccelerationStructureNV(): If update is VK_TRUE, The size member of the "
@@ -7163,7 +7163,7 @@ bool CoreChecks::PreCallValidateCmdBuildAccelerationStructureNV(VkCommandBuffer 
         }
         if (scratch_buffer_state != nullptr && dst_as_state != nullptr &&
             dst_as_state->build_scratch_memory_requirements.memoryRequirements.size >
-                (scratch_buffer_state->binding.size - (scratch_buffer_state->binding.offset + scratchOffset))) {
+                (scratch_buffer_state->binding.size - scratchOffset)) {
             skip |= log_msg(report_data, VK_DEBUG_REPORT_ERROR_BIT_EXT, VK_DEBUG_REPORT_OBJECT_TYPE_COMMAND_BUFFER_EXT,
                             HandleToUint64(commandBuffer), "VUID-vkCmdBuildAccelerationStructureNV-update-02491",
                             "vkCmdBuildAccelerationStructureNV(): If update is VK_FALSE, The size member of the "
