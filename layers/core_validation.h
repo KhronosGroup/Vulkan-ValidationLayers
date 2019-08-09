e1efc5279e618890d30c6276508ef95fecdfb1c6
ByumjinConffx <byumjin@conffx.com>
2019-08-09 10:06:22 -0700

    layers: Add VkFramebufferCreateInfo-pAttachments val
    Adds VkFramebufferCreateInfo-pAttachments validation codes for FragmentDensityMap.
    VkPhysicalDeviceFragmentDensityMapPropertiesEXT member value is added to
    DeviceExtensionProperties to get maxFragmentDensityTexelSize values.
    Also, math library is needed for ceil function.

--- layers/core_validation.h
+++ layers/core_validation.h
@@ -789,6 +789,7 @@ class ValidationStateTracker : public ValidationObject {
         VkPhysicalDeviceSubgroupProperties subgroup_props;
         VkPhysicalDeviceRayTracingPropertiesNV ray_tracing_props;
         VkPhysicalDeviceTexelBufferAlignmentPropertiesEXT texel_buffer_alignment_props;
+        VkPhysicalDeviceFragmentDensityMapPropertiesEXT fragment_density_map_props;
     };
     DeviceExtensionProperties phys_dev_ext_props = {};
     std::vector<VkCooperativeMatrixPropertiesNV> cooperative_matrix_properties;
