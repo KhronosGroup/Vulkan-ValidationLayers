72b1a22ec538b0e958dae519f61abf69f9e0752b
Mark Lobodzinski <mark@lunarg.com>
2020-03-18 11:36:10 -0600

    bufferval: Fix signed/unsigned warnings in clang
    Change-Id: Idb9be02bcb80aceeced9af76f48e942bddf91c10

--- layers/buffer_validation.cpp
+++ layers/buffer_validation.cpp
@@ -4909,7 +4909,8 @@ bool CoreChecks::ValidateBufferImageCopyData(uint32_t regionCount, const VkBuffe
         VkExtent3D adjusted_image_extent = GetImageSubresourceExtent(image_state, &pRegions[i].imageSubresource);
         // imageOffset.x and (imageExtent.width + imageOffset.x) must both be >= 0 and <= image subresource width
         if ((pRegions[i].imageOffset.x < 0) || (pRegions[i].imageOffset.x > static_cast<int32_t>(adjusted_image_extent.width)) ||
-            ((pRegions[i].imageOffset.x + pRegions[i].imageExtent.width) > static_cast<int32_t>(adjusted_image_extent.width))) {
+            ((pRegions[i].imageOffset.x + static_cast<int32_t>(pRegions[i].imageExtent.width)) >
+             static_cast<int32_t>(adjusted_image_extent.width))) {
             skip |= LogError(image_state->image, "VUID-VkBufferImageCopy-imageOffset-00197",
                              "%s(): Both pRegion[%d] imageoffset.x (%d) and (imageExtent.width + imageOffset.x) (%d) must be >= "
                              "zero or <= image subresource width (%d).",
@@ -4919,7 +4920,8 @@ bool CoreChecks::ValidateBufferImageCopyData(uint32_t regionCount, const VkBuffe
 
         // imageOffset.y and (imageExtent.height + imageOffset.y) must both be >= 0 and <= image subresource height
         if ((pRegions[i].imageOffset.y < 0) || (pRegions[i].imageOffset.y > static_cast<int32_t>(adjusted_image_extent.height)) ||
-            ((pRegions[i].imageOffset.y + pRegions[i].imageExtent.height) > static_cast<int32_t>(adjusted_image_extent.height))) {
+            ((pRegions[i].imageOffset.y + static_cast<int32_t>(pRegions[i].imageExtent.height)) >
+             static_cast<int32_t>(adjusted_image_extent.height))) {
             skip |= LogError(image_state->image, "VUID-VkBufferImageCopy-imageOffset-00198",
                              "%s(): Both pRegion[%d] imageoffset.y (%d) and (imageExtent.height + imageOffset.y) (%d) must be >= "
                              "zero or <= image subresource height (%d).",
@@ -4929,7 +4931,8 @@ bool CoreChecks::ValidateBufferImageCopyData(uint32_t regionCount, const VkBuffe
 
         // imageOffset.z and (imageExtent.depth + imageOffset.z) must both be >= 0 and <= image subresource depth
         if ((pRegions[i].imageOffset.z < 0) || (pRegions[i].imageOffset.z > static_cast<int32_t>(adjusted_image_extent.depth)) ||
-            ((pRegions[i].imageOffset.z + pRegions[i].imageExtent.depth) > static_cast<int32_t>(adjusted_image_extent.depth))) {
+            ((pRegions[i].imageOffset.z + static_cast<int32_t>(pRegions[i].imageExtent.depth)) >
+             static_cast<int32_t>(adjusted_image_extent.depth))) {
             skip |= LogError(image_state->image, "VUID-VkBufferImageCopy-imageOffset-00200",
                              "%s(): Both pRegion[%d] imageoffset.z (%d) and (imageExtent.depth + imageOffset.z) (%d) must be >= "
                              "zero or <= image subresource depth (%d).",
