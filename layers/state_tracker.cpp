bf8805f0342c3ee7c8e15c9c07b4777b5047ce28
John Zulauf <jzulauf@lunarg.com>
2020-03-20 10:34:33 -0600

    layers: Add subpass transition info to RP state
    Add the list of attachment image layout transitions for each subpass of
    a renderpass to the RENDER_PASS_STATE structure.
    
    Change-Id: I8f4bdfe4c0b199ca92564a4846652353d37fb3a7

--- layers/state_tracker.cpp
+++ layers/state_tracker.cpp
@@ -4057,18 +4057,29 @@ void ValidationStateTracker::RecordCreateRenderPassState(RenderPassCreateVersion
 
     RecordRenderPassDAG(RENDER_PASS_VERSION_1, create_info, render_pass.get());
 
-    struct AttachmentFirstLast {
+    struct AttachmentTracker {  // This is really only of local interest, but a bit big for a lambda
+        RENDER_PASS_STATE *const rp;
         std::vector<uint32_t> &first;
         std::vector<uint32_t> &last;
+        std::vector<std::vector<RENDER_PASS_STATE::AttachmentTransition>> &subpass_transitions;
         std::unordered_map<uint32_t, bool> &first_read;
         const uint32_t attachment_count;
-        AttachmentFirstLast(std::shared_ptr<RENDER_PASS_STATE> &render_pass)
-            : first(render_pass->attachment_first_subpass),
-              last(render_pass->attachment_last_subpass),
-              first_read(render_pass->attachment_first_read),
-              attachment_count(render_pass->createInfo.attachmentCount) {
+        std::vector<VkImageLayout> attachment_layout;
+        AttachmentTracker(std::shared_ptr<RENDER_PASS_STATE> &render_pass)
+            : rp(render_pass.get()),
+              first(rp->attachment_first_subpass),
+              last(rp->attachment_last_subpass),
+              subpass_transitions(rp->subpass_transitions),
+              first_read(rp->attachment_first_read),
+              attachment_count(rp->createInfo.attachmentCount),
+              attachment_layout() {
             first.resize(attachment_count, VK_SUBPASS_EXTERNAL);
             last.resize(attachment_count, VK_SUBPASS_EXTERNAL);
+            subpass_transitions.resize(rp->createInfo.subpassCount + 1);  // Add an extra for EndRenderPass
+            attachment_layout.reserve(attachment_count);
+            for (uint32_t j = 0; j < attachment_count; j++) {
+                attachment_layout.push_back(rp->createInfo.pAttachments[j].initialLayout);
+            }
         }
 
         void Update(uint32_t subpass, const VkAttachmentReference2 *attach_ref, uint32_t count, bool is_read) {
@@ -4076,27 +4087,45 @@ void ValidationStateTracker::RecordCreateRenderPassState(RenderPassCreateVersion
             for (uint32_t j = 0; j < count; ++j) {
                 const auto attachment = attach_ref[j].attachment;
                 if (attachment != VK_ATTACHMENT_UNUSED) {
+                    const auto layout = attach_ref[j].layout;
                     // Take advantage of the fact that insert won't overwrite, so we'll only write the first time.
                     first_read.insert(std::make_pair(attachment, is_read));
                     if (first[attachment] == VK_SUBPASS_EXTERNAL) first[attachment] = subpass;
                     last[attachment] = subpass;
+
+                    if (layout != attachment_layout[attachment]) {
+                        subpass_transitions[subpass].emplace_back(attachment, attachment_layout[attachment], layout);
+                        // TODO: Determine if this simple minded tracking is sufficient (it is for correct definitions)
+                        attachment_layout[attachment] = layout;
+                    }
+                }
+            }
+        }
+        void FinalTransitions() {
+            auto &final_transitions = subpass_transitions[rp->createInfo.subpassCount];
+
+            for (uint32_t attachment = 0; attachment < attachment_count; ++attachment) {
+                const auto final_layout = rp->createInfo.pAttachments[attachment].finalLayout;
+                if (final_layout != attachment_layout[attachment]) {
+                    final_transitions.emplace_back(attachment, attachment_layout[attachment], final_layout);
                 }
             }
         }
     };
-    AttachmentFirstLast first_last(render_pass);
+    AttachmentTracker attachment_tracker(render_pass);
 
     for (uint32_t subpass_index = 0; subpass_index < create_info->subpassCount; ++subpass_index) {
         const VkSubpassDescription2KHR &subpass = create_info->pSubpasses[subpass_index];
-        first_last.Update(subpass_index, subpass.pColorAttachments, subpass.colorAttachmentCount, false);
-        first_last.Update(subpass_index, subpass.pResolveAttachments, subpass.colorAttachmentCount, false);
-        first_last.Update(subpass_index, subpass.pDepthStencilAttachment, 1, false);
-        first_last.Update(subpass_index, subpass.pInputAttachments, subpass.inputAttachmentCount, true);
+        attachment_tracker.Update(subpass_index, subpass.pColorAttachments, subpass.colorAttachmentCount, false);
+        attachment_tracker.Update(subpass_index, subpass.pResolveAttachments, subpass.colorAttachmentCount, false);
+        attachment_tracker.Update(subpass_index, subpass.pDepthStencilAttachment, 1, false);
+        attachment_tracker.Update(subpass_index, subpass.pInputAttachments, subpass.inputAttachmentCount, true);
     }
+    attachment_tracker.FinalTransitions();
 
     // Add implicit dependencies
-    for (uint32_t attachment = 0; attachment < first_last.attachment_count; attachment++) {
-        const auto first_use = first_last.first[attachment];
+    for (uint32_t attachment = 0; attachment < attachment_tracker.attachment_count; attachment++) {
+        const auto first_use = attachment_tracker.first[attachment];
         if (first_use != VK_SUBPASS_EXTERNAL) {
             auto &subpass_dep = render_pass->subpass_dependencies[first_use];
             if (!subpass_dep.barrier_from_external) {
@@ -4107,7 +4136,7 @@ void ValidationStateTracker::RecordCreateRenderPassState(RenderPassCreateVersion
             }
         }
 
-        const auto last_use = first_last.last[attachment];
+        const auto last_use = attachment_tracker.last[attachment];
         if (last_use != VK_SUBPASS_EXTERNAL) {
             auto &subpass_dep = render_pass->subpass_dependencies[last_use];
             if (!render_pass->subpass_dependencies[last_use].barrier_to_external) {
