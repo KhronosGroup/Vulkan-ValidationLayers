719fcb969e3894d8d52c43e1236749d861899693
Mark Lobodzinski <mark@lunarg.com>
2020-06-08 14:24:10 -0600

    layers: Add private settings extension and write support
    Adds infrastructure for a private layer extension to query and set
    layer settings. Query functionality is defined but not implemented.
    
    Change-Id: Icde52684a5594f0c8dd61a11e5529bdb5930526b

--- scripts/layer_chassis_generator.py
+++ scripts/layer_chassis_generator.py
@@ -222,6 +222,7 @@ class LayerChassisOutputGenerator(OutputGenerator):
 
 #include "vk_loader_platform.h"
 #include "vulkan/vulkan.h"
+#include "vk_layer_settings_ext.h"
 #include "vk_layer_config.h"
 #include "vk_layer_data.h"
 #include "vk_layer_logging.h"
@@ -1020,6 +1021,20 @@ void ProcessConfigAndEnvSettings(const char* layer_description, CHECK_ENABLED &e
     CreateFilterMessageIdList(list_of_env_filter_ids, env_delimiter, message_filter_list);
 }
 
+const VkLayerSettingsEXT *FindSettingsInChain(const void *next) {
+    const VkBaseOutStructure *current = reinterpret_cast<const VkBaseOutStructure *>(next);
+    const VkLayerSettingsEXT *found = nullptr;
+    while (current) {
+        if (static_cast<VkStructureType>(VK_STRUCTURE_TYPE_INSTANCE_LAYER_SETTINGS_EXT) == current->sType) {
+            found = reinterpret_cast<const VkLayerSettingsEXT*>(current);
+            current = nullptr;
+        } else {
+            current = current->pNext;
+        }
+    }
+    return found;
+}
+
 void OutputLayerStatusInfo(ValidationObject *context) {
     std::string list_of_enables;
     std::string list_of_disables;
@@ -1174,6 +1189,22 @@ VKAPI_ATTR VkResult VKAPI_CALL CreateInstance(const VkInstanceCreateInfo *pCreat
     CHECK_ENABLED local_enables {};
     CHECK_DISABLED local_disables {};
 
+    const auto layer_settings_ext = FindSettingsInChain(pCreateInfo->pNext);
+    if (layer_settings_ext) {
+        for (uint32_t i = 0; i < layer_settings_ext->settingCount; i++) {
+            std::string name(layer_settings_ext->pSettings[i].name);
+            if (name == "enables") {
+                std::string data(layer_settings_ext->pSettings[i].data.arrayString.pCharArray);
+                SetLocalEnableSetting(data, ",", local_enables);
+            } else if (name == "disables") {
+                std::string data(layer_settings_ext->pSettings[i].data.arrayString.pCharArray);
+                SetLocalDisableSetting(data, ",", local_disables);
+            } else if (name == "message_id_filter") {
+                std::string data(layer_settings_ext->pSettings[i].data.arrayString.pCharArray);
+                CreateFilterMessageIdList(data, ",", report_data->filter_message_ids);
+            }
+        }
+    }
     const auto *validation_features_ext = lvl_find_in_chain<VkValidationFeaturesEXT>(pCreateInfo->pNext);
     if (validation_features_ext) {
         SetValidationFeatures(local_disables, local_enables, validation_features_ext);
