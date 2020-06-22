d5f0f816f74ec6814d709f936a252a42889ebd7a
Szilard Papp <szilard.papp@arm.com>
2020-06-22 09:01:29 +0100

    practices: Adding check for Presentation mode
    Change-Id: I53746649238738832de633224d03cec3d747afcb

--- layers/best_practices_error_enums.h
+++ layers/best_practices_error_enums.h
@@ -126,5 +126,7 @@ static const char DECORATE_UNUSED *kVUID_BestPractices_BeginCommandBuffer_OneTim
     "UNASSIGNED-BestPractices-vkBeginCommandBuffer-one-time-submit";
 static const char DECORATE_UNUSED *kVUID_BestPractices_BeginRenderPass_AttachmentNeedsReadback =
     "UNASSIGNED-BestPractices-vkCmdBeginRenderPass-attachment-needs-readback";
+static const char DECORATE_UNUSED *kVUID_BestPractices_CreateSwapchain_PresentMode =
+    "UNASSIGNED-BestPractices-vkCreateSwapchainKHR-swapchain-presentmode-not-fifo";
 
 #endif
