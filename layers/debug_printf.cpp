8a51b7dfa4ce24d58271dd27838badf5a3f83a40
Tony-LunarG <tony@lunarg.com>
2020-07-01 15:57:23 -0600

    layers: Use helper to pick spirv env
    Change-Id: I574b407cb5b8ba2b50dfe37f1764c6c4cad982fe

--- layers/debug_printf.cpp
+++ layers/debug_printf.cpp
@@ -262,7 +262,7 @@ bool DebugPrintf::InstrumentShader(const VkShaderModuleCreateInfo *pCreateInfo, 
     // Use the unique_shader_module_id as a shader ID so we can look up its handle later in the shader_map.
     // If descriptor indexing is enabled, enable length checks and updated descriptor checks
     using namespace spvtools;
-    spv_target_env target_env = SPV_ENV_VULKAN_1_1;
+    spv_target_env target_env = PickSpirvEnv(api_version, (device_extensions.vk_khr_spirv_1_4 != kNotEnabled));
     Optimizer optimizer(target_env);
     optimizer.RegisterPass(CreateInstDebugPrintfPass(desc_set_bind_index, unique_shader_module_id));
     bool pass = optimizer.Run(new_pgm.data(), new_pgm.size(), &new_pgm);
