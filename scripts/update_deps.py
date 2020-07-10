6aab64fb1ba88b2c376ced05eeec0fdff2c757c0
Bob Ellison <bob@lunarg.com>
2020-07-10 17:33:57 -0600

    scripts: update copyright notice for update_deps
    As per code review.

--- scripts/update_deps.py
+++ scripts/update_deps.py
@@ -2,8 +2,7 @@
 
 # Copyright 2017 The Glslang Authors. All rights reserved.
 # Copyright (c) 2018 Valve Corporation
-# Copyright (c) 2018 LunarG, Inc.
-# Copyright (c) 2020 LunarG, Inc.
+# Copyright (c) 2018-2020 LunarG, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
