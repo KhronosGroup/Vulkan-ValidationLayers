48407c3193f40efaa177e4f240d07c73e14df475
Tony-LunarG <tony@lunarg.com>
2020-07-10 11:26:10 -0600

    tests: Add copyright to gpu tests
    Change-Id: Ib9f3649109cd928a4e732c3c7b937f82fe869488

--- tests/vklayertests_gpu.cpp
+++ tests/vklayertests_gpu.cpp
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2020 The Khronos Group Inc.
+ * Copyright (c) 2020 Valve Corporation
+ * Copyright (c) 2020 LunarG, Inc.
+ * Copyright (c) 2020 Google, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Author: Mark Lobodzinski <mark@lunarg.com>
+ * Author: Tony Barbour <tony@LunarG.com>
+ */
+
 #include "layer_validation_tests.h"
 
 bool VkGpuAssistedLayerTest::InitGpuAssistedFramework(bool request_descriptor_indexing) {
