KhronosGroup/Vulkan-ValidationLayers

commit 24184fe81c6f5946acecbee7cada33bb3c46af1e

refactor: Move CALL_STATE into best_practices
Move CALL_STATE enum and associated checks outside of core_validation
and into best_practices.

Change-Id: Id842fe45f32feae73333f5bfbc20d6cf10878d02

layers/best_practices_validation.h
@@ -53,6 +53,28 @@ typedef enum {
 } BPVendorFlagBits;
 typedef VkFlags BPVendorFlags;
 
+enum CALL_STATE {
+    UNCALLED,       // Function has not been called
+    QUERY_COUNT,    // Function called once to query a count
+    QUERY_DETAILS,  // Function called w/ a count to query details
+};
+
+struct PHYSICAL_DEVICE_STATE_BP {
+    // Track the call state and array sizes for various query functions
+    CALL_STATE vkGetPhysicalDeviceQueueFamilyPropertiesState = UNCALLED;
+    CALL_STATE vkGetPhysicalDeviceLayerPropertiesState = UNCALLED;      // Currently unused
+    CALL_STATE vkGetPhysicalDeviceExtensionPropertiesState = UNCALLED;  // Currently unused
+    CALL_STATE vkGetPhysicalDeviceFeaturesState = UNCALLED;
+    CALL_STATE vkGetPhysicalDeviceSurfaceCapabilitiesKHRState = UNCALLED;
+    CALL_STATE vkGetPhysicalDeviceSurfacePresentModesKHRState = UNCALLED;
+    CALL_STATE vkGetPhysicalDeviceSurfaceFormatsKHRState = UNCALLED;
+    CALL_STATE vkGetPhysicalDeviceDisplayPlanePropertiesKHRState = UNCALLED;
+};
+
+struct SWAPCHAIN_STATE_BP {
+    CALL_STATE vkGetSwapchainImagesKHRState = UNCALLED;
+};
+
 // How many small indexed drawcalls in a command buffer before a warning is thrown
 static const uint32_t kMaxSmallIndexedDrawcalls = 10;
 
@@ -252,6 +274,64 @@ class BestPractices : public ValidationStateTracker {
                                                      const VkAllocationCallbacks* pAllocator, VkPipeline* pPipelines,
                                                      VkResult result, void* cgpl_state_data);
 
+    bool PreCallValidateAcquireNextImageKHR(VkDevice device, VkSwapchainKHR swapchain, uint64_t timeout, VkSemaphore semaphore,
+                                            VkFence fence, uint32_t* pImageIndex) const final;
+
+    void ManualPostCallRecordGetPhysicalDeviceQueueFamilyProperties(VkPhysicalDevice physicalDevice,
+                                                                    uint32_t* pQueueFamilyPropertyCount,
+                                                                    VkQueueFamilyProperties* pQueueFamilyProperties);
+
+    void ManualPostCallRecordGetPhysicalDeviceFeatures(VkPhysicalDevice physicalDevice, VkPhysicalDeviceFeatures* pFeatures);
+
+    void ManualPostCallRecordGetPhysicalDeviceFeatures2(VkPhysicalDevice physicalDevice, VkPhysicalDeviceFeatures2* pFeatures);
+
+    void ManualPostCallRecordGetPhysicalDeviceFeatures2KHR(VkPhysicalDevice physicalDevice, VkPhysicalDeviceFeatures2* pFeatures);
+
+    void ManualPostCallRecordGetPhysicalDeviceSurfaceCapabilitiesKHR(VkPhysicalDevice physicalDevice, VkSurfaceKHR surface,
+                                                                     VkSurfaceCapabilitiesKHR* pSurfaceCapabilities,
+                                                                     VkResult result);
+
+    void ManualPostCallRecordGetPhysicalDeviceSurfaceCapabilities2KHR(VkPhysicalDevice physicalDevice,
+                                                                      const VkPhysicalDeviceSurfaceInfo2KHR* pSurfaceInfo,
+                                                                      VkSurfaceCapabilities2KHR* pSurfaceCapabilities,
+                                                                      VkResult result);
+
+    void ManualPostCallRecordGetPhysicalDeviceSurfaceCapabilities2EXT(VkPhysicalDevice physicalDevice, VkSurfaceKHR surface,
+                                                                      VkSurfaceCapabilities2EXT* pSurfaceCapabilities,
+                                                                      VkResult result);
+
+    void ManualPostCallRecordGetPhysicalDeviceSurfacePresentModesKHR(VkPhysicalDevice physicalDevice, VkSurfaceKHR surface,
+                                                                     uint32_t* pPresentModeCount, VkPresentModeKHR* pPresentModes,
+                                                                     VkResult result);
+
+    void ManualPostCallRecordGetPhysicalDeviceSurfaceFormatsKHR(VkPhysicalDevice physicalDevice, VkSurfaceKHR surface,
+                                                                uint32_t* pSurfaceFormatCount, VkSurfaceFormatKHR* pSurfaceFormats,
+                                                                VkResult result);
+
+    void ManualPostCallRecordGetPhysicalDeviceSurfaceFormats2KHR(VkPhysicalDevice physicalDevice,
+                                                                 const VkPhysicalDeviceSurfaceInfo2KHR* pSurfaceInfo,
+                                                                 uint32_t* pSurfaceFormatCount,
+                                                                 VkSurfaceFormat2KHR* pSurfaceFormats, VkResult result);
+
+    void ManualPostCallRecordGetPhysicalDeviceDisplayPlanePropertiesKHR(VkPhysicalDevice physicalDevice, uint32_t* pPropertyCount,
+                                                                        VkDisplayPlanePropertiesKHR* pProperties, VkResult result);
+
+    void ManualPostCallRecordCreateSwapchainKHR(VkDevice device, const VkSwapchainCreateInfoKHR* pCreateInfo,
+                                                const VkAllocationCallbacks* pAllocator, VkSwapchainKHR* pSwapchain,
+                                                VkResult result);
+
+    void ManualPostCallRecordDestroySwapchainKHR(VkDevice device, VkSwapchainKHR swapchain,
+                                                 const VkAllocationCallbacks* pAllocator);
+
+    void ManualPostCallRecordGetSwapchainImagesKHR(VkDevice device, VkSwapchainKHR swapchain, uint32_t* pSwapchainImageCount,
+                                                   VkImage* pSwapchainImages, VkResult result);
+
+    void ManualPostCallRecordEnumeratePhysicalDevices(VkInstance instance, uint32_t* pPhysicalDeviceCount,
+                                                      VkPhysicalDevice* pPhysicalDevices, VkResult result);
+
+    void ManualPostCallRecordCreateDevice(VkPhysicalDevice gpu, const VkDeviceCreateInfo* pCreateInfo,
+                                          const VkAllocationCallbacks* pAllocator, VkDevice* pDevice, VkResult result);
+
 // Include code-generated functions
 #include "best_practices.h"
 
@@ -304,4 +384,20 @@ class BestPractices : public ValidationStateTracker {
 
     // used to track depth pre-pass heuristic data per command buffer
     std::unordered_map<VkCommandBuffer, DepthPrePassState> cbDepthPrePassStates = {};
+
+    // Used for instance versions of this object
+    std::unordered_map<VkSwapchainKHR, SWAPCHAIN_STATE_BP> swapchain_bp_state_map;
+
+    // Backing data for BP-specific state data
+    std::unordered_map<VkPhysicalDevice, PHYSICAL_DEVICE_STATE_BP> phys_device_bp_state_map;
+    // Physical device state for this instance
+    PHYSICAL_DEVICE_STATE_BP* instance_device_bp_state = nullptr;
+
+    // Get BestPractices-specific state for the given physical devices
+    PHYSICAL_DEVICE_STATE_BP* GetPhysicalDeviceStateBP(const VkPhysicalDevice& phys_device);
+    const PHYSICAL_DEVICE_STATE_BP* GetPhysicalDeviceStateBP(const VkPhysicalDevice& phys_device) const;
+
+    // Get BestPractices-specific for the current instance
+    PHYSICAL_DEVICE_STATE_BP* GetPhysicalDeviceStateBP();
+    const PHYSICAL_DEVICE_STATE_BP* GetPhysicalDeviceStateBP() const;
 };

scripts/best_practices_generator.py
@@ -110,6 +110,22 @@ class BestPracticesOutputGenerator(OutputGenerator):
             'vkQueuePresentKHR',
             'vkQueueBindSparse',
             'vkCreateGraphicsPipelines',
+            'vkGetPhysicalDeviceQueueFamilyProperties',
+            'vkGetPhysicalDeviceFeatures',
+            'vkGetPhysicalDeviceFeatures2',
+            'vkGetPhysicalDeviceFeatures2KHR',
+            'vkGetPhysicalDeviceSurfaceCapabilitiesKHR',
+            'vkGetPhysicalDeviceSurfaceCapabilities2KHR',
+            'vkGetPhysicalDeviceSurfaceCapabilities2EXT',
+            'vkGetPhysicalDeviceSurfacePresentModesKHR',
+            'vkGetPhysicalDeviceSurfaceFormatsKHR',
+            'vkGetPhysicalDeviceSurfaceFormats2KHR',
+            'vkGetPhysicalDeviceDisplayPlanePropertiesKHR',
+            'vkCreateSwapchainKHR',
+            'vkDestroySwapchainKHR',
+            'vkGetSwapchainImagesKHR',
+            'vkEnumeratePhysicalDevices',
+            'vkCreateDevice',
             ]
 
         self.extension_info = dict()

layers/core_validation.cpp
@@ -12096,13 +12096,6 @@ bool CoreChecks::ValidateAcquireNextImage(VkDevice device, const CommandVersion 
                                  acquirable > 1 ? "are" : "is", swapchain_image_count, min_image_count);
             }
         }
-
-        if (swapchain_data->images.size() == 0) {
-            skip |= LogWarning(swapchain, kVUID_Core_DrawState_SwapchainImagesNotFound,
-                               "%s: No images found to acquire from. Application probably did not call "
-                               "vkGetSwapchainImagesKHR after swapchain creation.",
-                               func_name);
-        }
     }
     return skip;
 }

layers/core_validation_types.h
@@ -71,12 +71,6 @@ struct CMD_BUFFER_STATE;
 class CoreChecks;
 class ValidationStateTracker;
 
-enum CALL_STATE {
-    UNCALLED,       // Function has not been called
-    QUERY_COUNT,    // Function called once to query a count
-    QUERY_DETAILS,  // Function called w/ a count to query details
-};
-
 class BASE_NODE {
   public:
     // Track when object is being used by an in-flight command buffer
@@ -551,7 +545,6 @@ class SWAPCHAIN_NODE : public BASE_NODE {
     std::vector<SWAPCHAIN_IMAGE> images;
     bool retired = false;
     bool shared_presentable = false;
-    CALL_STATE vkGetSwapchainImagesKHRState = UNCALLED;
     uint32_t get_swapchain_image_count = 0;
     SWAPCHAIN_NODE(const VkSwapchainCreateInfoKHR *pCreateInfo, VkSwapchainKHR swapchain)
         : createInfo(pCreateInfo), swapchain(swapchain) {}

commit b469020bac0a724b819d2a6da888904091291085

best_practices: Fix bug introduced in refactor
Fixes a bug introduced by 24184fe8 where the appropriate "post call"
code was incorrectly added as a "Manual" call for several methods that
do not get automatically generated.

This addresses
https://github.com/KhronosGroup/Vulkan-ValidationLayers/issues/2215 and https://github.com/KhronosGroup/Vulkan-ValidationLayers/issues/2216.

Change-Id: I507a8d2e3fbbfd0141c34079ea247e9a6cd5deeb

layers/best_practices_utils.cpp
@@ -2117,11 +2117,12 @@ bool BestPractices::PreCallValidateAcquireNextImageKHR(VkDevice device, VkSwapch
     return skip;
 }
 
-void BestPractices::ManualPostCallRecordGetPhysicalDeviceQueueFamilyProperties(VkPhysicalDevice physicalDevice,
-                                                                               uint32_t* pQueueFamilyPropertyCount,
-                                                                               VkQueueFamilyProperties* pQueueFamilyProperties) {
+void BestPractices::PostCallRecordGetPhysicalDeviceQueueFamilyProperties(VkPhysicalDevice physicalDevice,
+                                                                         uint32_t* pQueueFamilyPropertyCount,
+                                                                         VkQueueFamilyProperties* pQueueFamilyProperties) {
+    ValidationStateTracker::PostCallRecordGetPhysicalDeviceQueueFamilyProperties(physicalDevice, pQueueFamilyPropertyCount,
+                                                                                 pQueueFamilyProperties);
     auto* bp_pd_state = GetPhysicalDeviceStateBP(physicalDevice);
-
     if (bp_pd_state) {
         if (!pQueueFamilyProperties) {
             if (UNCALLED == bp_pd_state->vkGetPhysicalDeviceQueueFamilyPropertiesState)
@@ -2132,24 +2133,26 @@ void BestPractices::ManualPostCallRecordGetPhysicalDeviceQueueFamilyProperties(V
     }
 }
 
-void BestPractices::ManualPostCallRecordGetPhysicalDeviceFeatures(VkPhysicalDevice physicalDevice,
-                                                                  VkPhysicalDeviceFeatures* pFeatures) {
+void BestPractices::PostCallRecordGetPhysicalDeviceFeatures(VkPhysicalDevice physicalDevice, VkPhysicalDeviceFeatures* pFeatures) {
+    ValidationStateTracker::PostCallRecordGetPhysicalDeviceFeatures(physicalDevice, pFeatures);
     auto* bp_pd_state = GetPhysicalDeviceStateBP(physicalDevice);
     if (bp_pd_state) {
         bp_pd_state->vkGetPhysicalDeviceFeaturesState = QUERY_DETAILS;
     }
 }
 
-void BestPractices::ManualPostCallRecordGetPhysicalDeviceFeatures2(VkPhysicalDevice physicalDevice,
-                                                                   VkPhysicalDeviceFeatures2* pFeatures) {
+void BestPractices::PostCallRecordGetPhysicalDeviceFeatures2(VkPhysicalDevice physicalDevice,
+                                                             VkPhysicalDeviceFeatures2* pFeatures) {
+    ValidationStateTracker::PostCallRecordGetPhysicalDeviceFeatures2(physicalDevice, pFeatures);
     auto* bp_pd_state = GetPhysicalDeviceStateBP(physicalDevice);
     if (bp_pd_state) {
         bp_pd_state->vkGetPhysicalDeviceFeaturesState = QUERY_DETAILS;
     }
 }
 
-void BestPractices::ManualPostCallRecordGetPhysicalDeviceFeatures2KHR(VkPhysicalDevice physicalDevice,
-                                                                      VkPhysicalDeviceFeatures2* pFeatures) {
+void BestPractices::PostCallRecordGetPhysicalDeviceFeatures2KHR(VkPhysicalDevice physicalDevice,
+                                                                VkPhysicalDeviceFeatures2* pFeatures) {
+    ValidationStateTracker::PostCallRecordGetPhysicalDeviceFeatures2KHR(physicalDevice, pFeatures);
     auto* bp_pd_state = GetPhysicalDeviceStateBP(physicalDevice);
     if (bp_pd_state) {
         bp_pd_state->vkGetPhysicalDeviceFeaturesState = QUERY_DETAILS;
@@ -2271,8 +2274,9 @@ void BestPractices::ManualPostCallRecordCreateSwapchainKHR(VkDevice device, cons
     }
 }
 
-void BestPractices::ManualPostCallRecordDestroySwapchainKHR(VkDevice device, VkSwapchainKHR swapchain,
-                                                            const VkAllocationCallbacks* pAllocator) {
+void BestPractices::PostCallRecordDestroySwapchainKHR(VkDevice device, VkSwapchainKHR swapchain,
+                                                      const VkAllocationCallbacks* pAllocator) {
+    ValidationStateTracker::PostCallRecordDestroySwapchainKHR(device, swapchain, pAllocator);
     auto swapchain_state_itr = swapchain_bp_state_map.find(swapchain);
     if (swapchain_state_itr != swapchain_bp_state_map.cend()) {
         swapchain_bp_state_map.erase(swapchain_state_itr);

layers/best_practices_validation.h
@@ -277,15 +277,14 @@ class BestPractices : public ValidationStateTracker {
     bool PreCallValidateAcquireNextImageKHR(VkDevice device, VkSwapchainKHR swapchain, uint64_t timeout, VkSemaphore semaphore,
                                             VkFence fence, uint32_t* pImageIndex) const final;
 
-    void ManualPostCallRecordGetPhysicalDeviceQueueFamilyProperties(VkPhysicalDevice physicalDevice,
-                                                                    uint32_t* pQueueFamilyPropertyCount,
-                                                                    VkQueueFamilyProperties* pQueueFamilyProperties);
+    void PostCallRecordGetPhysicalDeviceQueueFamilyProperties(VkPhysicalDevice physicalDevice, uint32_t* pQueueFamilyPropertyCount,
+                                                              VkQueueFamilyProperties* pQueueFamilyProperties) final;
 
-    void ManualPostCallRecordGetPhysicalDeviceFeatures(VkPhysicalDevice physicalDevice, VkPhysicalDeviceFeatures* pFeatures);
+    void PostCallRecordGetPhysicalDeviceFeatures(VkPhysicalDevice physicalDevice, VkPhysicalDeviceFeatures* pFeatures) final;
 
-    void ManualPostCallRecordGetPhysicalDeviceFeatures2(VkPhysicalDevice physicalDevice, VkPhysicalDeviceFeatures2* pFeatures);
+    void PostCallRecordGetPhysicalDeviceFeatures2(VkPhysicalDevice physicalDevice, VkPhysicalDeviceFeatures2* pFeatures) final;
 
-    void ManualPostCallRecordGetPhysicalDeviceFeatures2KHR(VkPhysicalDevice physicalDevice, VkPhysicalDeviceFeatures2* pFeatures);
+    void PostCallRecordGetPhysicalDeviceFeatures2KHR(VkPhysicalDevice physicalDevice, VkPhysicalDeviceFeatures2* pFeatures) final;
 
     void ManualPostCallRecordGetPhysicalDeviceSurfaceCapabilitiesKHR(VkPhysicalDevice physicalDevice, VkSurfaceKHR surface,
                                                                      VkSurfaceCapabilitiesKHR* pSurfaceCapabilities,
@@ -320,8 +319,8 @@ class BestPractices : public ValidationStateTracker {
                                                 const VkAllocationCallbacks* pAllocator, VkSwapchainKHR* pSwapchain,
                                                 VkResult result);
 
-    void ManualPostCallRecordDestroySwapchainKHR(VkDevice device, VkSwapchainKHR swapchain,
-                                                 const VkAllocationCallbacks* pAllocator);
+    void PostCallRecordDestroySwapchainKHR(VkDevice device, VkSwapchainKHR swapchain,
+                                           const VkAllocationCallbacks* pAllocator) final;
 
     void ManualPostCallRecordGetSwapchainImagesKHR(VkDevice device, VkSwapchainKHR swapchain, uint32_t* pSwapchainImageCount,
                                                    VkImage* pSwapchainImages, VkResult result);

scripts/best_practices_generator.py
@@ -110,10 +110,6 @@ class BestPracticesOutputGenerator(OutputGenerator):
             'vkQueuePresentKHR',
             'vkQueueBindSparse',
             'vkCreateGraphicsPipelines',
-            'vkGetPhysicalDeviceQueueFamilyProperties',
-            'vkGetPhysicalDeviceFeatures',
-            'vkGetPhysicalDeviceFeatures2',
-            'vkGetPhysicalDeviceFeatures2KHR',
             'vkGetPhysicalDeviceSurfaceCapabilitiesKHR',
             'vkGetPhysicalDeviceSurfaceCapabilities2KHR',
             'vkGetPhysicalDeviceSurfaceCapabilities2EXT',
@@ -122,7 +118,6 @@ class BestPracticesOutputGenerator(OutputGenerator):
             'vkGetPhysicalDeviceSurfaceFormats2KHR',
             'vkGetPhysicalDeviceDisplayPlanePropertiesKHR',
             'vkCreateSwapchainKHR',
-            'vkDestroySwapchainKHR',
             'vkGetSwapchainImagesKHR',
             'vkEnumeratePhysicalDevices',
             'vkCreateDevice',

tests/vklayertests_best_practices.cpp
@@ -691,3 +691,71 @@ TEST_F(VkBestPracticesLayerTest, TripleBufferingTest) {
     ASSERT_VK_SUCCESS(err)
     DestroySwapchain();
 }
+
+TEST_F(VkBestPracticesLayerTest, ExpectedQueryDetails) {
+    TEST_DESCRIPTION("Check that GetPhysicalDeviceQueueFamilyProperties is working as expected");
+
+    ASSERT_NO_FATAL_FAILURE(InitBestPracticesFramework());
+    const vk_testing::PhysicalDevice phys_device_obj(gpu_);
+
+    std::vector<VkQueueFamilyProperties> queue_family_props;
+
+    m_errorMonitor->ExpectSuccess(kErrorBit | kWarningBit);
+
+    // Ensure we can find a graphics queue family.
+    uint32_t queue_count = 0;
+    vk::GetPhysicalDeviceQueueFamilyProperties(phys_device_obj.handle(), &queue_count, nullptr);
+
+    queue_family_props.resize(queue_count);
+    vk::GetPhysicalDeviceQueueFamilyProperties(phys_device_obj.handle(), &queue_count, queue_family_props.data());
+
+    vk_testing::Device device(phys_device_obj.handle());
+    device.init();
+}
+
+TEST_F(VkBestPracticesLayerTest, MissingQueryDetails) {
+    TEST_DESCRIPTION("Check that GetPhysicalDeviceQueueFamilyProperties generates appropriate query warning");
+
+    ASSERT_NO_FATAL_FAILURE(InitBestPracticesFramework());
+    const vk_testing::PhysicalDevice phys_device_obj(gpu_);
+
+    std::vector<VkQueueFamilyProperties> queue_family_props(1);
+    uint32_t queue_count = static_cast<uint32_t>(queue_family_props.size());
+
+    m_errorMonitor->SetDesiredFailureMsg(kWarningBit, "UNASSIGNED-CoreValidation-DevLimit-MissingQueryCount");
+    vk::GetPhysicalDeviceQueueFamilyProperties(phys_device_obj.handle(), &queue_count, queue_family_props.data());
+    m_errorMonitor->VerifyFound();
+
+    // Now get information correctly
+    m_errorMonitor->ExpectSuccess(kErrorBit | kWarningBit);
+
+    vk_testing::QueueCreateInfoArray queue_info(phys_device_obj.queue_properties());
+    // Only request creation with queuefamilies that have at least one queue
+    std::vector<VkDeviceQueueCreateInfo> create_queue_infos;
+    auto qci = queue_info.data();
+    for (uint32_t j = 0; j < queue_info.size(); ++j) {
+        if (qci[j].queueCount) {
+            create_queue_infos.push_back(qci[j]);
+        }
+    }
+    m_errorMonitor->VerifyNotFound();
+
+    VkPhysicalDeviceFeatures all_features;
+    VkDeviceCreateInfo device_ci = {};
+    device_ci.sType = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO;
+    device_ci.pNext = nullptr;
+    device_ci.queueCreateInfoCount = create_queue_infos.size();
+    device_ci.pQueueCreateInfos = create_queue_infos.data();
+    device_ci.enabledLayerCount = 0;
+    device_ci.ppEnabledLayerNames = NULL;
+    device_ci.enabledExtensionCount = 0;
+    device_ci.ppEnabledExtensionNames = nullptr;
+    device_ci.pEnabledFeatures = &all_features;
+
+    // vkGetPhysicalDeviceFeatures has not been called, so this should produce a warning
+    m_errorMonitor->SetDesiredFailureMsg(kWarningBit,
+                                         "UNASSIGNED-BestPractices-vkCreateDevice-physical-device-features-not-retrieved");
+    VkDevice device;
+    vk::CreateDevice(phys_device_obj.handle(), &device_ci, nullptr, &device);
+    m_errorMonitor->VerifyFound();
+}