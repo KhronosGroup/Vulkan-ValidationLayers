249c0506de5d61c950bffa23d75602a4499c166c
Mark Lobodzinski <mark@lunarg.com>
2020-10-30 12:34:38 -0600

    stateless: Remove non-VU pNext chain cycle check
    Change-Id: I8f66af6fc5eddde46256e2d17a0d2b3d03f13746

--- layers/stateless_validation.h
+++ layers/stateless_validation.h
@@ -43,7 +43,6 @@ static const char DECORATE_UNUSED *kVUID_PVError_DeviceFeature = "UNASSIGNED-Gen
 static const char DECORATE_UNUSED *kVUID_PVError_FailureCode = "UNASSIGNED-GeneralParameterError-FailureCode";
 static const char DECORATE_UNUSED *kVUID_PVError_ExtensionNotEnabled = "UNASSIGNED-GeneralParameterError-ExtensionNotEnabled";
 static const char DECORATE_UNUSED *kVUID_PVError_ApiVersionViolation = "UNASSIGNED-API-Version-Violation";
-
 extern const uint32_t GeneratedVulkanHeaderVersion;
 
 extern const VkQueryPipelineStatisticFlags AllVkQueryPipelineStatisticFlagBits;
@@ -521,21 +520,11 @@ class StatelessValidation : public ValidationObject {
                 const VkStructureType *end = allowed_types + allowed_type_count;
                 const VkBaseOutStructure *current = reinterpret_cast<const VkBaseOutStructure *>(next);
 
-                cycle_check.insert(next);
-
                 while (current != NULL) {
                     if (((strncmp(api_name, "vkCreateInstance", strlen(api_name)) != 0) ||
                          (current->sType != VK_STRUCTURE_TYPE_LOADER_INSTANCE_CREATE_INFO)) &&
                         ((strncmp(api_name, "vkCreateDevice", strlen(api_name)) != 0) ||
                          (current->sType != VK_STRUCTURE_TYPE_LOADER_DEVICE_CREATE_INFO))) {
-                        if (cycle_check.find(current->pNext) != cycle_check.end()) {
-                            std::string message = "%s: %s chain contains a cycle -- pNext pointer %" PRIx64 " is repeated.";
-                            skip_call |= LogError(device, kVUID_PVError_InvalidStructPNext, message.c_str(), api_name,
-                                                  parameter_name.get_name().c_str(), reinterpret_cast<uint64_t>(next));
-                            break;
-                        } else {
-                            cycle_check.insert(current->pNext);
-                        }
 
                         std::string type_name = string_VkStructureType(current->sType);
                         if (unique_stype_check.find(current->sType) != unique_stype_check.end()) {
