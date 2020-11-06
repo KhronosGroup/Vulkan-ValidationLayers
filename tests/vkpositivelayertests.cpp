6668bbea6c935f33441b0a2dfd0e5808988c3c0a
Mark Lobodzinski <mark@lunarg.com>
2020-11-05 18:15:52 -0700

    tests: Add oldSwapchain-from-different-device test
    Change-Id: I2c0d96bd84393b80ec65d5ed584f1930592333b5

--- tests/vkpositivelayertests.cpp
+++ tests/vkpositivelayertests.cpp
@@ -9918,6 +9918,33 @@ TEST_F(VkPositiveLayerTest, SwapchainExclusiveModeQueueFamilyPropertiesReference
 
         vk::CreateSwapchainKHR(device(), &swapchain_create_info, nullptr, &m_swapchain);
 
+        // Create another device, create another swapchain, and use this one for oldSwapchain
+        // It is legal to include an 'oldSwapchain' object that is from a different device
+        const float q_priority[] = {1.0f};
+        VkDeviceQueueCreateInfo queue_ci = {};
+        queue_ci.sType = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO;
+        queue_ci.queueFamilyIndex = 0;
+        queue_ci.queueCount = 1;
+        queue_ci.pQueuePriorities = q_priority;
+
+        VkDeviceCreateInfo device_ci = {};
+        device_ci.sType = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO;
+        device_ci.queueCreateInfoCount = 1;
+        device_ci.pQueueCreateInfos = &queue_ci;
+        device_ci.ppEnabledExtensionNames = m_device_extension_names.data();
+        device_ci.enabledExtensionCount = m_device_extension_names.size();
+
+        VkDevice test_device;
+        vk::CreateDevice(gpu(), &device_ci, nullptr, &test_device);
+
+        swapchain_create_info.oldSwapchain = m_swapchain;
+        VkSwapchainKHR new_swapchain;
+        vk::CreateSwapchainKHR(test_device, &swapchain_create_info, nullptr, &new_swapchain);
+
+        vk::DestroySwapchainKHR(test_device, new_swapchain, nullptr);
+
+        vk::DestroyDevice(test_device, nullptr);
+
         if (m_surface != VK_NULL_HANDLE) {
             vk::DestroySurfaceKHR(instance(), m_surface, nullptr);
             m_surface = VK_NULL_HANDLE;
