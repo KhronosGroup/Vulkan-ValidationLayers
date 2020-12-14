d4aabc43dd0598b4e07cf4e757b7fd25b09ea51e
Mark Lobodzinski <mark@lunarg.com>
2020-12-14 11:01:36 -0700

    actions: Rework gn script
    Change-Id: Id4903b36d6457c2d38613e54d7bb26f9114a3e9c

---
+++ scripts/common_ci.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3 -i
+#
+# Copyright (c) 2015-2017, 2019-2020 The Khronos Group Inc.
+# Copyright (c) 2015-2017, 2019-2020 Valve Corporation
+# Copyright (c) 2015-2017, 2019-2020 LunarG, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# Author: Mark Lobodzinski <mark@lunarg.com>
+
+import os,re,sys,string
+import shutil
+import subprocess
+import platform
+import xml.etree.ElementTree as etree
+from collections import namedtuple, OrderedDict
+
+# helper to define paths relative to the repo root
+def repo_relative(path):
+    return os.path.abspath(os.path.join(os.path.dirname(__file__), '..', path))
+
+# Runs a command in a directory and returns its return code.
+#    Captures the standard error stream and prints it if error.
+def RunShellCmd(command, directory):
+    print("Command: ", command)
+    cmd_list = command.split(" ")
+    p = subprocess.Popen(cmd_list, cwd=directory, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    (stdout, stderr) = p.communicate()
+    if p.returncode != 0:
+        print('Error -- stderr contents:\n', stderr.decode())
+    else:
+        print(stdout.decode())
+    return p.returncode
+
--- scripts/github_ci_gn.py
+++ scripts/github_ci_gn.py
@@ -16,75 +16,44 @@
 #
 # Author: Mark Lobodzinski <mark@lunarg.com>
 
-# Script to determine if source code in Pull Request is properly formatted.
-
 import os
 import argparse
 import shutil
 import subprocess
 import sys
 import platform
-import common_codegen
+import common_ci
 from argparse import RawDescriptionHelpFormatter
 
-os.system("")
-
-VERBOSE = False
 PROJECT_ROOT = os.path.abspath(os.path.join(os.path.split(os.path.abspath(__file__))[0], '..'))
 
-# Split command lines into a list of args for python subprocess
-def ListArgs(command_string):
-    return command_string.split(" ")
-
-# Runs a command in a directory and returns its standard output stream.
-#    Captures the standard error stream and prints it if error.
-#    Raises a RuntimeError if the command fails to launch or otherwise fails.
-def command_output(cmd, directory):
-    if VERBOSE:
-        print('In {d}: {cmd}'.format(d=directory, cmd=cmd))
-    p = subprocess.Popen(
-        cmd, cwd=directory, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    (stdout, stderr) = p.communicate()
-    #if p.returncode != 0:
-        #print('*** Error ***\nstderr contents:\n{}'.format(stderr))
-        #if not fail_ok:
-        #    raise RuntimeError('Failed to run {} in {}'.format(cmd, directory))
-    if VERBOSE:
-        print(stdout)
-    return p.returncode, stdout
-
 #
-# Fetch Android components, build Android VVL
+# Build VVL repo using gn and Ninja
 def BuildGn():
+    print("Cloning Ninja depot_tools\n")
     clone_cmd = 'git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git depot_tools'
-    ret_code, out_text = command_output(ListArgs(clone_cmd), PROJECT_ROOT)
-    os.environ['PATH'] = os.environ.get('PATH') + ":" + common_codegen.repo_relative("depot_tools")
-    print("Cloning Ninja depot_tools\n", out_text.decode())
+    ret_code = common_ci.RunShellCmd(clone_cmd, PROJECT_ROOT)
+    os.environ['PATH'] = os.environ.get('PATH') + ":" + common_ci.repo_relative("depot_tools")
 
     if ret_code == 0:
-        # Run GN build update dependencies script
+        print("Updating Ninja build dependencies\n")
         update_cmd = './build-gn/update_deps.sh'
-        ret_code, out_text = command_output(ListArgs(update_cmd), PROJECT_ROOT)
-        print("Updating Ninja build dependencies\n", out_text.decode())
+        ret_code = common_ci.RunShellCmd(update_cmd, PROJECT_ROOT)
 
     if ret_code == 0:
-        # Gen GN files
+        print("Generating Ninja Files\n", )
         # TODO: Enable ccache for Ninja builds : --args="cc_wrapper=\"ccache\""
         #       There are issues passing this on the command line.
         #       Possible solution is to write/append this line to the out/Debug/args.gn file
         gn_gen_cmd = 'gn gen out/Debug'
-        ret_code, out_text = command_output(ListArgs(gn_gen_cmd), PROJECT_ROOT)
-        print("Generating Ninja Files\n", out_text.decode())
+        ret_code = common_ci.RunShellCmd(gn_gen_cmd, PROJECT_ROOT)
 
     if ret_code == 0:
-        # GN Build
+        print("Running Ninja Build\n", )
         ninja_build_cmd = 'ninja -C out/Debug'
-        ret_code, out_text = command_output(ListArgs(ninja_build_cmd), PROJECT_ROOT)
-        print("Running Ninja Build\n", out_text.decode())
+        ret_code = common_ci.RunShellCmd(ninja_build_cmd, PROJECT_ROOT)
 
-    if ret_code != 0:
-        sys.exit(ret_code)
-    return 0
+    return ret_code
 
 #
 # Module Entrypoint
@@ -96,14 +65,13 @@ def main():
 
     if sys.version_info[0] != 3:
         print("This script requires Python 3. Run script with [-h] option for more details.")
-        exit()
+        sys.exit(0)
 
-    failed = BuildGn()
-
-    if failed:
-        exit(1)
+    ret_code = BuildGn()
+    if ret_code:
+        sys.exit(ret_code)
     else:
-        exit(0)
+        sys.exit(0)
 
 if __name__ == '__main__':
   main()
