9ae84724a4ad9b16fddbfdd49ca6b127b87ad6ac
Shannon McPherson <shannon@lunarg.com>
2021-03-01 10:44:36 -0700

    scripts: Update copyright in `common_codegen.py`
    Change-Id: I25c9f6f59a01b7383326e93cd414fcc16c9cbc36

--- scripts/common_codegen.py
+++ scripts/common_codegen.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3 -i
 #
-# Copyright (c) 2015-2017, 2019-2020 The Khronos Group Inc.
-# Copyright (c) 2015-2017, 2019-2020 Valve Corporation
-# Copyright (c) 2015-2017, 2019-2020 LunarG, Inc.
+# Copyright (c) 2015-2017, 2019-2021 The Khronos Group Inc.
+# Copyright (c) 2015-2017, 2019-2021 Valve Corporation
+# Copyright (c) 2015-2017, 2019-2021 LunarG, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -25,10 +25,10 @@ from collections import namedtuple, OrderedDict
 # Copyright text prefixing all headers (list of strings).
 prefixStrings = [
     '/*',
-    '** Copyright (c) 2015-2017, 2019 The Khronos Group Inc.',
-    '** Copyright (c) 2015-2017, 2019 Valve Corporation',
-    '** Copyright (c) 2015-2017, 2019 LunarG, Inc.',
-    '** Copyright (c) 2015-2017, 2019 Google Inc.',
+    '** Copyright (c) 2015-2017, 2019-2021 The Khronos Group Inc.',
+    '** Copyright (c) 2015-2017, 2019-2021 Valve Corporation',
+    '** Copyright (c) 2015-2017, 2019-2021 LunarG, Inc.',
+    '** Copyright (c) 2015-2017, 2019-2021 Google Inc.',
     '**',
     '** Licensed under the Apache License, Version 2.0 (the "License");',
     '** you may not use this file except in compliance with the License.',
