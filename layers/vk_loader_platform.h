42dc7dc31755f3138a2e2ba158b9d48772a321f5
xantares <xantares09@hotmail.com>
2021-03-25 13:48:58 +0100

    layers: Bump vk_loader_platform.h copyright

--- layers/vk_loader_platform.h
+++ layers/vk_loader_platform.h
@@ -1,8 +1,8 @@
 /*
  *
- * Copyright (c) 2015-2020 The Khronos Group Inc.
- * Copyright (c) 2015-2020 Valve Corporation
- * Copyright (c) 2015-2020 LunarG, Inc.
+ * Copyright (c) 2015-2021 The Khronos Group Inc.
+ * Copyright (c) 2015-2021 Valve Corporation
+ * Copyright (c) 2015-2021 LunarG, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
