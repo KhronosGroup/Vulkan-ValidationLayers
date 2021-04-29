KhronosGroup/Vulkan-ValidationLayers

commit 5363130b75709e96e75cfecb75750c7ffe458b3e

corechecks: Simplify Image Layout Transition maps
Use a single range_map in ImageSubresourceLayoutMap instead of having
3 separate maps for each type of data tracked. This speeds up
construction, lookups and iteration.

Change-Id: I2184e03b82163d6a164a2cad8cba49d04fc67a4b

layers/image_layout_map.h
@@ -62,11 +62,6 @@ class ImageSubresourceLayoutMap {
   public:
     typedef std::function<bool(const VkImageSubresource&, VkImageLayout, VkImageLayout)> Callback;
 
-    struct Layouts {
-        VkImageLayout current_layout;
-        VkImageLayout initial_layout;
-    };
-
     struct SubresourceLayout {
         VkImageSubresource subresource;
         VkImageLayout current_layout;
@@ -79,23 +74,42 @@ class ImageSubresourceLayoutMap {
         SubresourceLayout() = default;
     };
 
-    struct SubresourceRangeLayout {
-        VkImageSubresourceRange subresource_range;
-        VkImageLayout current_layout;
+    struct LayoutEntry {
         VkImageLayout initial_layout;
-        bool operator==(const SubresourceRangeLayout& rhs) const {
-            bool is_equal = (current_layout == rhs.current_layout) && (initial_layout == rhs.initial_layout) &&
-                            (subresource_range == rhs.subresource_range);
-            return is_equal;
-        }
-        bool operator!=(const SubresourceRangeLayout& rhs) const { return !(*this == rhs); }
-    };
+        VkImageLayout current_layout;
+        InitialLayoutState* state;
 
-    using RangeMap = subresource_adapter::BothRangeMap<VkImageLayout, 16>;
-    template <typename MapA, typename MapB>
-    using ParallelIterator = sparse_container::parallel_iterator<MapA, MapB>;
+        LayoutEntry(VkImageLayout initial_ = kInvalidLayout, VkImageLayout current_ = kInvalidLayout,
+                    InitialLayoutState* s = nullptr)
+            : initial_layout(initial_), current_layout(current_), state(s) {}
+
+        bool operator!=(const LayoutEntry& rhs) const {
+            return initial_layout != rhs.initial_layout || current_layout != rhs.current_layout || state != rhs.state;
+        }
+        bool Update(const LayoutEntry& src) {
+            bool updated_current = false;
+            // current_layout can be updated repeatedly.
+            if (current_layout != src.current_layout && src.current_layout != kInvalidLayout) {
+                current_layout = src.current_layout;
+                updated_current = true;
+            }
+            // initial_layout and state cannot be updated once they have a valid value.
+            if (initial_layout == kInvalidLayout) {
+                initial_layout = src.initial_layout;
+            }
+            if (state == nullptr) {
+                state = src.state;
+            }
+            return updated_current;
+        }
+        // updater for splice()
+        struct Updater {
+            bool update(LayoutEntry& dst, const LayoutEntry& src) const { return dst.Update(src); }
+            layer_data::optional<LayoutEntry> insert(const LayoutEntry& src) const { return layer_data::optional<LayoutEntry>(layer_data::in_place, src); }
+        };
+    };
+    using RangeMap = subresource_adapter::BothRangeMap<LayoutEntry, 16>;
     using LayoutMap = RangeMap;
-    using InitialLayoutMap = RangeMap;
     using InitialLayoutStates = small_vector<InitialLayoutState, 2, uint32_t>;
 
     class ConstIterator {
@@ -108,7 +122,7 @@ class ImageSubresourceLayoutMap {
         const SubresourceLayout* operator->() const { return &pos_; }
         const SubresourceLayout& operator*() const { return pos_; }
 
-        ConstIterator() : range_gen_(), parallel_it_(), skip_invalid_(false), always_get_initial_(false), pos_() {}
+        ConstIterator() : range_gen_(), layouts_(nullptr), iter_(), skip_invalid_(false), always_get_initial_(false), pos_() {}
         bool AtEnd() const { return pos_.subresource.aspectMask == 0; }
 
         // Only for comparisons to end()
@@ -119,13 +133,14 @@ class ImageSubresourceLayoutMap {
       protected:
         void Increment();
         friend ImageSubresourceLayoutMap;
-        ConstIterator(const RangeMap& current, const RangeMap& initial, const Encoder& encoder,
-                      const VkImageSubresourceRange& subres, bool skip_invalid, bool always_get_initial);
+        ConstIterator(const RangeMap& layouts, const Encoder& encoder, const VkImageSubresourceRange& subres, bool skip_invalid,
+                      bool always_get_initial);
         void UpdateRangeAndValue();
         void ForceEndCondition() { pos_.subresource.aspectMask = 0; }
 
         RangeGenerator range_gen_;
-        ParallelIterator<const RangeMap, const RangeMap> parallel_it_;
+        const RangeMap* layouts_;
+        RangeMap::const_iterator iter_;
         bool skip_invalid_;
         bool always_get_initial_;
         SubresourceLayout pos_;
@@ -136,7 +151,7 @@ class ImageSubresourceLayoutMap {
     ConstIterator Find(const VkImageSubresourceRange& subres_range, bool skip_invalid = true,
                        bool always_get_initial = false) const {
         if (InRange(subres_range)) {
-            return ConstIterator(layouts_.current, layouts_.initial, encoder_, subres_range, skip_invalid, always_get_initial);
+            return ConstIterator(layouts_, encoder_, subres_range, skip_invalid, always_get_initial);
         }
         return End();
     }
@@ -146,35 +161,22 @@ class ImageSubresourceLayoutMap {
     inline ConstIterator begin() const { return Begin(); }  // STL style, for range based loops and familiarity
     const ConstIterator& End() const { return end_iterator; }
     const ConstIterator& end() const { return End(); }  // STL style, for range based loops and familiarity.
-    inline size_t InitialLayoutSize() const { return layouts_.initial.size(); }
-    inline size_t CurrentLayoutSize() const { return layouts_.current.size(); }
 
     bool SetSubresourceRangeLayout(const CMD_BUFFER_STATE& cb_state, const VkImageSubresourceRange& range, VkImageLayout layout,
                                    VkImageLayout expected_layout = kInvalidLayout);
-    bool SetSubresourceRangeInitialLayout(const CMD_BUFFER_STATE& cb_state, const VkImageSubresourceRange& range,
-                                          VkImageLayout layout, const IMAGE_VIEW_STATE* view_state = nullptr);
-    bool SetSubresourceRangeInitialLayout(const CMD_BUFFER_STATE& cb_state, VkImageLayout layout,
+    void SetSubresourceRangeInitialLayout(const CMD_BUFFER_STATE& cb_state, const VkImageSubresourceRange& range,
+                                          VkImageLayout layout);
+    void SetSubresourceRangeInitialLayout(const CMD_BUFFER_STATE& cb_state, VkImageLayout layout,
                                           const IMAGE_VIEW_STATE& view_state);
-    bool ForRange(const VkImageSubresourceRange& range, const Callback& callback, bool skip_invalid = true,
-                  bool always_get_initial = false) const;
-    VkImageLayout GetSubresourceLayout(const VkImageSubresource& subresource) const;
-    VkImageLayout GetSubresourceInitialLayout(const VkImageSubresource& subresource) const;
-    Layouts GetSubresourceLayouts(const VkImageSubresource& subresource, bool always_get_initial = true) const;
+    const LayoutEntry* GetSubresourceLayouts(const VkImageSubresource& subresource) const;
     const InitialLayoutState* GetSubresourceInitialLayoutState(const IndexType index) const;
     const InitialLayoutState* GetSubresourceInitialLayoutState(const VkImageSubresource& subresource) const;
     bool UpdateFrom(const ImageSubresourceLayoutMap& from);
     uintptr_t CompatibilityKey() const;
-    const InitialLayoutMap& GetInitialLayoutMap() const { return layouts_.initial; }
-    const LayoutMap& GetCurrentLayoutMap() const { return layouts_.current; }
+    const LayoutMap& GetLayoutMap() const { return layouts_; }
     ImageSubresourceLayoutMap(const IMAGE_STATE& image_state);
     ~ImageSubresourceLayoutMap() {}
     const IMAGE_STATE* GetImageView() const { return &image_state_; };
-
-    struct LayoutMaps {
-        LayoutMap current;
-        InitialLayoutMap initial;
-        LayoutMaps(typename LayoutMap::index_type size) : current(size), initial(size) {}
-    };
 
   protected:
     // This looks a bit ponderous but kAspectCount is a compile time constant
@@ -189,28 +191,14 @@ class ImageSubresourceLayoutMap {
     bool InRange(const VkImageSubresource& subres) const { return encoder_.InRange(subres); }
     bool InRange(const VkImageSubresourceRange& range) const { return encoder_.InRange(range); }
 
-    inline InitialLayoutState* UpdateInitialLayoutState(const IndexRange& range, InitialLayoutState* initial_state,
-                                                        const CMD_BUFFER_STATE& cb_state, const IMAGE_VIEW_STATE* view_state) {
-        if (!initial_state) {
-            // Allocate on demand...  initial_layout_states_ holds ownership as a unique_ptr, while
-            // each subresource has a non-owning copy of the plain pointer.
-            initial_layout_states_.emplace_back(cb_state, view_state);
-            initial_state = &initial_layout_states_.back();
-        }
-        assert(initial_state);
-        sparse_container::update_range_value(initial_layout_state_map_, range, initial_state, WritePolicy::prefer_dest);
-        return initial_state;
-    }
-
     // This map *also* needs "write once" semantics
     using InitialLayoutStateMap = subresource_adapter::BothRangeMap<InitialLayoutState*, 16>;
 
   private:
     const IMAGE_STATE& image_state_;
     const Encoder& encoder_;
-    LayoutMaps layouts_;
+    LayoutMap layouts_;
     InitialLayoutStates initial_layout_states_;
-    InitialLayoutStateMap initial_layout_state_map_;
 
     static const ConstIterator end_iterator;  // Just to hold the end condition tombstone (aspectMask == 0)
 };

layers/buffer_validation.cpp
@@ -4451,6 +4451,25 @@ const GlobalImageLayoutRangeMap *GetLayoutRangeMap(const GlobalImageLayoutMap &m
     return nullptr;
 }
 
+// Helper to update the Global or Overlay layout map
+struct GlobalLayoutUpdater {
+    bool update(VkImageLayout &dst, const image_layout_map::ImageSubresourceLayoutMap::LayoutEntry &src) const {
+        if (src.current_layout != image_layout_map::kInvalidLayout && dst != src.current_layout) {
+            dst = src.current_layout;
+            return true;
+        }
+        return false;
+    }
+
+    layer_data::optional<VkImageLayout> insert(const image_layout_map::ImageSubresourceLayoutMap::LayoutEntry &src) const {
+        layer_data::optional<VkImageLayout> result;
+        if (src.current_layout != image_layout_map::kInvalidLayout) {
+            result.emplace(src.current_layout);
+        }
+        return result;
+    }
+};
+
 // This validates that the initial layout specified in the command buffer for the IMAGE is the same as the global IMAGE layout
 bool CoreChecks::ValidateCmdBufImageLayouts(const CMD_BUFFER_STATE *pCB, const GlobalImageLayoutMap &globalImageLayoutMap,
                                             GlobalImageLayoutMap &overlayLayoutMap) const {
@@ -4463,9 +4482,9 @@ bool CoreChecks::ValidateCmdBufImageLayouts(const CMD_BUFFER_STATE *pCB, const G
         const auto *image_state = GetImageState(image);
         if (!image_state) continue;  // Can't check layouts of a dead image
         const auto &subres_map = layout_map_entry.second;
-        const auto &initial_layout_map = subres_map->GetInitialLayoutMap();
+        const auto &layout_map = subres_map->GetLayoutMap();
         // Validate the initial_uses for each subresource referenced
-        if (initial_layout_map.empty()) continue;
+        if (layout_map.empty()) continue;
 
         auto *overlay_map = GetLayoutRangeMap(overlayLayoutMap, *image_state);
         const auto *global_map = GetLayoutRangeMap(globalImageLayoutMap, image);
@@ -4476,13 +4495,19 @@ bool CoreChecks::ValidateCmdBufImageLayouts(const CMD_BUFFER_STATE *pCB, const G
         // Note: don't know if it would matter
         // if (global_map->empty() && overlay_map->empty()) // skip this next loop...;
 
-        auto pos = initial_layout_map.begin();
-        const auto end = initial_layout_map.end();
-        sparse_container::parallel_iterator<const ImageSubresourceLayoutMap::LayoutMap> current_layout(*overlay_map, *global_map,
-                                                                                                       pos->first.begin);
+        auto pos = layout_map.begin();
+        const auto end = layout_map.end();
+        sparse_container::parallel_iterator<const GlobalImageLayoutRangeMap> current_layout(*overlay_map, *global_map,
+                                                                                            pos->first.begin);
         while (pos != end) {
-            VkImageLayout initial_layout = pos->second;
+            VkImageLayout initial_layout = pos->second.initial_layout;
+            assert(initial_layout != image_layout_map::kInvalidLayout);
+            if (initial_layout == image_layout_map::kInvalidLayout) {
+                continue;
+            }
+
             VkImageLayout image_layout = kInvalidLayout;
+
             if (current_layout->range.empty()) break;  // When we are past the end of data in overlay and global... stop looking
             if (current_layout->pos_A->valid) {        // pos_A denotes the overlay map in the parallel iterator
                 image_layout = current_layout->pos_A->lower_bound->second;
@@ -4519,10 +4544,8 @@ bool CoreChecks::ValidateCmdBufImageLayouts(const CMD_BUFFER_STATE *pCB, const G
                 }
             }
         }
-
         // Update all layout set operations (which will be a subset of the initial_layouts)
-        sparse_container::splice(*overlay_map, subres_map->GetCurrentLayoutMap(),
-                                 sparse_container::value_precedence::prefer_source);
+        sparse_container::splice(*overlay_map, subres_map->GetLayoutMap(), GlobalLayoutUpdater());
     }
 
     return skip;
@@ -4535,7 +4558,7 @@ void CoreChecks::UpdateCmdBufImageLayouts(CMD_BUFFER_STATE *pCB) {
         const auto *image_state = GetImageState(image);
         if (!image_state) continue;  // Can't set layouts of a dead image
         auto *global_map = GetLayoutRangeMap(imageLayoutMap, *image_state);
-        sparse_container::splice(*global_map, subres_map->GetCurrentLayoutMap(), sparse_container::value_precedence::prefer_source);
+        sparse_container::splice(*global_map, subres_map->GetLayoutMap(), GlobalLayoutUpdater());
     }
 }
 

layers/core_validation.cpp
@@ -11261,11 +11261,11 @@ bool CoreChecks::PreCallValidateCmdExecuteCommands(VkCommandBuffer commandBuffer
                 if (VK_IMAGE_LAYOUT_UNDEFINED == sub_layout) continue;  // secondary doesn't care about current or initial
 
                 // Look up the layout to compared to the intial layout of the sub command buffer (current else initial)
-                auto cb_layouts = cb_subres_map->GetSubresourceLayouts(subresource);
-                auto cb_layout = cb_layouts.current_layout;
+                const auto *cb_layouts = cb_subres_map->GetSubresourceLayouts(subresource);
+                auto cb_layout = cb_layouts ? cb_layouts->current_layout : kInvalidLayout;
                 const char *layout_type = "current";
-                if (cb_layouts.current_layout == kInvalidLayout) {
-                    cb_layout = cb_layouts.initial_layout;
+                if (cb_layout == kInvalidLayout) {
+                    cb_layout = cb_layouts ? cb_layouts->initial_layout : kInvalidLayout;
                     layout_type = "initial";
                 }
                 if ((cb_layout != kInvalidLayout) && (cb_layout != sub_layout)) {

layers/core_validation_types.h
@@ -1253,7 +1253,7 @@ struct QFOTransferCBScoreboards {
 
 typedef std::map<QueryObject, QueryState> QueryMap;
 typedef layer_data::unordered_map<VkEvent, VkPipelineStageFlags2KHR> EventToStageMap;
-typedef ImageSubresourceLayoutMap::LayoutMap GlobalImageLayoutRangeMap;
+typedef subresource_adapter::BothRangeMap<VkImageLayout, 16> GlobalImageLayoutRangeMap;
 typedef layer_data::unordered_map<VkImage, layer_data::optional<GlobalImageLayoutRangeMap>> GlobalImageLayoutMap;
 
 typedef layer_data::unordered_map<VkImage, layer_data::optional<ImageSubresourceLayoutMap>> CommandBufferImageLayoutMap;

commit 37028e0ff0ffa8a1b24844100854f79cb23eccc4

layers: Fix Image Layout range splitting
Commit 5363130b introduced a bug in Image Layout Transition
validation that would cause false postive errors if a program
did multiple updates to a subresource range with different layout
values. Fix this by updating the correct ranges when changing
an existing LayoutEntry.

layers/image_layout_map.cpp
@@ -62,12 +62,18 @@ static bool UpdateLayoutStateImpl(LayoutsMap& layouts, InitialLayoutStates& init
         }
         // Note that after the "fill" operation pos may have become valid so we check again
         if (pos->valid) {
-            LayoutEntry& orig_entry = pos->lower_bound->second;
-            assert(orig_entry.state != nullptr);
-            updated_current |= orig_entry.Update(new_entry);
-            // Point just past the end of this section,  if it's within the given range, it will get filled next iteration
-            // ++pos could move us past the end of range (which would exit the loop) so we don't use it.
-            pos.seek(pos->lower_bound->first.end);
+            if (pos->lower_bound->second.CurrentWillChange(new_entry.current_layout)) {
+                LayoutEntry orig_entry = pos->lower_bound->second; //intentional copy
+                assert(orig_entry.state != nullptr);
+                updated_current |= orig_entry.Update(new_entry);
+
+                layouts.overwrite_range(std::make_pair(range, orig_entry));
+                break;
+            } else {
+                // Point just past the end of this section,  if it's within the given range, it will get filled next iteration
+                // ++pos could move us past the end of range (which would exit the loop) so we don't use it.
+                pos.seek(pos->lower_bound->first.end);
+            }
         }
     }
 

layers/image_layout_map.h
@@ -86,10 +86,13 @@ class ImageSubresourceLayoutMap {
         bool operator!=(const LayoutEntry& rhs) const {
             return initial_layout != rhs.initial_layout || current_layout != rhs.current_layout || state != rhs.state;
         }
+        bool CurrentWillChange(VkImageLayout new_layout) const {
+            return new_layout != kInvalidLayout && current_layout != new_layout;
+        }
         bool Update(const LayoutEntry& src) {
             bool updated_current = false;
             // current_layout can be updated repeatedly.
-            if (current_layout != src.current_layout && src.current_layout != kInvalidLayout) {
+            if (CurrentWillChange(src.current_layout)) {
                 current_layout = src.current_layout;
                 updated_current = true;
             }