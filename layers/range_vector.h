KhronosGroup/Vulkan-ValidationLayers

commit a72a0b1dac7c0e6028dcf6a15c804efa303d3760

layers: Add a more flexible version of sparse_container::splice
Add a version of splice that can handle translating between different
map types by taking an update function to allow callers to specify
how each value is handled.

Change-Id: Ia0530edf105a317008fe1c7c4c0beada72f87c17

layers/range_vector.h
@@ -1691,15 +1691,16 @@ class parallel_iterator {
     const value_type *operator->() const { return &pos_; }
 };
 
-template <typename RangeMap, typename SourceIterator = typename RangeMap::const_iterator>
-bool splice(RangeMap *to, const RangeMap &from, value_precedence arbiter, SourceIterator begin, SourceIterator end) {
+template <typename DstRangeMap, typename SrcRangeMap, typename Updater,
+          typename SourceIterator = typename SrcRangeMap::const_iterator>
+bool splice(DstRangeMap &to, const SrcRangeMap &from, SourceIterator begin, SourceIterator end, const Updater &updater) {
     if (from.empty() || (begin == end) || (begin == from.cend())) return false;  // nothing to merge.
 
-    using ParallelIterator = parallel_iterator<RangeMap, const RangeMap>;
-    using Key = typename RangeMap::key_type;
-    using CachedLowerBound = cached_lower_bound_impl<RangeMap>;
-    using ConstCachedLowerBound = cached_lower_bound_impl<const RangeMap>;
-    ParallelIterator par_it(*to, from, begin->first.begin);
+    using ParallelIterator = parallel_iterator<DstRangeMap, const SrcRangeMap>;
+    using Key = typename SrcRangeMap::key_type;
+    using CachedLowerBound = cached_lower_bound_impl<DstRangeMap>;
+    using ConstCachedLowerBound = cached_lower_bound_impl<const SrcRangeMap>;
+    ParallelIterator par_it(to, from, begin->first.begin);
     bool updated = false;
     while (par_it->range.non_empty() && par_it->pos_B->lower_bound != end) {
         const Key &range = par_it->range;
@@ -1711,24 +1712,15 @@ bool splice(RangeMap *to, const RangeMap &from, value_precedence arbiter, Source
             // Because of how the parallel iterator walk, "to" is valid over the whole range or it isn't (ranges don't span
             // transitions between map entries or between valid and invalid ranges)
             if (to_lb->valid) {
-                // Only rewrite this range if source is preferred (and the value differs)
-                // TODO determine if equality checks are always wanted. (for example heavyweight values)
-                if (arbiter == value_precedence::prefer_source && (write_it->second != read_it->second)) {
-                    // Both ranges occupied and source is preferred and from differs from to
-                    if (write_it->first == range) {
-                        // we're writing the whole destination range, so just set the value
-                        write_it->second = read_it->second;
-                    } else {
-                        to->overwrite_range(write_it, std::make_pair(range, read_it->second));
-                        par_it.invalidate_A();  // we've changed map 'to' behind to_lb's back... let it know.
-                    }
-                    updated = true;
-                }
+                updated |= updater.update(write_it->second, read_it->second);
             } else {
                 // Insert into the gap.
-                to->insert(write_it, std::make_pair(range, read_it->second));
-                par_it.invalidate_A();  // we've changed map 'to' behind to_lb's back... let it know.
-                updated = true;
+                auto opt = updater.insert(read_it->second);
+                if (opt) {
+                    to.insert(write_it, std::make_pair(range, std::move(*opt)));
+                    updated = true;
+                    par_it.invalidate_A();  // we've changed map 'to' behind to_lb's back... let it know.
+                }
             }
         }
         ++par_it;  // next range over which both 'to' and 'from' stay constant
@@ -1736,8 +1728,43 @@ bool splice(RangeMap *to, const RangeMap &from, value_precedence arbiter, Source
     return updated;
 }
 // And short hand for "from begin to end"
+template <typename DstRangeMap, typename SrcRangeMap, typename Updater>
+bool splice(DstRangeMap &to, const SrcRangeMap &from, const Updater &updater) {
+    return splice(to, from, from.cbegin(), from.cend(), updater);
+}
+
+template <typename T>
+struct update_prefer_source {
+    bool update(T &dst, const T &src) const {
+        if (dst != src) {
+            dst = src;
+            return true;
+        }
+        return false;
+    }
+
+    layer_data::optional<T> insert(const T &src) const { return layer_data::optional<T>(layer_data::in_place, src); }
+};
+
+template <typename T>
+struct update_prefer_dest {
+    bool update(T &dst, const T &src) const { return false; }
+
+    layer_data::optional<T> insert(const T &src) const { return layer_data::optional<T>(layer_data::in_place, src); }
+};
+
+template <typename RangeMap, typename SourceIterator = typename RangeMap::const_iterator>
+bool splice(RangeMap &to, const RangeMap &from, value_precedence arbiter, SourceIterator begin, SourceIterator end) {
+    if (arbiter == value_precedence::prefer_source) {
+        return splice(to, from, from.cbegin(), from.cend(), update_prefer_source<typename RangeMap::mapped_type>());
+    } else {
+        return splice(to, from, from.cbegin(), from.cend(), update_prefer_dest<typename RangeMap::mapped_type>());
+    }
+}
+
+// And short hand for "from begin to end"
 template <typename RangeMap>
-bool splice(RangeMap *to, const RangeMap &from, value_precedence arbiter) {
+bool splice(RangeMap &to, const RangeMap &from, value_precedence arbiter) {
     return splice(to, from, arbiter, from.cbegin(), from.cend());
 }
 

layers/buffer_validation.cpp
@@ -4521,7 +4521,8 @@ bool CoreChecks::ValidateCmdBufImageLayouts(const CMD_BUFFER_STATE *pCB, const G
         }
 
         // Update all layout set operations (which will be a subset of the initial_layouts)
-        sparse_container::splice(overlay_map, subres_map->GetCurrentLayoutMap(), sparse_container::value_precedence::prefer_source);
+        sparse_container::splice(*overlay_map, subres_map->GetCurrentLayoutMap(),
+                                 sparse_container::value_precedence::prefer_source);
     }
 
     return skip;
@@ -4534,7 +4535,7 @@ void CoreChecks::UpdateCmdBufImageLayouts(CMD_BUFFER_STATE *pCB) {
         const auto *image_state = GetImageState(image);
         if (!image_state) continue;  // Can't set layouts of a dead image
         auto *global_map = GetLayoutRangeMap(imageLayoutMap, *image_state);
-        sparse_container::splice(global_map, subres_map->GetCurrentLayoutMap(), sparse_container::value_precedence::prefer_source);
+        sparse_container::splice(*global_map, subres_map->GetCurrentLayoutMap(), sparse_container::value_precedence::prefer_source);
     }
 }
 

layers/image_layout_map.cpp
@@ -229,13 +229,13 @@ bool ImageSubresourceLayoutMap::UpdateFrom(const ImageSubresourceLayoutMap& othe
     if (CompatibilityKey() != other.CompatibilityKey()) return false;
 
     bool updated = false;
-    updated |= sparse_container::splice(&layouts_.initial, other.layouts_.initial, Arbiter::prefer_dest);
-    updated |= sparse_container::splice(&layouts_.current, other.layouts_.current, Arbiter::prefer_source);
+    updated |= sparse_container::splice(layouts_.initial, other.layouts_.initial, Arbiter::prefer_dest);
+    updated |= sparse_container::splice(layouts_.current, other.layouts_.current, Arbiter::prefer_source);
     // NOTE -- we are copying plain pointers from 'other' which owns them as unique_ptr.  This works because
     //         currently this function is only used to import from secondary command buffers, destruction of which
     //         invalidate the referencing primary command buffer, meaning that the dangling pointer will either be
     //         cleaned up in invalidation, on not referenced by validation code.
-    sparse_container::splice(&initial_layout_state_map_, other.initial_layout_state_map_, Arbiter::prefer_dest);
+    sparse_container::splice(initial_layout_state_map_, other.initial_layout_state_map_, Arbiter::prefer_dest);
 
     return updated;
 }

commit 07e4cd53b46e7c598a0b92da18c19a3aae99d0aa

layers: Fix sparse_container::splice()
Commit a72a0b1d introduced a bug into splice where it
incorrectly splits existing ranges in some cases.

Fixes #2793

layers/range_vector.h
@@ -1713,7 +1713,16 @@ bool splice(DstRangeMap &to, const SrcRangeMap &from, SourceIterator begin, Sour
             // Because of how the parallel iterator walk, "to" is valid over the whole range or it isn't (ranges don't span
             // transitions between map entries or between valid and invalid ranges)
             if (to_lb->valid) {
-                updated |= updater.update(write_it->second, read_it->second);
+                if (write_it->first == range) {
+                    // if the source and destination ranges match we can overwrite everything
+                    updated |= updater.update(write_it->second, read_it->second);
+                } else {
+                    // otherwise we need to split the destination range.
+                    auto value_to_update = write_it->second; // intentional copy
+                    updated |= updater.update(value_to_update, read_it->second);
+                    to.overwrite_range(to_lb->lower_bound, std::make_pair(range, value_to_update));
+                    par_it.invalidate_A();  // we've changed map 'to' behind to_lb's back... let it know.
+                }
             } else {
                 // Insert into the gap.
                 auto opt = updater.insert(read_it->second);