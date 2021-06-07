5878485e0ec2d88fe1f74288b897cab2b29ccead
Tony-LunarG <tony@lunarg.com>
2021-06-07 11:47:03 -0600

    tests: Add clear value for each attachment
    Change-Id: I9a6ec5eaa9cf6a6c2b0d07eccb59335ed0839b7d

--- tests/vksyncvaltests.cpp
+++ tests/vksyncvaltests.cpp
@@ -2193,6 +2193,10 @@ TEST_F(VkSyncValTest, SyncRenderPassWithWrongDepthStencilInitialLayout) {
     ASSERT_VK_SUCCESS(g_pipe.CreateGraphicsPipeline());
 
     m_commandBuffer->begin();
+    VkClearValue clear = {};
+    std::array<VkClearValue, 2> clear_values = { {clear, clear} };
+    m_renderPassBeginInfo.pClearValues = clear_values.data();
+    m_renderPassBeginInfo.clearValueCount = clear_values.size();
     m_renderPassBeginInfo.renderArea = {{0, 0}, {32, 32}};
     m_renderPassBeginInfo.renderPass = rp;
 
@@ -2787,7 +2791,7 @@ TEST_F(VkSyncValTest, RenderPassAsyncHazard) {
 
     VkClearValue clear = {};
     clear.color = m_clear_color;
-    std::array<VkClearValue, 3> clear_values = {{clear, clear, clear}};
+    std::array<VkClearValue, 4> clear_values = {{clear, clear, clear, clear}};
 
     // run the renderpass with no dependencies
     {
