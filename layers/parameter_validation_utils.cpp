519dfaad52c583cff39dceaaa606e2e097a0c283
ziga-lunarg <ziga@lunarg.com>
2021-07-18 23:51:57 +0200

    layers: Validate createComputePipeline flags

--- layers/parameter_validation_utils.cpp
+++ layers/parameter_validation_utils.cpp
@@ -3200,6 +3200,12 @@ bool StatelessValidation::manual_PreCallValidateCreateComputePipelines(VkDevice 
                              "VK_PIPELINE_CREATE_RAY_TRACING_SHADER_GROUP_HANDLE_CAPTURE_REPLAY_BIT_KHR.",
                              i, flags);
         }
+        if ((flags & VK_PIPELINE_CREATE_RAY_TRACING_ALLOW_MOTION_BIT_NV) != 0) {
+            skip |= LogError(device, "VUID-VkComputePipelineCreateInfo-flags-04945",
+                             "vkCreateComputePipelines(): pCreateInfos[%u]->flags (0x%x) must not include "
+                             "VK_PIPELINE_CREATE_RAY_TRACING_ALLOW_MOTION_BIT_NV.",
+                             i, flags);
+        }
         if ((flags & VK_PIPELINE_CREATE_INDIRECT_BINDABLE_BIT_NV) != 0) {
             skip |= LogError(device, "VUID-VkComputePipelineCreateInfo-flags-02874",
                              "vkCreateComputePipelines(): pCreateInfos[%u]->flags (0x%x) must not include "
