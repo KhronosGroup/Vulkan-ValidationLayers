175aafa47b22216325b83bbb23707643262cd8fd
sfricke-samsung <s.fricke@samsung.com>
2021-08-29 22:16:34 -0700

    layers: Move VkIndexType from formats untils

--- layers/vk_layer_utils.h
+++ layers/vk_layer_utils.h
@@ -198,6 +198,21 @@ static inline bool IsIdentitySwizzle(VkComponentMapping components) {
     // clang-format on
 }
 
+static inline VkDeviceSize GetIndexAlignment(VkIndexType indexType) {
+    switch (indexType) {
+        case VK_INDEX_TYPE_UINT16:
+            return 2;
+        case VK_INDEX_TYPE_UINT32:
+            return 4;
+        case VK_INDEX_TYPE_UINT8_EXT:
+            return 1;
+        default:
+            // Not a real index type. Express no alignment requirement here; we expect upper layer
+            // to have already picked up on the enum being nonsense.
+            return 1;
+    }
+}
+
 extern "C" {
 #endif
 
