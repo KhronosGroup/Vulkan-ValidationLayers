af3457c96a18d1cf3d255873f902dbd544496605
Nathaniel Cesario <nathaniel@lunarg.com>
2021-10-05 11:28:02 -0600

    build: Experiment with find_package

--- cmake/fetch_content.cmake
+++ cmake/fetch_content.cmake
@@ -3,20 +3,18 @@ include(FetchContent)
 macro(FetchRepo name url tag)
     set(argn_ ${ARGN})
     list(LENGTH argn_ num_extra_args_)
-    if (num_extra_args_ EQUAL 2)
-        list(GET argn_ 0 source_dir)
-        list(GET argn_ 1 bin_dir)
-        message("Using local directory ${source_dir} as source directory and ${bin_dir} as binary directory for ${name}")
-        FetchContent_GetProperties(${name})
-        string(TOLOWER "${name}" lc_name_)
-        if (NOT ${lc_name_}_POPULATED)
-            FetchContent_Populate(${name}
-                SOURCE_DIR ${source_dir}
-                BINARY_DIR ${bin_dir}
-            )
-            add_subdirectory(${source_dir} ${bin_dir})
-        endif()
-    else()
+    set(need_fetch_ FALSE)
+    if (num_extra_args_ GREATER 0)
+        foreach (t ${argn_})
+            find_package(${t})
+            if (NOT ${t}_FOUND)
+                set(need_fetch_ TRUE)
+                break()
+            endif()
+        endforeach()
+    endif()
+
+    if (need_fetch_)
         message("Fetching ${name} @ ${tag} from ${url}")
         FetchContent_Declare(
             ${name}
