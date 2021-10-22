KhronosGroup/Vulkan-ValidationLayers

commit ee3a3a2298c38361301498d7c2eb715c98df48d5

layers: Set up PHYSICAL_DEVICE_STATE during vkCreateInstance()
Set up these state objects immediately rather than waiting for
the application to enumerate them. This ensures that the contents
of physical_device_map won't change during the lifetime of the
instance.

layers/state_tracker.cpp
@@ -3627,12 +3627,20 @@ std::shared_ptr<PHYSICAL_DEVICE_STATE> ValidationStateTracker::CreatePhysicalDev
     return std::make_shared<PHYSICAL_DEVICE_STATE>(phys_dev);
 }
 
-void ValidationStateTracker::PostCallRecordEnumeratePhysicalDevices(VkInstance instance, uint32_t *pPhysicalDeviceCount,
-                                                                    VkPhysicalDevice *pPhysicalDevices, VkResult result) {
-    if ((NULL != pPhysicalDevices) && ((result == VK_SUCCESS || result == VK_INCOMPLETE))) {
-        for (uint32_t i = 0; i < *pPhysicalDeviceCount; i++) {
-            physical_device_map.emplace(pPhysicalDevices[i], CreatePhysicalDeviceState(pPhysicalDevices[i]));
-        }
+void ValidationStateTracker::PostCallRecordCreateInstance(const VkInstanceCreateInfo *pCreateInfo,
+                                                          const VkAllocationCallbacks *pAllocator, VkInstance *pInstance,
+                                                          VkResult result) {
+    if (result != VK_SUCCESS) {
+        return;
+    }
+    uint32_t count = 0;
+    DispatchEnumeratePhysicalDevices(*pInstance, &count, nullptr);
+    std::vector<VkPhysicalDevice> physdev_handles(count);
+    DispatchEnumeratePhysicalDevices(*pInstance, &count, physdev_handles.data());
+
+    physical_device_map.reserve(count);
+    for (auto physdev : physdev_handles) {
+        physical_device_map.emplace(physdev, CreatePhysicalDeviceState(physdev));
     }
 }
 
@@ -3796,33 +3804,6 @@ void ValidationStateTracker::PreCallRecordCmdInsertDebugUtilsLabelEXT(VkCommandB
     cb_state->RecordCmd(CMD_INSERTDEBUGUTILSLABELEXT);
     // Squirrel away an easily accessible copy.
     cb_state->debug_label = LoggingLabel(pLabelInfo);
-}
-
-void ValidationStateTracker::RecordEnumeratePhysicalDeviceGroupsState(
-    uint32_t *pPhysicalDeviceGroupCount, VkPhysicalDeviceGroupProperties *pPhysicalDeviceGroupProperties) {
-    if (NULL != pPhysicalDeviceGroupProperties) {
-        for (uint32_t i = 0; i < *pPhysicalDeviceGroupCount; i++) {
-            for (uint32_t j = 0; j < pPhysicalDeviceGroupProperties[i].physicalDeviceCount; j++) {
-                VkPhysicalDevice cur_phys_dev = pPhysicalDeviceGroupProperties[i].physicalDevices[j];
-
-                physical_device_map.emplace(cur_phys_dev, CreatePhysicalDeviceState(cur_phys_dev));
-            }
-        }
-    }
-}
-
-void ValidationStateTracker::PostCallRecordEnumeratePhysicalDeviceGroups(
-    VkInstance instance, uint32_t *pPhysicalDeviceGroupCount, VkPhysicalDeviceGroupProperties *pPhysicalDeviceGroupProperties,
-    VkResult result) {
-    if ((VK_SUCCESS != result) && (VK_INCOMPLETE != result)) return;
-    RecordEnumeratePhysicalDeviceGroupsState(pPhysicalDeviceGroupCount, pPhysicalDeviceGroupProperties);
-}
-
-void ValidationStateTracker::PostCallRecordEnumeratePhysicalDeviceGroupsKHR(
-    VkInstance instance, uint32_t *pPhysicalDeviceGroupCount, VkPhysicalDeviceGroupProperties *pPhysicalDeviceGroupProperties,
-    VkResult result) {
-    if ((VK_SUCCESS != result) && (VK_INCOMPLETE != result)) return;
-    RecordEnumeratePhysicalDeviceGroupsState(pPhysicalDeviceGroupCount, pPhysicalDeviceGroupProperties);
 }
 
 void ValidationStateTracker::RecordEnumeratePhysicalDeviceQueueFamilyPerformanceQueryCounters(VkPhysicalDevice physicalDevice,

layers/state_tracker.h
@@ -529,14 +529,8 @@ class ValidationStateTracker : public ValidationObject {
     // State update functions
     // Gets/Enumerations
     virtual std::shared_ptr<PHYSICAL_DEVICE_STATE> CreatePhysicalDeviceState(VkPhysicalDevice phys_dev);
-    void PostCallRecordEnumeratePhysicalDeviceGroups(VkInstance instance, uint32_t* pPhysicalDeviceGroupCount,
-                                                     VkPhysicalDeviceGroupProperties* pPhysicalDeviceGroupProperties,
-                                                     VkResult result) override;
-    void PostCallRecordEnumeratePhysicalDeviceGroupsKHR(VkInstance instance, uint32_t* pPhysicalDeviceGroupCount,
-                                                        VkPhysicalDeviceGroupProperties* pPhysicalDeviceGroupProperties,
-                                                        VkResult result) override;
-    void PostCallRecordEnumeratePhysicalDevices(VkInstance instance, uint32_t* pPhysicalDeviceCount,
-                                                VkPhysicalDevice* pPhysicalDevices, VkResult result) override;
+    void PostCallRecordCreateInstance(const VkInstanceCreateInfo* pCreateInfo, const VkAllocationCallbacks* pAllocator,
+                                      VkInstance* pInstance, VkResult result) override;
     void PostCallRecordEnumeratePhysicalDeviceQueueFamilyPerformanceQueryCountersKHR(
         VkPhysicalDevice physicalDevice, uint32_t queueFamilyIndex, uint32_t* pCounterCount, VkPerformanceCounterKHR* pCounters,
         VkPerformanceCounterDescriptionKHR* pCounterDescriptions, VkResult result) override;

commit b6f9ef06da9510e570c6dcc6706cd23762b99c6d

layers: Handle allocation failures in vkCreateInstance()
This fixes a crash introduced by commit ee3a3a229 in the CTS case:
dEQP-VK.api.device_init.create_instance_device_intentional_alloc_fail

layers/state_tracker.cpp
@@ -3416,9 +3416,16 @@ void ValidationStateTracker::PostCallRecordCreateInstance(const VkInstanceCreate
         return;
     }
     uint32_t count = 0;
-    DispatchEnumeratePhysicalDevices(*pInstance, &count, nullptr);
+    // this can fail if the allocator fails
+    result = DispatchEnumeratePhysicalDevices(*pInstance, &count, nullptr);
+    if (result != VK_SUCCESS) {
+        return;
+    }
     std::vector<VkPhysicalDevice> physdev_handles(count);
-    DispatchEnumeratePhysicalDevices(*pInstance, &count, physdev_handles.data());
+    result = DispatchEnumeratePhysicalDevices(*pInstance, &count, physdev_handles.data());
+    if (result != VK_SUCCESS) {
+        return;
+    }
 
     physical_device_map.reserve(count);
     for (auto physdev : physdev_handles) {