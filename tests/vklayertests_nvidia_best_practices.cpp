43ef8acb859851922a259025fb8ef88346f95a10
Rodrigo Locatti <rlocatti@nvidia.com>
2022-03-14 17:01:14 -0300

    tests: Test PageableDeviceLocalMemory

--- tests/vklayertests_nvidia_best_practices.cpp
+++ tests/vklayertests_nvidia_best_practices.cpp
@@ -18,3 +18,49 @@
 
 // Tests for NVIDIA-specific best practices
 const char *kEnableNVIDIAValidation = "VALIDATION_CHECK_ENABLE_VENDOR_SPECIFIC_NVIDIA";
+
+static constexpr float defaultQueuePriority = 0.0f;
+
+TEST_F(VkNvidiaBestPracticesLayerTest, PageableDeviceLocalMemory) {
+    AddRequiredExtensions(VK_EXT_PAGEABLE_DEVICE_LOCAL_MEMORY_EXTENSION_NAME);
+    InitBestPracticesFramework(kEnableNVIDIAValidation);
+
+    if (!AreRequiredExtensionsEnabled()) {
+        GTEST_SKIP() << RequiredExtensionsNotSupported() << " not supported";
+    }
+
+    VkDeviceQueueCreateInfo queue_ci = LvlInitStruct<VkDeviceQueueCreateInfo>();
+    queue_ci.queueCount = 1;
+    queue_ci.pQueuePriorities = &defaultQueuePriority;
+
+    VkDeviceCreateInfo device_ci = LvlInitStruct<VkDeviceCreateInfo>();
+    device_ci.queueCreateInfoCount = 1;
+    device_ci.pQueueCreateInfos = &queue_ci;
+
+    VkPhysicalDevicePageableDeviceLocalMemoryFeaturesEXT pageable = LvlInitStruct<VkPhysicalDevicePageableDeviceLocalMemoryFeaturesEXT>();
+    pageable.pageableDeviceLocalMemory = VK_TRUE;
+
+    {
+        m_errorMonitor->SetDesiredFailureMsg(VK_DEBUG_REPORT_PERFORMANCE_WARNING_BIT_EXT,
+                                             "UNASSIGNED-BestPractices-CreateDevice-PageableDeviceLocalMemory");
+        VkDevice test_device = VK_NULL_HANDLE;
+        VkResult err = vk::CreateDevice(gpu(), &device_ci, nullptr, &test_device);
+        m_errorMonitor->VerifyFound();
+        if (err == VK_SUCCESS) {
+            vk::DestroyDevice(test_device, nullptr);
+        }
+    }
+
+    // Now enable the expected features
+    device_ci.enabledExtensionCount = m_device_extension_names.size();
+    device_ci.ppEnabledExtensionNames = m_device_extension_names.data();
+    device_ci.pNext = &pageable;
+
+    {
+        m_errorMonitor->SetDesiredFailureMsg(VK_DEBUG_REPORT_PERFORMANCE_WARNING_BIT_EXT,
+                                             "UNASSIGNED-BestPractices-CreateDevice-PageableDeviceLocalMemory");
+        vk_testing::Device test_device(gpu());
+        test_device.init(device_ci);
+        m_errorMonitor->Finish();
+    }
+}
