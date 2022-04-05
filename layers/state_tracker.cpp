f85001094c131a9836c8a594a42fba25451078b6
Nathaniel Cesario <nathaniel@lunarg.com>
2022-04-05 13:47:44 -0600

    build: Build fixes after rebase

--- layers/state_tracker.cpp
+++ layers/state_tracker.cpp
@@ -2069,6 +2069,12 @@ bool ValidationStateTracker::PreCallValidateCreateGraphicsPipelines(VkDevice dev
         } else if (enabled_features.core13.dynamicRendering) {
             auto dynamic_rendering = LvlFindInChain<VkPipelineRenderingCreateInfo>(create_info.pNext);
             render_pass = std::make_shared<RENDER_PASS_STATE>(dynamic_rendering);
+        } else {
+            const bool is_graphics_lib = GetGraphicsLibType(create_info) != static_cast<VkGraphicsPipelineLibraryFlagsEXT>(0);
+            const bool has_link_info = LvlFindInChain<VkPipelineLibraryCreateInfoKHR>(create_info.pNext) != nullptr;
+            if (!is_graphics_lib && !has_link_info) {
+                skip = true;
+            }
         }
         cgpl_state->pipe_state.push_back(
             CreateGraphicsPipelineState(&create_info, std::move(render_pass), std::move(layout_state)));
