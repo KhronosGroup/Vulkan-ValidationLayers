c77f0c0196440a9700fb90580fecf4fb8d8e0afd
ziga-lunarg <ziga@lunarg.com>
2022-04-18 00:15:16 +0200

    layers: Warn when semaphore is set but count is 0

--- layers/best_practices_error_enums.h
+++ layers/best_practices_error_enums.h
@@ -123,6 +123,7 @@ static const char DECORATE_UNUSED *kVUID_BestPractices_SpirvDeprecated_Workgroup
 static const char DECORATE_UNUSED *kVUID_BestPractices_ImageCreateFlags = "UNASSIGNED-BestPractices-ImageCreateFlags";
 static const char DECORATE_UNUSED *kVUID_BestPractices_TransitionUndefinedToReadOnly =
     "UNASSIGNED-BestPractices-TransitionUndefinedToReadOnly";
+static const char DECORATE_UNUSED *kVUID_BestPractices_SemaphoreCount = "UNASSIGNED-BestPractices-SemaphoreCount";
 static const char DECORATE_UNUSED *kVUID_BestPractices_EmptyDescriptorPool =
     "UNASSIGNED-BestPractices-EmptyDescriptorPool";
 
