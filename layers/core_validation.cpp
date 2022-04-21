13327c3aaef495a28fd7df7569dc94972e0c1640
ziga-lunarg <ziga@lunarg.com>
2022-04-21 14:07:02 +0200

    layers: Validate secondary command buffer subpass

--- layers/core_validation.cpp
+++ layers/core_validation.cpp
@@ -14907,11 +14907,19 @@ bool CoreChecks::PreCallValidateCmdExecuteCommands(VkCommandBuffer commandBuffer
                     }
                 }
 
-                if (cb_state->activeRenderPass && !IsExtEnabled(device_extensions.vk_khr_dynamic_rendering) &&
-                    cb_state->activeRenderPass->renderPass() != secondary_rp_state->renderPass()) {
-                    skip |= ValidateRenderPassCompatibility(
-                        "primary command buffer", cb_state->activeRenderPass.get(), "secondary command buffer",
-                        secondary_rp_state.get(), "vkCmdExecuteCommands()", "VUID-vkCmdExecuteCommands-pInheritanceInfo-00098");
+                if (cb_state->activeRenderPass && !IsExtEnabled(device_extensions.vk_khr_dynamic_rendering)) {
+                    if (cb_state->activeRenderPass->renderPass() != secondary_rp_state->renderPass()) {
+                        skip |= ValidateRenderPassCompatibility(
+                            "primary command buffer", cb_state->activeRenderPass.get(), "secondary command buffer",
+                            secondary_rp_state.get(), "vkCmdExecuteCommands()", "VUID-vkCmdExecuteCommands-pInheritanceInfo-00098");
+                    }
+                    if (sub_cb_state->beginInfo.pInheritanceInfo->subpass != cb_state->activeSubpass) {
+                        skip |= LogError(pCommandBuffers[i], "VUID-vkCmdExecuteCommands-pCommandBuffers-00097",
+                                         "vkCmdExecuteCommands(): Secondary %s subpass %" PRIu32
+                                         " is different than the active subpass %" PRIu32 ".",
+                                         report_data->FormatHandle(pCommandBuffers[i]).c_str(),
+                                         sub_cb_state->beginInfo.pInheritanceInfo->subpass, cb_state->activeSubpass);
+                    }
                 }
 
                 if (cb_state->activeRenderPass && (cb_state->activeRenderPass->use_dynamic_rendering == false) &&
