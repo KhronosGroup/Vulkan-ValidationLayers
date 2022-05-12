21883f7fb218b2fd73a3f7d8a75f1354ab4c28d6
Jeremy Gebben <jeremyg@lunarg.com>
2022-05-12 08:51:10 -0600

    corechecks: Move validation code out of descriptor_sets.cpp
    Add descriptor_validation.cpp for the core validation code,
    descriptor_sets.cpp now only contains state object code.

--- build-android/jni/Android.mk
+++ build-android/jni/Android.mk
@@ -50,6 +50,7 @@ LOCAL_SRC_FILES += $(SRC_DIR)/layers/render_pass_state.cpp
 LOCAL_SRC_FILES += $(SRC_DIR)/layers/core_validation.cpp
 LOCAL_SRC_FILES += $(SRC_DIR)/layers/drawdispatch.cpp
 LOCAL_SRC_FILES += $(SRC_DIR)/layers/descriptor_sets.cpp
+LOCAL_SRC_FILES += $(SRC_DIR)/layers/descriptor_validation.cpp
 LOCAL_SRC_FILES += $(SRC_DIR)/layers/buffer_validation.cpp
 LOCAL_SRC_FILES += $(SRC_DIR)/layers/shader_module.cpp
 LOCAL_SRC_FILES += $(SRC_DIR)/layers/shader_validation.cpp
