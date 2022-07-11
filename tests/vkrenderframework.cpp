74268fe9776d4b84f1ad8c7418e43c195197c0bd
paul-lunarg <paul@lunarg.com>
2022-07-11 14:08:26 -0600

    tests: Fix VkLayerTest.SwapchainNotSupported
    Deleting surface if created

--- tests/vkrenderframework.cpp
+++ tests/vkrenderframework.cpp
@@ -702,6 +702,8 @@ void VkRenderFramework::ShutdownFramework() {
         DestroySwapchain();
     }
 
+    DestroySwapchain();
+
     // reset the driver
     delete m_device;
     m_device = nullptr;
