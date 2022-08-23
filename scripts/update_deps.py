ba031e3e5699e03a7c16e6c3e4967bc201dbb959
Karl Schultz <karl@lunarg.com>
2022-08-23 14:25:30 -0600

    build: Improve GitHub Actions caching
    - Extend update_deps.py to skip repos if install dir is present
    - Cache only build/install directories from dependent repos in the
      external directory
    
    This change keeps update_deps from fetching a repo on top of a cached
    repo and building an already built repo. (Previously, the entire
    external directory was cached.)
    
    It also significantly lowers the amount of data cached, allowing more
    caches (related to versions of known_good.json) to be saved.

--- scripts/update_deps.py
+++ scripts/update_deps.py
@@ -2,7 +2,7 @@
 
 # Copyright 2017 The Glslang Authors. All rights reserved.
 # Copyright (c) 2018 Valve Corporation
-# Copyright (c) 2018-2021 LunarG, Inc.
+# Copyright (c) 2018-2022 LunarG, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -652,6 +652,12 @@ def main():
         help="Delete install directory before building",
         default=False)
     parser.add_argument(
+        '--skip-existing-install',
+        dest='skip_existing_install',
+        action='store_true',
+        help="Skip build if install directory exists",
+        default=False)
+    parser.add_argument(
         '--arch',
         dest='arch',
         choices=['32', '64', 'x86', 'x64', 'win32', 'win64'],
@@ -692,6 +698,14 @@ def main():
         # If the repo has a platform whitelist, skip the repo
         # unless we are building on a whitelisted platform.
         if not repo.on_build_platform:
+            continue
+
+        # Skip building the repo if its install directory already exists
+        # and requested via an option.  This is useful for cases where the
+        # install directory is restored from a cache that is known to be up
+        # to date.
+        if args.skip_existing_install and os.path.isdir(repo.install_dir):
+            print('Skipping build for repo {n} due to existing install directory'.format(n=repo.name))
             continue
 
         # Skip test-only repos if the --tests option was not passed in
