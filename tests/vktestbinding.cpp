KhronosGroup/Vulkan-ValidationLayers

commit adf933c0d3f1ff2cffaefb21093176c0bd0af40a

tests: Cache instance layer and extension enumeration
Help speed up tests by calling the enumerate extension and layer functions
with an already provided buffer of adequate size, then storing the found
list in a vector for any subsequent queries.

tests/vktestbinding.cpp
@@ -111,22 +111,17 @@ VkPhysicalDeviceFeatures PhysicalDevice::features() const {
  */
 std::vector<VkLayerProperties> GetGlobalLayers() {
     VkResult err;
-    uint32_t layer_count;
-    std::vector<VkLayerProperties> layers;
-
+    uint32_t layer_count = 32;
+    std::vector<VkLayerProperties> layers(layer_count);
     do {
-        err = vk::EnumerateInstanceLayerProperties(&layer_count, nullptr);
-        assert(!err);
-        if (err || 0 == layer_count) return {};
-
-        layers.resize(layer_count);
         err = vk::EnumerateInstanceLayerProperties(&layer_count, layers.data());
+        if (err || 0 == layer_count) return {};
+        if (err == VK_INCOMPLETE) layer_count *= 2; // wasn't enough space, increase it
+        layers.resize(layer_count);
+
     } while (VK_INCOMPLETE == err);
 
     assert(!err);
-    if (err) return {};
-    layers.resize(layer_count);
-
     return layers;
 }
 
@@ -142,22 +137,16 @@ std::vector<VkExtensionProperties> GetGlobalExtensions() { return GetGlobalExten
  */
 std::vector<VkExtensionProperties> GetGlobalExtensions(const char *pLayerName) {
     VkResult err;
-    uint32_t extension_count;
-    std::vector<VkExtensionProperties> extensions;
-
+    uint32_t extension_count = 32;
+    std::vector<VkExtensionProperties> extensions(extension_count);
     do {
-        err = vk::EnumerateInstanceExtensionProperties(nullptr, &extension_count, nullptr);
-        assert(!err);
-        if (err || 0 == extension_count) return {};
-
-        extensions.resize(extension_count);
         err = vk::EnumerateInstanceExtensionProperties(nullptr, &extension_count, extensions.data());
+        if (err || 0 == extension_count) return {};
+        if (err == VK_INCOMPLETE) extension_count *= 2; // wasn't enough space, increase it
+        extensions.resize(extension_count);
     } while (VK_INCOMPLETE == err);
 
     assert(!err);
-    if (err) return {};
-    extensions.resize(extension_count);
-
     return extensions;
 }
 
@@ -167,18 +156,14 @@ std::vector<VkExtensionProperties> GetGlobalExtensions(const char *pLayerName) {
  */
 std::vector<VkExtensionProperties> PhysicalDevice::extensions(const char *pLayerName) const {
     VkResult err;
-    uint32_t extension_count;
-    std::vector<VkExtensionProperties> extensions;
+    uint32_t extension_count = 256;
+    std::vector<VkExtensionProperties> extensions(extension_count);
     do {
-        err = vk::EnumerateDeviceExtensionProperties(handle(), pLayerName, &extension_count, nullptr);
-        if (err || 0 == extension_count) return {};
-
-        extensions.resize(extension_count);
         err = vk::EnumerateDeviceExtensionProperties(handle(), pLayerName, &extension_count, extensions.data());
+        if (err || 0 == extension_count) return {};
+        if (err == VK_INCOMPLETE) extension_count *= 2; // wasn't enough space, increase it
+        extensions.resize(extension_count);
     } while (VK_INCOMPLETE == err);
-
-    if (err) return {};
-    extensions.resize(extension_count);
 
     return extensions;
 }
@@ -207,18 +192,14 @@ bool PhysicalDevice::set_memory_type(const uint32_t type_bits, VkMemoryAllocateI
  */
 std::vector<VkLayerProperties> PhysicalDevice::layers() const {
     VkResult err;
-    uint32_t layer_count;
-    std::vector<VkLayerProperties> layers;
+    uint32_t layer_count = 32;
+    std::vector<VkLayerProperties> layers(layer_count);
     do {
-        err = vk::EnumerateDeviceLayerProperties(handle(), &layer_count, nullptr);
-        if (err || 0 == layer_count) return {};
-
-        layers.resize(layer_count);
         err = vk::EnumerateDeviceLayerProperties(handle(), &layer_count, layers.data());
+        if (err || 0 == layer_count) return {};
+        if (err == VK_INCOMPLETE) layer_count *= 2; // wasn't enough space, increase it
+        layers.resize(layer_count);
     } while (VK_INCOMPLETE == err);
-
-    if (err) return {};
-    layers.resize(layer_count);
 
     return layers;
 }

tests/vkrenderframework.cpp
@@ -330,9 +330,12 @@ VkPhysicalDeviceProperties VkRenderFramework::physDevProps() {
 // Return true if layer name is found and spec+implementation values are >= requested values
 bool VkRenderFramework::InstanceLayerSupported(const char *const layer_name, const uint32_t spec_version,
                                                const uint32_t impl_version) {
-    const auto layers = vk_testing::GetGlobalLayers();
 
-    for (const auto &layer : layers) {
+    if (available_layers_.empty()) {
+        available_layers_ = vk_testing::GetGlobalLayers();
+    }
+
+    for (const auto &layer : available_layers_) {
         if (0 == strncmp(layer_name, layer.layerName, VK_MAX_EXTENSION_NAME_SIZE)) {
             return layer.specVersion >= spec_version && layer.implementationVersion >= impl_version;
         }
@@ -348,14 +351,16 @@ bool VkRenderFramework::InstanceExtensionSupported(const char *const extension_n
     if (0 == strncmp(extension_name, VK_EXT_DEBUG_REPORT_EXTENSION_NAME, VK_MAX_EXTENSION_NAME_SIZE)) return true;
     if (0 == strncmp(extension_name, VK_EXT_VALIDATION_FEATURES_EXTENSION_NAME, VK_MAX_EXTENSION_NAME_SIZE)) return true;
 
-    const auto extensions = vk_testing::GetGlobalExtensions();
+    if (available_extensions_.empty()) {
+        available_extensions_ = vk_testing::GetGlobalExtensions();
+    }
 
     const auto IsTheQueriedExtension = [extension_name, spec_version](const VkExtensionProperties &extension) {
         return strncmp(extension_name, extension.extensionName, VK_MAX_EXTENSION_NAME_SIZE) == 0 &&
                extension.specVersion >= spec_version;
     };
 
-    return std::any_of(extensions.begin(), extensions.end(), IsTheQueriedExtension);
+    return std::any_of(available_extensions_.begin(), available_extensions_.end(), IsTheQueriedExtension);
 }
 
 // Enable device profile as last layer on stack overriding devsim if there, or return if not available
@@ -440,7 +445,7 @@ VkInstanceCreateInfo VkRenderFramework::GetInstanceCreateInfo() const {
 void VkRenderFramework::InitFramework(void * /*unused compatibility parameter*/, void *instance_pnext) {
     ASSERT_EQ((VkInstance)0, instance_);
 
-    const auto LayerNotSupportedWithReporting = [](const char *layer) {
+    const auto LayerNotSupportedWithReporting = [this](const char *layer) {
         if (InstanceLayerSupported(layer))
             return false;
         else {
@@ -448,7 +453,7 @@ void VkRenderFramework::InitFramework(void * /*unused compatibility parameter*/,
             return true;
         }
     };
-    const auto ExtensionNotSupportedWithReporting = [](const char *extension) {
+    const auto ExtensionNotSupportedWithReporting = [this](const char *extension) {
         if (InstanceExtensionSupported(extension))
             return false;
         else {

tests/vkrenderframework.h
@@ -252,8 +252,8 @@ class VkRenderFramework : public VkTestFramework {
     ErrorMonitor &Monitor();
     VkPhysicalDeviceProperties physDevProps();
 
-    static bool InstanceLayerSupported(const char *layer_name, uint32_t spec_version = 0, uint32_t impl_version = 0);
-    static bool InstanceExtensionSupported(const char *extension_name, uint32_t spec_version = 0);
+    bool InstanceLayerSupported(const char *layer_name, uint32_t spec_version = 0, uint32_t impl_version = 0);
+    bool InstanceExtensionSupported(const char *extension_name, uint32_t spec_version = 0);
 
     VkInstanceCreateInfo GetInstanceCreateInfo() const;
     void InitFramework(void * /*unused compatibility parameter*/ = NULL, void *instance_pnext = NULL);
@@ -324,6 +324,9 @@ class VkRenderFramework : public VkTestFramework {
   protected:
     VkRenderFramework();
     virtual ~VkRenderFramework() = 0;
+
+    std::vector<VkLayerProperties> available_layers_; // allow caching of available layers
+    std::vector<VkExtensionProperties> available_extensions_; // allow caching of available instance extensions
 
     DebugReporter debug_reporter_;
     ErrorMonitor *m_errorMonitor = &debug_reporter_.error_monitor_;  // compatibility alias name

commit 87af67585fe8da2aade8025b923b40646c405cb4

tests: Fix layer/extension enumeration
adf933c0d3f1ff2cffaefb21093176c0bd0af40a broke extension enumeration by
returning {} for VK_INCOMPLETE. Found because header 1.3.226 pushed the
device extension count > 256.

tests/vktestbinding.cpp
@@ -115,16 +115,18 @@ std::vector<VkLayerProperties> GetGlobalLayers() {
     VkResult err;
     uint32_t layer_count = 32;
     std::vector<VkLayerProperties> layers(layer_count);
-    do {
+    for (;;) {
         err = vk::EnumerateInstanceLayerProperties(&layer_count, layers.data());
-        if (err || 0 == layer_count) return {};
-        if (err == VK_INCOMPLETE) layer_count *= 2; // wasn't enough space, increase it
-        layers.resize(layer_count);
-
-    } while (VK_INCOMPLETE == err);
-
-    assert(!err);
-    return layers;
+        if (err == VK_SUCCESS) {
+            layers.resize(layer_count);
+            return layers;
+        } else if (err == VK_INCOMPLETE) {
+            layer_count *= 2;  // wasn't enough space, increase it
+            layers.resize(layer_count);
+        } else {
+            return {};
+        }
+    }
 }
 
 /*
@@ -141,15 +143,18 @@ std::vector<VkExtensionProperties> GetGlobalExtensions(const char *pLayerName) {
     VkResult err;
     uint32_t extension_count = 32;
     std::vector<VkExtensionProperties> extensions(extension_count);
-    do {
+    for (;;) {
         err = vk::EnumerateInstanceExtensionProperties(nullptr, &extension_count, extensions.data());
-        if (err || 0 == extension_count) return {};
-        if (err == VK_INCOMPLETE) extension_count *= 2; // wasn't enough space, increase it
-        extensions.resize(extension_count);
-    } while (VK_INCOMPLETE == err);
-
-    assert(!err);
-    return extensions;
+        if (err == VK_SUCCESS) {
+            extensions.resize(extension_count);
+            return extensions;
+        } else if (err == VK_INCOMPLETE) {
+            extension_count *= 2;  // wasn't enough space, increase it
+            extensions.resize(extension_count);
+        } else {
+            return {};
+        }
+    }
 }
 
 /*
@@ -160,14 +165,18 @@ std::vector<VkExtensionProperties> PhysicalDevice::extensions(const char *pLayer
     VkResult err;
     uint32_t extension_count = 256;
     std::vector<VkExtensionProperties> extensions(extension_count);
-    do {
+    for (;;) {
         err = vk::EnumerateDeviceExtensionProperties(handle(), pLayerName, &extension_count, extensions.data());
-        if (err || 0 == extension_count) return {};
-        if (err == VK_INCOMPLETE) extension_count *= 2; // wasn't enough space, increase it
-        extensions.resize(extension_count);
-    } while (VK_INCOMPLETE == err);
-
-    return extensions;
+        if (err == VK_SUCCESS) {
+            extensions.resize(extension_count);
+            return extensions;
+        } else if (err == VK_INCOMPLETE) {
+            extension_count *= 2;  // wasn't enough space, increase it
+            extensions.resize(extension_count);
+        } else {
+            return {};
+        }
+    }
 }
 
 bool PhysicalDevice::set_memory_type(const uint32_t type_bits, VkMemoryAllocateInfo *info, const VkFlags properties,
@@ -197,14 +206,18 @@ std::vector<VkLayerProperties> PhysicalDevice::layers() const {
     VkResult err;
     uint32_t layer_count = 32;
     std::vector<VkLayerProperties> layers(layer_count);
-    do {
+    for (;;) {
         err = vk::EnumerateDeviceLayerProperties(handle(), &layer_count, layers.data());
-        if (err || 0 == layer_count) return {};
-        if (err == VK_INCOMPLETE) layer_count *= 2; // wasn't enough space, increase it
-        layers.resize(layer_count);
-    } while (VK_INCOMPLETE == err);
-
-    return layers;
+        if (err == VK_SUCCESS) {
+            layers.resize(layer_count);
+            return layers;
+        } else if (err == VK_INCOMPLETE) {
+            layer_count *= 2;  // wasn't enough space, increase it
+            layers.resize(layer_count);
+        } else {
+            return {};
+        }
+    }
 }
 
 QueueCreateInfoArray::QueueCreateInfoArray(const std::vector<VkQueueFamilyProperties> &queue_props)