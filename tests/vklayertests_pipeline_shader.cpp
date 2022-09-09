4a2a06e78dbeabfcb03810df98a5c5c7e3696e34
Jarredhdavies <jarred.davies@imgtec.com>
2022-09-09 07:38:44 +0100

    tests: Add tests for VK_EXT_pipeline_robustness VUIDs
    Tests for 06926 to 06934

--- tests/vklayertests_pipeline_shader.cpp
+++ tests/vklayertests_pipeline_shader.cpp
@@ -15856,7 +15856,7 @@ TEST_F(VkLayerTest, DeviceMemoryScope) {
         layout(set = 0, binding = 0) buffer ssbo { uint y; };
         void main() {
             atomicStore(y, 1u, gl_ScopeDevice, gl_StorageSemanticsBuffer, gl_SemanticsRelaxed);
-	   }
+       }
     )glsl";
 
     const auto set_info = [&](CreateComputePipelineHelper &helper) {
@@ -15889,7 +15889,7 @@ TEST_F(VkLayerTest, QueueFamilyMemoryScope) {
         layout(set = 0, binding = 0) buffer ssbo { uint y; };
         void main() {
             atomicStore(y, 1u, gl_ScopeQueueFamily, gl_StorageSemanticsBuffer, gl_SemanticsRelaxed);
-	   }
+       }
     )glsl";
 
     const auto set_info = [&](CreateComputePipelineHelper &helper) {
@@ -17890,4 +17890,227 @@ TEST_F(VkLayerTest, StorageTexelBufferWriteLessComponent) {
         helper.dsl_bindings_ = {{0, VK_DESCRIPTOR_TYPE_STORAGE_TEXEL_BUFFER, 1, VK_SHADER_STAGE_COMPUTE_BIT, nullptr}};
     };
     CreateComputePipelineHelper::OneshotTest(*this, set_info, kErrorBit, "VUID-RuntimeSpirv-OpImageWrite-07112");
+}
+
+TEST_F(VkLayerTest, PipelineRobustnessDisabled) {
+    TEST_DESCRIPTION("Create a pipeline using VK_EXT_pipeline_robustness but with pipelineRobustness == VK_FALSE");
+
+    AddRequiredExtensions(VK_EXT_PIPELINE_ROBUSTNESS_EXTENSION_NAME);
+    SetTargetApiVersion(VK_API_VERSION_1_1);
+    ASSERT_NO_FATAL_FAILURE(InitFramework());
+    if (DeviceValidationVersion() < VK_API_VERSION_1_1) {
+        GTEST_SKIP() << "At least Vulkan version 1.1 is required";
+    }
+    if (!AreRequiredExtensionsEnabled()) {
+        GTEST_SKIP() << RequiredExtensionsNotSupported() << " not supported";
+    }
+    ASSERT_NO_FATAL_FAILURE(InitState());
+
+    CreateComputePipelineHelper pipe(*this);
+
+    VkPipelineRobustnessCreateInfoEXT pipeline_robustness_info;
+
+    pipe.InitInfo();
+    pipe.InitState();
+    pipeline_robustness_info = LvlInitStruct<VkPipelineRobustnessCreateInfoEXT>();
+    pipeline_robustness_info.storageBuffers = VK_PIPELINE_ROBUSTNESS_BUFFER_BEHAVIOR_ROBUST_BUFFER_ACCESS_2_EXT;
+    pipe.cp_ci_.pNext = &pipeline_robustness_info;
+
+    m_errorMonitor->SetDesiredFailureMsg(kErrorBit, "VUID-VkPipelineRobustnessCreateInfoEXT-pipelineRobustness-06926");
+    pipe.CreateComputePipeline();
+    m_errorMonitor->VerifyFound();
+
+    pipe.InitInfo();
+    pipe.InitState();
+    pipeline_robustness_info = LvlInitStruct<VkPipelineRobustnessCreateInfoEXT>();
+    pipeline_robustness_info.uniformBuffers = VK_PIPELINE_ROBUSTNESS_BUFFER_BEHAVIOR_ROBUST_BUFFER_ACCESS_2_EXT;
+    pipe.cp_ci_.pNext = &pipeline_robustness_info;
+
+    m_errorMonitor->SetDesiredFailureMsg(kErrorBit, "VUID-VkPipelineRobustnessCreateInfoEXT-pipelineRobustness-06927");
+    pipe.CreateComputePipeline();
+    m_errorMonitor->VerifyFound();
+
+    pipe.InitInfo();
+    pipe.InitState();
+    pipeline_robustness_info = LvlInitStruct<VkPipelineRobustnessCreateInfoEXT>();
+    pipeline_robustness_info.vertexInputs = VK_PIPELINE_ROBUSTNESS_BUFFER_BEHAVIOR_ROBUST_BUFFER_ACCESS_2_EXT;
+    pipe.cp_ci_.pNext = &pipeline_robustness_info;
+
+    m_errorMonitor->SetDesiredFailureMsg(kErrorBit, "VUID-VkPipelineRobustnessCreateInfoEXT-pipelineRobustness-06928");
+    pipe.CreateComputePipeline();
+    m_errorMonitor->VerifyFound();
+
+    pipe.InitInfo();
+    pipe.InitState();
+    pipeline_robustness_info = LvlInitStruct<VkPipelineRobustnessCreateInfoEXT>();
+    pipeline_robustness_info.images = VK_PIPELINE_ROBUSTNESS_IMAGE_BEHAVIOR_ROBUST_IMAGE_ACCESS_2_EXT;
+    pipe.cp_ci_.pNext = &pipeline_robustness_info;
+
+    m_errorMonitor->SetDesiredFailureMsg(kErrorBit, "VUID-VkPipelineRobustnessCreateInfoEXT-pipelineRobustness-06929");
+    pipe.CreateComputePipeline();
+    m_errorMonitor->VerifyFound();
+}
+
+TEST_F(VkLayerTest, PipelineRobustnessRobustBufferAccess2Unsupported) {
+    TEST_DESCRIPTION("Create a pipeline using VK_EXT_pipeline_robustness with robustBufferAccess2 being unsupported");
+
+    AddRequiredExtensions(VK_EXT_PIPELINE_ROBUSTNESS_EXTENSION_NAME);
+    AddOptionalExtensions(VK_EXT_ROBUSTNESS_2_EXTENSION_NAME);
+
+    SetTargetApiVersion(VK_API_VERSION_1_1);
+    ASSERT_NO_FATAL_FAILURE(InitFramework());
+    if (DeviceValidationVersion() < VK_API_VERSION_1_1) {
+        GTEST_SKIP() << "At least Vulkan version 1.1 is required";
+    }
+    if (!AreRequiredExtensionsEnabled()) {
+        GTEST_SKIP() << RequiredExtensionsNotSupported() << " not supported";
+    }
+
+    if (IsExtensionsEnabled(VK_EXT_ROBUSTNESS_2_EXTENSION_NAME)) {
+        auto robustness2_features = LvlInitStruct<VkPhysicalDeviceRobustness2FeaturesEXT>();
+        GetPhysicalDeviceFeatures2(robustness2_features);
+
+        if (robustness2_features.robustBufferAccess2) {
+            GTEST_SKIP() << "robustBufferAccess2 is supported";
+        }
+    }
+
+    auto pipeline_robustness_features = LvlInitStruct<VkPhysicalDevicePipelineRobustnessFeaturesEXT>();
+    auto features2 = GetPhysicalDeviceFeatures2(pipeline_robustness_features);
+
+    if (!pipeline_robustness_features.pipelineRobustness) {
+        GTEST_SKIP() << "pipelineRobustness is not supported";
+    }
+
+    ASSERT_NO_FATAL_FAILURE(InitState(nullptr, &features2));
+
+    CreateComputePipelineHelper pipe(*this);
+
+    VkPipelineRobustnessCreateInfoEXT pipeline_robustness_info;
+
+    pipe.InitInfo();
+    pipe.InitState();
+    pipeline_robustness_info = LvlInitStruct<VkPipelineRobustnessCreateInfoEXT>();
+    pipeline_robustness_info.storageBuffers = VK_PIPELINE_ROBUSTNESS_BUFFER_BEHAVIOR_ROBUST_BUFFER_ACCESS_2_EXT;
+    pipe.cp_ci_.pNext = &pipeline_robustness_info;
+
+    m_errorMonitor->SetDesiredFailureMsg(kErrorBit, "VUID-VkPipelineRobustnessCreateInfoEXT-robustBufferAccess2-06931");
+    pipe.CreateComputePipeline();
+    m_errorMonitor->VerifyFound();
+
+    pipe.InitInfo();
+    pipe.InitState();
+    pipeline_robustness_info = LvlInitStruct<VkPipelineRobustnessCreateInfoEXT>();
+    pipeline_robustness_info.uniformBuffers = VK_PIPELINE_ROBUSTNESS_BUFFER_BEHAVIOR_ROBUST_BUFFER_ACCESS_2_EXT;
+    pipe.cp_ci_.pNext = &pipeline_robustness_info;
+
+    m_errorMonitor->SetDesiredFailureMsg(kErrorBit, "VUID-VkPipelineRobustnessCreateInfoEXT-robustBufferAccess2-06932");
+    pipe.CreateComputePipeline();
+    m_errorMonitor->VerifyFound();
+
+    pipe.InitInfo();
+    pipe.InitState();
+    pipeline_robustness_info = LvlInitStruct<VkPipelineRobustnessCreateInfoEXT>();
+    pipeline_robustness_info.vertexInputs = VK_PIPELINE_ROBUSTNESS_BUFFER_BEHAVIOR_ROBUST_BUFFER_ACCESS_2_EXT;
+    pipe.cp_ci_.pNext = &pipeline_robustness_info;
+
+    m_errorMonitor->SetDesiredFailureMsg(kErrorBit, "VUID-VkPipelineRobustnessCreateInfoEXT-robustBufferAccess2-06933");
+    pipe.CreateComputePipeline();
+    m_errorMonitor->VerifyFound();
+}
+
+TEST_F(VkLayerTest, PipelineRobustnessRobustImageAccess2Unsupported) {
+    TEST_DESCRIPTION("Create a pipeline using VK_EXT_pipeline_robustness with robustImageAccess2 being unsupported");
+
+    AddRequiredExtensions(VK_EXT_PIPELINE_ROBUSTNESS_EXTENSION_NAME);
+    AddOptionalExtensions(VK_EXT_ROBUSTNESS_2_EXTENSION_NAME);
+
+    SetTargetApiVersion(VK_API_VERSION_1_1);
+    ASSERT_NO_FATAL_FAILURE(InitFramework());
+    if (DeviceValidationVersion() < VK_API_VERSION_1_1) {
+        GTEST_SKIP() << "At least Vulkan version 1.1 is required";
+    }
+    if (!AreRequiredExtensionsEnabled()) {
+        GTEST_SKIP() << RequiredExtensionsNotSupported() << " not supported";
+    }
+
+    if (IsExtensionsEnabled(VK_EXT_ROBUSTNESS_2_EXTENSION_NAME)) {
+        auto robustness2_features = LvlInitStruct<VkPhysicalDeviceRobustness2FeaturesEXT>();
+        GetPhysicalDeviceFeatures2(robustness2_features);
+
+        if (robustness2_features.robustImageAccess2) {
+            GTEST_SKIP() << "robustImageAccess2 is supported";
+        }
+    }
+
+    auto pipeline_robustness_features = LvlInitStruct<VkPhysicalDevicePipelineRobustnessFeaturesEXT>();
+    auto features2 = GetPhysicalDeviceFeatures2(pipeline_robustness_features);
+
+    if (!pipeline_robustness_features.pipelineRobustness) {
+        GTEST_SKIP() << "pipelineRobustness is not supported";
+    }
+
+    ASSERT_NO_FATAL_FAILURE(InitState(nullptr, &features2));
+
+    CreateComputePipelineHelper pipe(*this);
+
+    VkPipelineRobustnessCreateInfoEXT pipeline_robustness_info;
+
+    pipe.InitInfo();
+    pipe.InitState();
+    pipeline_robustness_info = LvlInitStruct<VkPipelineRobustnessCreateInfoEXT>();
+    pipeline_robustness_info.images = VK_PIPELINE_ROBUSTNESS_IMAGE_BEHAVIOR_ROBUST_IMAGE_ACCESS_2_EXT;
+    pipe.cp_ci_.pNext = &pipeline_robustness_info;
+
+    m_errorMonitor->SetDesiredFailureMsg(kErrorBit, "VUID-VkPipelineRobustnessCreateInfoEXT-robustImageAccess2-06934");
+    pipe.CreateComputePipeline();
+    m_errorMonitor->VerifyFound();
+}
+
+TEST_F(VkLayerTest, PipelineRobustnessRobustImageAccessUnsupported) {
+    TEST_DESCRIPTION("Create a pipeline using VK_EXT_pipeline_robustness with robustImageAccess being unsupported");
+
+    AddRequiredExtensions(VK_EXT_PIPELINE_ROBUSTNESS_EXTENSION_NAME);
+    AddOptionalExtensions(VK_EXT_IMAGE_ROBUSTNESS_EXTENSION_NAME);
+
+    SetTargetApiVersion(VK_API_VERSION_1_1);
+    ASSERT_NO_FATAL_FAILURE(InitFramework());
+    if (DeviceValidationVersion() < VK_API_VERSION_1_1) {
+        GTEST_SKIP() << "At least Vulkan version 1.1 is required";
+    }
+    if (!AreRequiredExtensionsEnabled()) {
+        GTEST_SKIP() << RequiredExtensionsNotSupported() << " not supported";
+    }
+
+    if (IsExtensionsEnabled(VK_EXT_IMAGE_ROBUSTNESS_EXTENSION_NAME)) {
+        auto robust_image_features = LvlInitStruct<VkPhysicalDeviceImageRobustnessFeatures>();
+        GetPhysicalDeviceFeatures2(robust_image_features);
+
+        if (robust_image_features.robustImageAccess) {
+            GTEST_SKIP() << "robustImageAccess is supported";
+        }
+    }
+
+    auto pipeline_robustness_features = LvlInitStruct<VkPhysicalDevicePipelineRobustnessFeaturesEXT>();
+    auto features2 = GetPhysicalDeviceFeatures2(pipeline_robustness_features);
+
+    if (!pipeline_robustness_features.pipelineRobustness) {
+        GTEST_SKIP() << "pipelineRobustness is not supported";
+    }
+
+    ASSERT_NO_FATAL_FAILURE(InitState(nullptr, &features2));
+
+    CreateComputePipelineHelper pipe(*this);
+
+    VkPipelineRobustnessCreateInfoEXT pipeline_robustness_info;
+
+    pipe.InitInfo();
+    pipe.InitState();
+    pipeline_robustness_info = LvlInitStruct<VkPipelineRobustnessCreateInfoEXT>();
+    pipeline_robustness_info.images = VK_PIPELINE_ROBUSTNESS_IMAGE_BEHAVIOR_ROBUST_IMAGE_ACCESS_EXT;
+    pipe.cp_ci_.pNext = &pipeline_robustness_info;
+
+    m_errorMonitor->SetDesiredFailureMsg(kErrorBit, "VUID-VkPipelineRobustnessCreateInfoEXT-robustImageAccess-06930");
+    pipe.CreateComputePipeline();
+    m_errorMonitor->VerifyFound();
 }
