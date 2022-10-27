42075ab1192d3f0e67b1b0306dcf791c0efe157b
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-27 00:39:12 +0000

    Adding tarfile member sanitization to extractall()

--- scripts/utils/utils.py
+++ scripts/utils/utils.py
@@ -72,7 +72,26 @@ class URLRequest:
 def expand_archive(path):
     if path.endswith('tar.gz') or path.endswith('tgz'):
         import tarfile
-        with tarfile.open(path, 'r:gz') as tar: tar.extractall()
+                                                def is_within_directory(directory, target):
+                                                    
+                                                    abs_directory = os.path.abspath(directory)
+                                                    abs_target = os.path.abspath(target)
+                                                
+                                                    prefix = os.path.commonprefix([abs_directory, abs_target])
+                                                    
+                                                    return prefix == abs_directory
+                                                
+                                                def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                                                
+                                                    for member in tar.getmembers():
+                                                        member_path = os.path.join(path, member.name)
+                                                        if not is_within_directory(path, member_path):
+                                                            raise Exception("Attempted Path Traversal in Tar File")
+                                                
+                                                    tar.extractall(path, members, numeric_owner=numeric_owner) 
+                                                    
+                                                
+                                                safe_extract(tar)
     elif path.endswith('.zip'):
         import zipfile
         with zipfile.ZipFile(path, 'r') as archive: archive.extractall('.')
