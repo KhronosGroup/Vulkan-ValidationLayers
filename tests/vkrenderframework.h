63a2d10bb52b1add6f74d430ebc30e61933bb1b5
Juan Ramos <juan@lunarg.com>
2022-11-11 17:35:15 -0700

    tests: Move ErrorMonitor into it's own files

--- tests/vkrenderframework.h
+++ tests/vkrenderframework.h
@@ -23,6 +23,7 @@
 #define VKRENDERFRAMEWORK_H
 
 #include "lvt_function_pointers.h"
+#include "vkerrormonitor.h"
 
 #ifdef ANDROID
 #include "vktestframeworkandroid.h"
@@ -82,89 +83,6 @@ class VkDeviceObj : public vk_testing::Device {
     VkQueueObj *m_queue_obj = nullptr;
     VkQueue m_queue;
 };
-
-// ErrorMonitor Usage:
-//
-// Call SetDesiredFailureMsg with a string to be compared against all
-// encountered log messages, or a validation error enum identifying
-// desired error message. Passing NULL or VALIDATION_ERROR_MAX_ENUM
-// will match all log messages. logMsg will return true for skipCall
-// only if msg is matched or NULL.
-//
-// Call VerifyFound to determine if all desired failure messages
-// were encountered. Call VerifyNotFound to determine if any unexpected
-// failure was encountered.
-class ErrorMonitor {
-  public:
-    ErrorMonitor();
-    ~ErrorMonitor() noexcept;
-
-    // Set monitor to pristine state
-    void Reset();
-
-    // ErrorMonitor will look for an error message containing the specified string(s)
-    void SetDesiredFailureMsg(const VkFlags msgFlags, const std::string &msg);
-    void SetDesiredFailureMsg(const VkFlags msgFlags, const char *const msgString);
-
-    // ErrorMonitor will look for an error message containing the specified string(s)
-    template <typename Iter>
-    void SetDesiredFailureMsg(const VkFlags msgFlags, Iter iter, const Iter end) {
-        for (; iter != end; ++iter) {
-            SetDesiredFailureMsg(msgFlags, *iter);
-        }
-    }
-
-    // Set an error that the error monitor will ignore. Do not use this function if you are creating a new test.
-    // TODO: This is stopgap to block new unexpected errors from being introduced. The long-term goal is to remove the use of this
-    // function and its definition.
-    void SetUnexpectedError(const char *const msg);
-
-    // Set an error that should not cause a test failure
-    void SetAllowedFailureMsg(const char *const msg);
-
-    VkBool32 CheckForDesiredMsg(const char *const msgString);
-    VkDebugReportFlagsEXT GetMessageFlags();
-    void SetError(const char *const errorString);
-    void SetBailout(std::atomic<bool> *bailout);
-
-    // Helpers
-
-    void VerifyFound();
-    void Finish() {
-        VerifyNotFound();
-        Reset();
-    }
-
-  private:
-    // ExpectSuccess now takes an optional argument allowing a custom combination of debug flags
-    void ExpectSuccess(VkDebugReportFlagsEXT const message_flag_mask = kErrorBit);
-    bool ExpectingSuccess() const {
-        return (desired_message_strings_.size() == 1) &&
-               (desired_message_strings_.count("") == 1 && ignore_message_strings_.size() == 0);
-    }
-    bool NeedCheckSuccess() const { return ExpectingSuccess(); }
-    void VerifyNotFound();
-    // TODO: This is stopgap to block new unexpected errors from being introduced. The long-term goal is to remove the use of this
-    // function and its definition.
-    bool IgnoreMessage(std::string const &msg) const;
-    std::vector<std::string> GetOtherFailureMsgs() const;
-    bool AnyDesiredMsgFound() const;
-    bool AllDesiredMsgsFound() const;
-    void DumpFailureMsgs() const;
-    void MonitorReset();
-    std::unique_lock<std::mutex> Lock() const { return std::unique_lock<std::mutex>(mutex_); }
-
-    VkFlags message_flags_;
-    std::unordered_multiset<std::string> desired_message_strings_;
-    std::unordered_multiset<std::string> failure_message_strings_;
-    std::vector<std::string> ignore_message_strings_;
-    std::vector<std::string> allowed_message_strings_;
-    std::vector<std::string> other_messages_;
-    mutable std::mutex mutex_;
-    std::atomic<bool> *bailout_;
-    bool message_found_;
-};
-
 struct DebugReporter {
     void Create(VkInstance instance) noexcept;
     void Destroy(VkInstance instance) noexcept;
