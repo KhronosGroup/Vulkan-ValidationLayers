73ddd026780b181b0da1fd2054c35484170d74e4
Juan Ramos <juan@lunarg.com>
2022-11-17 09:48:27 -0700

    build: Fix extra semicolon
    Not ISO compliant

--- layers/stateless_validation.h
+++ layers/stateless_validation.h
@@ -967,7 +967,7 @@ class StatelessValidation : public ValidationObject {
         }
 
         return skip;
-    };
+    }
 
     template <typename RenderPassCreateInfoGeneric>
     bool CreateRenderPassGeneric(VkDevice device, const RenderPassCreateInfoGeneric *pCreateInfo,
