56478485dc8bb91aad796c801fbe23fb3b388f1f
Charles Giessen <charles@lunarg.com>
2023-02-27 18:19:46 -0700

    tests: Fix memory leaks of queues & instances
    This commit fixes a few memory leaks detected with Leak Sanitizer.
    
    VkLayerTest.PnextOnlyStructValidation and
    VkLayerTest.InvalidQueueFamilyIndex both allocated memory that would
    be leaked. Replacing them with a std::vector cleans it up.
    
    VkLayerTest.InstanceCreateEnumeratePortability creates a VkInstance
    that was not being destroyed before application shutdown.

--- tests/vklayertests_others.cpp
+++ tests/vklayertests_others.cpp
@@ -767,8 +767,9 @@ TEST_F(VkLayerTest, PnextOnlyStructValidation) {
 
     uint32_t queue_node_count;
     vk::GetPhysicalDeviceQueueFamilyProperties(gpu(), &queue_node_count, NULL);
-    VkQueueFamilyProperties *queue_props = new VkQueueFamilyProperties[queue_node_count];
-    vk::GetPhysicalDeviceQueueFamilyProperties(gpu(), &queue_node_count, queue_props);
+    std::vector<VkQueueFamilyProperties> queue_props;
+    queue_props.resize(queue_node_count);
+    vk::GetPhysicalDeviceQueueFamilyProperties(gpu(), &queue_node_count, queue_props.data());
     float priorities[] = {1.0f};
     VkDeviceQueueCreateInfo queue_info = LvlInitStruct<VkDeviceQueueCreateInfo>();
     queue_info.flags = 0;
@@ -1805,16 +1806,17 @@ TEST_F(VkLayerTest, InvalidQueueFamilyIndex) {
     // with SHARING_MODE_CONCURRENT that uses a non-device PDEV queue family.
     uint32_t queue_count;
     vk::GetPhysicalDeviceQueueFamilyProperties(gpu(), &queue_count, NULL);
-    VkQueueFamilyProperties *queue_props = new VkQueueFamilyProperties[queue_count];
-    vk::GetPhysicalDeviceQueueFamilyProperties(gpu(), &queue_count, queue_props);
+    std::vector<VkQueueFamilyProperties> queue_props;
+    queue_props.resize(queue_count);
+    vk::GetPhysicalDeviceQueueFamilyProperties(gpu(), &queue_count, queue_props.data());
 
     if (queue_count < 3) {
         GTEST_SKIP() << "Multiple queue families are required to run this test.";
     }
-    std::vector<float> priorities(queue_props->queueCount, 1.0f);
+    std::vector<float> priorities(queue_props.at(0).queueCount, 1.0f);
     VkDeviceQueueCreateInfo queue_info = LvlInitStruct<VkDeviceQueueCreateInfo>();
     queue_info.queueFamilyIndex = 0;
-    queue_info.queueCount = queue_props->queueCount;
+    queue_info.queueCount = static_cast<uint32_t>(queue_props.at(0).queueCount);
     queue_info.pQueuePriorities = priorities.data();
     VkDeviceCreateInfo dev_info = LvlInitStruct<VkDeviceCreateInfo>();
     dev_info.queueCreateInfoCount = 1;
@@ -9179,6 +9181,7 @@ TEST_F(VkLayerTest, InstanceCreateEnumeratePortability) {
         ici.ppEnabledExtensionNames = enabled_extensions.data();
 
         vk::CreateInstance(&ici, nullptr, &local_instance);
+        vk::DestroyInstance(local_instance, nullptr);
     }
 }
 
