9124f788b7de42a4b2589b7ede812dd887447f0c
Mark Lobodzinski <mark@lunarg.com>
2023-03-24 14:39:11 -0600

    android: Revert c++_static lib settings
    This change caused random crashes in CI tests across
    several devices.  Going back to shared to get CI working
    again.
    
    Change-Id: I43a64b251ce4f641d54f8207ee3d422387d9b627

--- build-android/jni/shaderc/Application.mk
+++ build-android/jni/shaderc/Application.mk
@@ -1,4 +1,5 @@
 APP_ABI := all
 APP_BUILD_SCRIPT := Android.mk
-APP_STL := c++_static
+# Change back to shared lib until Android test crashes can be resolved
+APP_STL := c++_shared
 APP_PLATFORM := android-23
