bf29769ef00b85b4db18a9c770b8ac8398e8170f
Faith Ekstrand <faith.ekstrand@collabora.com>
2023-03-24 12:51:17 -0500

    layers: Tidy up ValidateMapMemRange()
    Pull allocationSize out into a temporary and make mem_info a reference
    so we don't have to think about pointers NULL checks.

--- layers/core_checks/core_validation.h
+++ layers/core_checks/core_validation.h
@@ -401,7 +401,7 @@ class CoreChecks : public ValidationStateTracker {
                                      const RENDER_PASS_STATE& rp2_state, const char* msg, const char* caller,
                                      const char* error_code) const;
     bool ValidateStageMaskHost(const Location& loc, VkPipelineStageFlags2KHR stageMask) const;
-    bool ValidateMapMemRange(const DEVICE_MEMORY_STATE* mem_info, VkDeviceSize offset, VkDeviceSize size) const;
+    bool ValidateMapMemRange(const DEVICE_MEMORY_STATE& mem_info, VkDeviceSize offset, VkDeviceSize size) const;
     bool ValidateRenderPassDAG(RenderPassCreateVersion rp_version, const VkRenderPassCreateInfo2* pCreateInfo) const;
     bool ValidateAttachmentCompatibility(const char* type1_string, const RENDER_PASS_STATE& rp1_state, const char* type2_string,
                                          const RENDER_PASS_STATE& rp2_state, uint32_t primary_attach, uint32_t secondary_attach,
