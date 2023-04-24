6473b26d64d46f43ecca8de3a11e621d3554c74b
juan-lunarg <juan@lunarg.com>
2023-04-24 13:39:40 -0600

    android: Create android_hardware_buffer.h
    Simplifies the Android build. This helper function has nothing
    to do with the test framework.
    
    This makes it simpler to consolidate the 2 Android builds.

--- tests/negative/android_hardware_buffer.cpp
+++ tests/negative/android_hardware_buffer.cpp
@@ -14,7 +14,10 @@
 
 #include "../framework/layer_validation_tests.h"
 
-#include "utils/android_ndk_types.h"
+#if defined(VK_USE_PLATFORM_ANDROID_KHR)
+#include "../framework/android_hardware_buffer.h"
+#endif
+
 #ifdef AHB_VALIDATION_SUPPORT
 
 TEST_F(VkLayerTest, AndroidHardwareBufferImageCreate) {
