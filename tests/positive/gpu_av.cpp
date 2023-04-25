646992d284be9528bcf78129a1db6c652f50682f
Jeremy Gebben <jeremyg@lunarg.com>
2023-04-25 16:56:26 -0600

    tests: Improve PositiveGpuAssistedLayer.SetSSBOPushDescriptor
    This test needs to call vkQueueSubmit() so that the GPU-AV
    instrumentation runs.
    
    vkCmdBindDescriptorSets() should not be called for push descriptors,
    the test was doing this and that was hiding the fact that
    validation wasn't handling vkCmdPushDescriptorSetKHR().
    
    Add PositiveGpuAssistedLayer.SetSSBOBindDescriptor to cover the bind
    use case.

--- tests/positive/gpu_av.cpp
+++ tests/positive/gpu_av.cpp
@@ -19,6 +19,107 @@
 
 class PositiveGpuAssistedLayer : public VkGpuAssistedLayerTest {};
 
+TEST_F(PositiveGpuAssistedLayer, SetSSBOBindDescriptor) {
+    TEST_DESCRIPTION("Makes sure we can use vkCmdBindDescriptorSets()");
+    SetTargetApiVersion(VK_API_VERSION_1_1);
+    VkValidationFeaturesEXT validation_features = GetValidationFeatures();
+    ASSERT_NO_FATAL_FAILURE(InitFramework(m_errorMonitor, &validation_features));
+    if (!AreRequiredExtensionsEnabled()) {
+        GTEST_SKIP() << RequiredExtensionsNotSupported() << " not supported";
+    }
+    if (!CanEnableGpuAV()) {
+        GTEST_SKIP() << "Requirements for GPU-AV are not met";
+    }
+    ASSERT_NO_FATAL_FAILURE(InitState());
+    ASSERT_NO_FATAL_FAILURE(InitRenderTarget());
+
+    VkPhysicalDeviceProperties properties = {};
+    vk::GetPhysicalDeviceProperties(gpu(), &properties);
+    if (properties.limits.maxBoundDescriptorSets < 8) {
+        GTEST_SKIP() << "maxBoundDescriptorSets is too low";
+    }
+
+    char const *csSource = R"glsl(
+        #version 450
+        layout(constant_id=0) const uint _const_2_0 = 1;
+        layout(constant_id=1) const uint _const_3_0 = 1;
+        layout(std430, binding=0) readonly restrict buffer _SrcBuf_0_0 {
+            layout(offset=0) uint src[256];
+        };
+        layout(std430, binding=1) writeonly restrict buffer _DstBuf_1_0 {
+            layout(offset=0) uint dst[256];
+        };
+        layout (local_size_x = 256, local_size_y = 1) in;
+
+        void main() {
+            uint word = src[_const_2_0 + gl_GlobalInvocationID.x];
+            word = (word & 0xFF00FF00u) >> 8 |
+                (word & 0x00FF00FFu) << 8;
+            dst[_const_3_0 + gl_GlobalInvocationID.x] = word;
+        }
+    )glsl";
+
+    VkShaderObj cs(this, csSource, VK_SHADER_STAGE_COMPUTE_BIT);
+
+    OneOffDescriptorSet descriptor_set_0(m_device,
+                                         {{0, VK_DESCRIPTOR_TYPE_STORAGE_BUFFER, 1, VK_SHADER_STAGE_COMPUTE_BIT, nullptr},
+                                          {1, VK_DESCRIPTOR_TYPE_STORAGE_BUFFER, 1, VK_SHADER_STAGE_COMPUTE_BIT, nullptr}});
+    const VkPipelineLayoutObj pipeline_layout(m_device, {&descriptor_set_0.layout_});
+    ASSERT_TRUE(pipeline_layout.initialized());
+
+    auto pipeline_info = LvlInitStruct<VkComputePipelineCreateInfo>();
+    pipeline_info.flags = 0;
+    pipeline_info.layout = pipeline_layout.handle();
+    pipeline_info.basePipelineHandle = VK_NULL_HANDLE;
+    pipeline_info.basePipelineIndex = -1;
+    pipeline_info.stage = cs.GetStageCreateInfo();
+
+    VkPipeline pipeline;
+    vk::CreateComputePipelines(device(), VK_NULL_HANDLE, 1, &pipeline_info, nullptr, &pipeline);
+
+    VkBufferObj buffer_0;
+    auto buffer_ci = LvlInitStruct<VkBufferCreateInfo>();
+    buffer_ci.usage = VK_BUFFER_USAGE_TRANSFER_SRC_BIT | VK_BUFFER_USAGE_TRANSFER_DST_BIT | VK_BUFFER_USAGE_STORAGE_BUFFER_BIT;
+    buffer_ci.size = 262144;
+    buffer_0.init(*m_device, buffer_ci);
+    VkBufferObj buffer_1;
+    buffer_1.init(*m_device, buffer_ci);
+
+    VkWriteDescriptorSet descriptor_writes[2];
+    descriptor_writes[0] = LvlInitStruct<VkWriteDescriptorSet>();
+    descriptor_writes[0].dstSet = descriptor_set_0.set_;
+    descriptor_writes[0].dstBinding = 0;
+    descriptor_writes[0].dstArrayElement = 0;
+    descriptor_writes[0].descriptorCount = 1;
+    descriptor_writes[0].descriptorType = VK_DESCRIPTOR_TYPE_STORAGE_BUFFER;
+    VkDescriptorBufferInfo buffer_info_0 = {buffer_0.handle(), 0, 1024};
+    descriptor_writes[0].pBufferInfo = &buffer_info_0;
+
+    descriptor_writes[1] = descriptor_writes[0];
+    descriptor_writes[1].dstBinding = 1;
+    VkDescriptorBufferInfo buffer_info_1 = {buffer_1.handle(), 0, 1024};
+    descriptor_writes[1].pBufferInfo = &buffer_info_1;
+
+    vk::UpdateDescriptorSets(device(), 2, descriptor_writes, 0, nullptr);
+
+    m_commandBuffer->begin();
+    vk::CmdBindPipeline(m_commandBuffer->handle(), VK_PIPELINE_BIND_POINT_COMPUTE, pipeline);
+    vk::CmdBindDescriptorSets(m_commandBuffer->handle(), VK_PIPELINE_BIND_POINT_COMPUTE, pipeline_layout.handle(), 0, 1,
+                              &descriptor_set_0.set_, 0, nullptr);
+
+    vk::CmdDispatch(m_commandBuffer->handle(), 1, 1, 1);
+    m_commandBuffer->end();
+
+    auto submit_info = LvlInitStruct<VkSubmitInfo>();
+    submit_info.commandBufferCount = 1;
+    submit_info.pCommandBuffers = &m_commandBuffer->handle();
+
+    vk::QueueSubmit(m_device->m_queue, 1, &submit_info, VK_NULL_HANDLE);
+    vk::QueueWaitIdle(m_device->m_queue);
+
+    vk::DestroyPipeline(m_device->device(), pipeline, nullptr);
+}
+
 TEST_F(PositiveGpuAssistedLayer, SetSSBOPushDescriptor) {
     TEST_DESCRIPTION("Makes sure we can use vkCmdPushDescriptorSetKHR instead of vkUpdateDescriptorSets");
     SetTargetApiVersion(VK_API_VERSION_1_1);
@@ -66,13 +167,7 @@ TEST_F(PositiveGpuAssistedLayer, SetSSBOPushDescriptor) {
                                          {{0, VK_DESCRIPTOR_TYPE_STORAGE_BUFFER, 1, VK_SHADER_STAGE_COMPUTE_BIT, nullptr},
                                           {1, VK_DESCRIPTOR_TYPE_STORAGE_BUFFER, 1, VK_SHADER_STAGE_COMPUTE_BIT, nullptr}},
                                          VK_DESCRIPTOR_SET_LAYOUT_CREATE_PUSH_DESCRIPTOR_BIT_KHR);
-    OneOffDescriptorSet descriptor_set_1(m_device, {});  // empty
-    OneOffDescriptorSet descriptor_set_2(m_device, {{0, VK_DESCRIPTOR_TYPE_STORAGE_BUFFER, 1, VK_SHADER_STAGE_ALL, nullptr},
-                                                    {1, VK_DESCRIPTOR_TYPE_STORAGE_BUFFER, 1, VK_SHADER_STAGE_ALL, nullptr},
-                                                    {2, VK_DESCRIPTOR_TYPE_STORAGE_BUFFER, 1, VK_SHADER_STAGE_ALL, nullptr}});
-
-    const VkPipelineLayoutObj pipeline_layout(
-        m_device, {&descriptor_set_0.layout_, &descriptor_set_1.layout_, &descriptor_set_1.layout_, &descriptor_set_2.layout_});
+    const VkPipelineLayoutObj pipeline_layout(m_device, {&descriptor_set_0.layout_});
     ASSERT_TRUE(pipeline_layout.initialized());
 
     auto pipeline_info = LvlInitStruct<VkComputePipelineCreateInfo>();
@@ -113,10 +208,15 @@ TEST_F(PositiveGpuAssistedLayer, SetSSBOPushDescriptor) {
     vk::CmdPushDescriptorSetKHR(m_commandBuffer->handle(), VK_PIPELINE_BIND_POINT_COMPUTE, pipeline_layout.handle(), 0, 2,
                                 descriptor_writes);
 
-    vk::CmdBindDescriptorSets(m_commandBuffer->handle(), VK_PIPELINE_BIND_POINT_COMPUTE, pipeline_layout.handle(), 3, 1,
-                              &descriptor_set_2.set_, 0, nullptr);
-
     vk::CmdDispatch(m_commandBuffer->handle(), 1, 1, 1);
     m_commandBuffer->end();
+
+    auto submit_info = LvlInitStruct<VkSubmitInfo>();
+    submit_info.commandBufferCount = 1;
+    submit_info.pCommandBuffers = &m_commandBuffer->handle();
+
+    vk::QueueSubmit(m_device->m_queue, 1, &submit_info, VK_NULL_HANDLE);
+    vk::QueueWaitIdle(m_device->m_queue);
+
     vk::DestroyPipeline(m_device->device(), pipeline, nullptr);
 }
