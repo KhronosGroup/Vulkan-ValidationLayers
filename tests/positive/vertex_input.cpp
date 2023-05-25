6cb94656cc7c159497eba98cb493ce815827ad57
spencer-lunarg <spencer@lunarg.com>
2023-05-25 18:03:25 +0900

    layers: Remove false positive for Vertex Input Structs

--- tests/positive/vertex_input.cpp
+++ tests/positive/vertex_input.cpp
@@ -57,7 +57,7 @@ TEST_F(PositiveVertexInput, AttributeMatrixType) {
 }
 
 TEST_F(PositiveVertexInput, AttributeArrayType) {
-    TEST_DESCRIPTION("Make sure same types don't throw an error");
+    TEST_DESCRIPTION("Input in OpTypeArray");
 
     ASSERT_NO_FATAL_FAILURE(Init());
     ASSERT_NO_FATAL_FAILURE(InitRenderTarget());
@@ -90,6 +90,118 @@ TEST_F(PositiveVertexInput, AttributeArrayType) {
     pipe.vi_ci_.vertexBindingDescriptionCount = 1;
     pipe.vi_ci_.pVertexAttributeDescriptions = input_attribs;
     pipe.vi_ci_.vertexAttributeDescriptionCount = 2;
+    pipe.shader_stages_ = {vs.GetStageCreateInfo(), fs.GetStageCreateInfo()};
+    pipe.InitState();
+    pipe.CreateGraphicsPipeline();
+}
+
+TEST_F(PositiveVertexInput, AttributeStructType) {
+    TEST_DESCRIPTION("Input is OpTypeStruct");
+
+    ASSERT_NO_FATAL_FAILURE(Init());
+    ASSERT_NO_FATAL_FAILURE(InitRenderTarget());
+
+    VkVertexInputBindingDescription input_binding = {0, 32, VK_VERTEX_INPUT_RATE_VERTEX};
+
+    VkVertexInputAttributeDescription input_attrib;
+    memset(&input_attrib, 0, sizeof(input_attrib));
+    input_attrib.format = VK_FORMAT_R32G32B32A32_SFLOAT;
+    input_attrib.location = 4;
+
+    // This is not valid GLSL (but is valid SPIR-V) - would look like:
+    //     in VertexIn {
+    //         layout(location = 4) vec4 x;
+    //     } x_struct;
+    char const *vsSource = R"(
+               OpCapability Shader
+               OpMemoryModel Logical Simple
+               OpEntryPoint Vertex %1 "main" %2
+               OpMemberDecorate %_struct_3 0 Location 4
+               OpDecorate %_struct_3 Block
+       %void = OpTypeVoid
+          %5 = OpTypeFunction %void
+      %float = OpTypeFloat 32
+    %v4float = OpTypeVector %float 4
+  %_struct_3 = OpTypeStruct %v4float
+%_ptr_Input__struct_3 = OpTypePointer Input %_struct_3
+          %2 = OpVariable %_ptr_Input__struct_3 Input
+          %1 = OpFunction %void None %5
+         %13 = OpLabel
+               OpReturn
+               OpFunctionEnd
+    )";
+
+    VkShaderObj vs(this, vsSource, VK_SHADER_STAGE_VERTEX_BIT, SPV_ENV_VULKAN_1_0, SPV_SOURCE_ASM);
+    VkShaderObj fs(this, bindStateFragShaderText, VK_SHADER_STAGE_FRAGMENT_BIT);
+
+    CreatePipelineHelper pipe(*this);
+    pipe.InitInfo();
+    pipe.vi_ci_.pVertexBindingDescriptions = &input_binding;
+    pipe.vi_ci_.vertexBindingDescriptionCount = 1;
+    pipe.vi_ci_.pVertexAttributeDescriptions = &input_attrib;
+    pipe.vi_ci_.vertexAttributeDescriptionCount = 1;
+    pipe.shader_stages_ = {vs.GetStageCreateInfo(), fs.GetStageCreateInfo()};
+    pipe.InitState();
+    pipe.CreateGraphicsPipeline();
+}
+
+TEST_F(PositiveVertexInput, AttributeStructTypeWithArray) {
+    TEST_DESCRIPTION("Input is OpTypeStruct that has an OpTypeArray. Locations are not in order netiher");
+
+    ASSERT_NO_FATAL_FAILURE(Init());
+    ASSERT_NO_FATAL_FAILURE(InitRenderTarget());
+
+    VkVertexInputBindingDescription input_binding = {0, 48, VK_VERTEX_INPUT_RATE_VERTEX};
+
+    VkVertexInputAttributeDescription input_attribs[3];
+    memset(input_attribs, 0, sizeof(input_attribs));
+    input_attribs[0].location = 1;
+    input_attribs[0].format = VK_FORMAT_R32G32B32_SFLOAT;
+
+    input_attribs[1].location = 4;
+    input_attribs[1].format = VK_FORMAT_R32G32B32A32_SFLOAT;
+
+    input_attribs[2].location = 5;
+    input_attribs[2].format = VK_FORMAT_R32G32B32A32_SFLOAT;
+
+    // This is not valid GLSL (but is valid SPIR-V) - would look like:
+    //     in VertexIn {
+    //         layout(location = 4) vec4 y[2];
+    //         layout(location = 1) vec3 x;
+    //     } x_struct;
+    char const *vsSource = R"(
+               OpCapability Shader
+               OpMemoryModel Logical Simple
+               OpEntryPoint Vertex %1 "main" %2
+               OpMemberDecorate %_struct_3 0 Location 4
+               OpMemberDecorate %_struct_3 1 Location 1
+               OpDecorate %_struct_3 Block
+       %void = OpTypeVoid
+          %5 = OpTypeFunction %void
+      %float = OpTypeFloat 32
+    %v3float = OpTypeVector %float 3
+    %v4float = OpTypeVector %float 4
+       %uint = OpTypeInt 32 0
+     %uint_2 = OpConstant %uint 2
+ %array_vec4 = OpTypeArray %v3float %uint_2
+  %_struct_3 = OpTypeStruct %array_vec4 %v4float
+%_ptr_Input__struct_3 = OpTypePointer Input %_struct_3
+          %2 = OpVariable %_ptr_Input__struct_3 Input
+          %1 = OpFunction %void None %5
+         %13 = OpLabel
+               OpReturn
+               OpFunctionEnd
+    )";
+
+    VkShaderObj vs(this, vsSource, VK_SHADER_STAGE_VERTEX_BIT, SPV_ENV_VULKAN_1_0, SPV_SOURCE_ASM);
+    VkShaderObj fs(this, bindStateFragShaderText, VK_SHADER_STAGE_FRAGMENT_BIT);
+
+    CreatePipelineHelper pipe(*this);
+    pipe.InitInfo();
+    pipe.vi_ci_.pVertexBindingDescriptions = &input_binding;
+    pipe.vi_ci_.vertexBindingDescriptionCount = 1;
+    pipe.vi_ci_.pVertexAttributeDescriptions = input_attribs;
+    pipe.vi_ci_.vertexAttributeDescriptionCount = 3;
     pipe.shader_stages_ = {vs.GetStageCreateInfo(), fs.GetStageCreateInfo()};
     pipe.InitState();
     pipe.CreateGraphicsPipeline();
