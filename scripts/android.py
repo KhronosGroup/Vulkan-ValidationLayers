ac2161ebf2293bd9faef2f5ade083cb05a28b658
Juan Ramos <juan@lunarg.com>
2023-06-28 19:50:35 -0600

    android: Cleanup android.py script
    - Ensure environment is setup properly
    - More error checking
    - Add helper functions

--- scripts/android.py
+++ scripts/android.py
@@ -20,20 +20,94 @@ import sys
 import shutil
 import common_ci
 
-# TODO: Find a more robust way to get the host_tag.
-def get_host_tag():
+# https://android.googlesource.com/platform/ndk/+/master/docs/BuildSystemMaintainers.md#STL
+# "If using the shared variant, libc++_shared.so must be included in the APK."
+def get_shared_stl(NDK : str, ABI : str) -> str:
+    # https://android.googlesource.com/platform/ndk/+/master/docs/BuildSystemMaintainers.md#architectures
+    # "abis.json" lets us get the triple based on the abi.
+    with open(f"{NDK}/meta/abis.json") as f:
+        abis_json = json.load(f)
+    triple = abis_json[ABI]['triple']
+
+    # Unlike the triple there doesn't seem to be a way to programatically retreive the host-tag.
+    # However, this is robust enough considering it's hardcoded in multiple places in the NDK.
     if sys.platform.startswith('linux'):
-        return 'linux-x86_64'
+        host_tag = 'linux-x86_64'
     elif sys.platform == 'darwin':
-        return 'darwin-x86_64'
+        host_tag = 'darwin-x86_64'
     elif sys.platform == 'win32' or sys.platform == 'cygwin':
-        return 'windows-x86_64'
-    sys.exit('Unsupported platform: ' + sys.platform)
+        host_tag = 'windows-x86_64'
+    else:
+        sys.exit(f'Unsupported platform: {sys.platform}')
+
+    # "https://android.googlesource.com/platform/ndk/+/master/docs/BuildSystemMaintainers.md#sysroot"
+    # The Android sysroot is installed to <NDK>/toolchains/llvm/prebuilt/<host-tag>/sysroot
+    sysroot = f'{NDK}/toolchains/llvm/prebuilt/{host_tag}/sysroot'
+    if not os.path.isdir(sysroot):
+        print("Unable to find sysroot!")
+        print('NDK = {NDK}')
+        print('HOST TAG = {host_tag}')
+        sys.exit(-1)
+
+    # https://android.googlesource.com/platform/ndk/+/master/docs/BuildSystemMaintainers.md#STL
+    # This library is installed to <NDK>/sysroot/usr/lib/<triple>."
+    src_shared_stl = f'{sysroot}/usr/lib/{triple}/libc++_shared.so'
+    if not os.path.isfile(src_shared_stl):
+        print("Unable to find libc++_shared.so!")
+        print('Triple = {triple}')
+        sys.exit(-1)
+
+    return src_shared_stl
+
+# Manifest file describing out test application
+def get_android_manifest() -> str:
+    manifest = common_ci.RepoRelative('build-android/AndroidManifest.xml')
+    if not os.path.isfile(manifest):
+        print(f"Unable to find manifest for APK! {manifest}")
+        sys.exit(-1)
+    return manifest
+
+# Resources for our test application.
+def get_android_resources() -> str:
+    res = common_ci.RepoRelative('build-android/res')
+    if not os.path.isdir(res):
+        print(f"Unable to find android resources for APK! {res}")
+        sys.exit(-1)
+    return res
+
+# Generate the APK from the CMake binaries
+def generate_apk(SDK_ROOT : str, CMAKE_INSTALL_DIR : str, APK_DIR: str) -> str:
+    android_manifest = get_android_manifest()
+    android_resources = get_android_resources()
+
+    android_jar = f"{SDK_ROOT}/platforms/android-26/android.jar"
+    if not os.path.isfile(android_jar):
+        print(f"Unable to find {android_jar}!")
+        sys.exit(-1)
+
+    apk_name = 'VulkanLayerValidationTests'
+
+    unaligned_apk = f'{APK_DIR}/{apk_name}-unaligned.apk'
+    test_apk = f'{APK_DIR}/{apk_name}.apk'
+
+    # Create APK
+    common_ci.RunShellCmd(f'aapt package -f -M {android_manifest} -I {android_jar} -S {android_resources} -F {unaligned_apk} {CMAKE_INSTALL_DIR}')
+
+    # Align APK
+    common_ci.RunShellCmd(f'zipalign -f 4 {unaligned_apk} {test_apk}')
+
+    # Create Key (If it doesn't already exist)
+    debug_key = common_ci.RepoRelative(f'{APK_DIR}/debug.keystore')
+    ks_pass = 'android'
+    if not os.path.isfile(debug_key):
+        dname = 'CN=Android-Debug,O=Android,C=US'
+        common_ci.RunShellCmd(f'keytool -genkey -v -keystore {debug_key} -alias androiddebugkey -storepass {ks_pass} -keypass {ks_pass} -keyalg RSA -keysize 2048 -validity 10000 -dname {dname}')
+
+    # Sign APK
+    common_ci.RunShellCmd(f'apksigner sign --verbose --ks {debug_key} --ks-pass pass:{ks_pass} {test_apk}')
 
 # NOTE: Android this documentation is crucial for understanding the layout of the NDK.
 # https://android.googlesource.com/platform/ndk/+/master/docs/BuildSystemMaintainers.md
-
-# TODO: Use CMake from Android SDK?
 
 # TODO: Handle both c++_shared and c++_static for CMAKE_ANDROID_STL_TYPE
 
@@ -53,20 +127,30 @@ def main():
 
     android_ndk_home = os.environ.get('ANDROID_NDK_HOME')
     android_sdk_root = os.environ.get('ANDROID_SDK_ROOT')
-    android_toolchain = android_ndk_home + '/build/cmake/android.toolchain.cmake'
+    android_toolchain = f'{android_ndk_home}/build/cmake/android.toolchain.cmake'
 
     print(f"ANDROID_NDK_HOME = {android_ndk_home}")
     print(f"ANDROID_SDK_ROOT = {android_sdk_root}")
-    print(f'CMAKE_TOOLCHAIN_FILE = {android_toolchain}')
 
-    # TODO: Verify aapt, ndk-build, cmake, ninja are in the path.
+    if not os.path.isfile(android_toolchain):
+        print(f'Unable to find android.toolchain.cmake at {android_toolchain}')
+        exit(-1)
+
+    # We require a fair amount of tools to be in the PATH
+    for tool in ['cmake', 'ninja', 'aapt', 'zipalign', 'keytool', 'apksigner']:
+        path = shutil.which(tool)
+        if path is None:
+            print(f"Unable to find {tool}!")
+            exit(-1)
+
+        print(f"Using {tool} : {path}")
 
     # TODO: Make ABI a cli argument.
     # android_abis = ["armeabi-v7a", "arm64-v8a", "x86", "x86_64"]
     android_abis = [ "arm64-v8a" ]
 
     apk_dir = common_ci.RepoRelative(f'build-android/bin')
-    cmake_install_dir = common_ci.RepoRelative(f'build-android/bin/libs')
+    cmake_install_dir = common_ci.RepoRelative(f'{apk_dir}/libs')
 
     # NOTE: I'm trying to roughly match what build-android/build_all.sh currently does.
     for abi in android_abis:
@@ -75,7 +159,7 @@ def main():
 
         # Delete CMakeCache.txt to ensure clean builds
         # NOTE: CMake 3.24 has --fresh which would be better to use in the future.
-        cmake_cache = build_dir + '/CMakeCache.txt'
+        cmake_cache = f'{build_dir}/CMakeCache.txt'
         if os.path.isfile(cmake_cache):
             os.remove(cmake_cache)
 
@@ -106,73 +190,17 @@ def main():
         install_cmd = f'cmake --install {build_dir} --prefix {cmake_install_dir}'
         common_ci.RunShellCmd(install_cmd)
 
-        # https://android.googlesource.com/platform/ndk/+/master/docs/BuildSystemMaintainers.md#architectures
-        # "abis.json" lets us get the triple based on the abi.
-        with open(android_ndk_home + "/meta/abis.json") as f:
-            abis_json = json.load(f)
-
-        host_tag = get_host_tag()
-
-        triple = abis_json[abi]['triple']
-        # "https://android.googlesource.com/platform/ndk/+/master/docs/BuildSystemMaintainers.md#sysroot"
-        # The Android sysroot is installed to <NDK>/toolchains/llvm/prebuilt/<host-tag>/sysroot
-        sysroot = f'{android_ndk_home}/toolchains/llvm/prebuilt/{host_tag}/sysroot'
-        if not os.path.isdir(sysroot):
-            print("Unable to find sysroot!")
-            print('NDK = {android_ndk_home}')
-            print('HOST-TAG = {host_tag}')
-            sys.exit(-1)
-
-        # https://android.googlesource.com/platform/ndk/+/master/docs/BuildSystemMaintainers.md#STL
-        # "If using the shared variant, libc++_shared.so must be included in the APK. This library is installed to <NDK>/sysroot/usr/lib/<triple>."
-        src_shared_stl = f'{sysroot}/usr/lib/{triple}/libc++_shared.so'
-        if not os.path.isfile(src_shared_stl):
-            print("Unable to find libc++_shared.so!")
-            print('Triple = {triple}')
-            sys.exit(-1)
-
         if not os.path.isfile(f'{cmake_install_dir}/{lib_dir}/libVulkanLayerValidationTests.so'):
             print("Unable to find tests!")
             sys.exit(-1)
 
+        src_shared_stl = get_shared_stl(NDK = android_ndk_home, ABI = abi)
         dst_shared_stl = f'{cmake_install_dir}/{lib_dir}/libc++_shared.so'
 
         shutil.copyfile(src_shared_stl, dst_shared_stl)
-        print(f'Copied {src_shared_stl} to {dst_shared_stl}')
-
-    # The following are CLI instructions I plan to automate with this script.
-    # The main problem I'm having with Android is the steps after the CMake build.
-    #
-    # IE generating the APK and generating binaries for multiple platforms.
-    # CMake only handles 1 toolchain per build. As a result we need to run CMake for each ISA Android supports.
-    # Then we need to create an APK with those binaries.
-    # However, we only need the APK for testing. For our Android releases we just put our `.so` files for each platform. No APK drama.
-    android_manifest = common_ci.RepoRelative('build-android/AndroidManifest.xml')
-
-    android_jar = android_sdk_root + "/platforms/android-26/android.jar"
-
-    android_res = common_ci.RepoRelative('build-android/res')
-
-    apk_name = 'VulkanLayerValidationTests'
-
-    unaligned_apk = f'{apk_dir}/{apk_name}-unaligned.apk'
-    aligned_apk = f'{apk_dir}/{apk_name}.apk'
-
-    # Create APK
-    common_ci.RunShellCmd(f'aapt package -f -M {android_manifest} -I {android_jar} -S {android_res} -F {unaligned_apk} {cmake_install_dir}')
-
-    # Align APK
-    common_ci.RunShellCmd(f'zipalign -f 4 {unaligned_apk} {aligned_apk}')
-
-    # Create Key (If it doesn't already exist)
-    debug_key = common_ci.RepoRelative('build-android/obj/debug.keystore')
-    ks_pass = 'android'
-    if not os.path.isfile(debug_key):
-        dname = 'CN=Android-Debug,O=Android,C=US'
-        common_ci.RunShellCmd(f'keytool -genkey -v -keystore {debug_key} -alias androiddebugkey -storepass {ks_pass} -keypass {ks_pass} -keyalg RSA -keysize 2048 -validity 10000 -dname {dname}')
-
-    # Sign APK
-    common_ci.RunShellCmd(f'apksigner sign --verbose --ks {debug_key} --ks-pass pass:{ks_pass} {aligned_apk}')
+        print(f'-- Installing: {dst_shared_stl}')
+    
+    generate_apk(SDK_ROOT = android_sdk_root, CMAKE_INSTALL_DIR = cmake_install_dir, APK_DIR = apk_dir)
 
 if __name__ == '__main__':
     main()
