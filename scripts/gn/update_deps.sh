632780c9630c4f79d9432d9ff2b9d448b99dc526
Juan Ramos <juan@lunarg.com>
2023-08-18 15:50:44 -0600

    build: Move build-gn to scripts/gn
    Less clutter in the top level directory

--- scripts/gn/update_deps.sh
+++ scripts/gn/update_deps.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# Copyright (c) 2019-2021 LunarG, Inc.
+# Copyright (c) 2019-2023 LunarG, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -15,17 +15,17 @@
 # limitations under the License.
 
 # Execute at repo root
-cd "$(dirname $0)/.."
+cd "$(dirname $0)/../../"
 
 # Use update_deps.py to update source dependencies from /scripts/known_good.json
 scripts/update_deps.py --dir="external" --no-build
 
-# Use gclient to update toolchain dependencies from /build-gn/DEPS (from chromium)
+# Use gclient to update toolchain dependencies from /scripts/gn/DEPS (from chromium)
 cat << EOF > .gclient
 solutions = [
   { "name"        : ".",
     "url"         : "https://github.com/KhronosGroup/Vulkan-ValidationLayers",
-    "deps_file"   : "build-gn/DEPS",
+    "deps_file"   : "scripts/gn/DEPS",
     "managed"     : False,
     "custom_deps" : {
     },
