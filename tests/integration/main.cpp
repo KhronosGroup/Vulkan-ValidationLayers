b0c43ab09ccd795467b1dfb1a6bc55aad4aeaf8f
Juan Ramos <juan@lunarg.com>
2023-08-25 13:20:53 -0600

    tests: Profiles no longer depends on VVL
    https://github.com/KhronosGroup/Vulkan-Profiles/pull/487

--- tests/integration/main.cpp
+++ tests/integration/main.cpp
@@ -3,7 +3,7 @@
 // NOTE: It needs to be in this exact location.
 #include <vulkan/vk_enum_string_helper.h>
 
-// Test other locations used by LunarG/VulkanTools and Vulkan-Profiles
+// Test other locations used by LunarG/VulkanTools
 #include <generated/vk_enum_string_helper.h>
 #include <vk_enum_string_helper.h>
 
@@ -16,10 +16,3 @@ static inline const char* foobar() { return string_VkResult(VK_SUCCESS); }
 #include <utils/vk_layer_utils.h>
 #include <generated/vk_dispatch_table_helper.h>
 #include <generated/vk_extension_helper.h>
-
-// Includes needed for Vulkan-Profiles
-// NOTE: Unlike LunarG/VulkanTools Profiles only uses header files.
-// https://github.com/KhronosGroup/Vulkan-Utility-Libraries/issues/13
-#include <vk_layer_config.h>
-#include <generated/vk_dispatch_table_helper.h>
-#include <utils/vk_layer_utils.h>
