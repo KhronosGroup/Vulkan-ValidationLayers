ccf47f962ac7ea406712a9ff4119a9da03dc5466
Juan Ramos <juan@lunarg.com>
2023-08-30 12:01:21 -0600

    cmake: Impending BUILD_LAYER_SUPPORT_FILES deprecation
    The utility libraries are ramping up and soon LunarG/VulkanTools
    will no longer depend on the validation layers.
    
    Update the integration/installation code to reflect
    progress made in LunarG/VulkanTools.
    
    We no longer need to install source files.

--- tests/integration/main.cpp
+++ tests/integration/main.cpp
@@ -2,5 +2,4 @@
 #include <containers/custom_containers.h>
 #include <utils/vk_layer_extension_utils.h>
 #include <utils/vk_layer_utils.h>
-#include <generated/vk_dispatch_table_helper.h>
 #include <generated/vk_extension_helper.h>
