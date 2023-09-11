52d8c85562549fced787ea07ca55c8463f2d3160
Juan Ramos <juan@lunarg.com>
2023-09-11 14:46:49 -0600

    cmake: Remove BUILD_LAYER_SUPPORT_FILES
    LunarG/VulkanTools no longer depends on the validation layers.
    
    See https://github.com/LunarG/VulkanTools/pull/1883
    
    BUILD_LAYERS and BUILD_LAYER_SUPPORT_FILES are removed as they
    are no longer needed.
    
    The corresponding integration tests are also no longer needed.
    
    closes #6246

--- tests/integration/main.cpp
+++
@@ -1,5 +0,0 @@
-#include <error_message/logging.h>
-#include <containers/custom_containers.h>
-#include <utils/vk_layer_extension_utils.h>
-#include <utils/vk_layer_utils.h>
-#include <generated/vk_extension_helper.h>
