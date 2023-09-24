7498aa29d2a5c24581117ef4f32679143980d71d
ziga-lunarg <ziga@lunarg.com>
2023-09-24 17:42:05 +0200

    layers: Fix trashed inherited viewports and scissors

--- layers/core_checks/cc_cmd_buffer.cpp
+++ layers/core_checks/cc_cmd_buffer.cpp
@@ -589,11 +589,11 @@ class CoreChecks::ViewportScissorInheritanceTracker {
         viewport_mask_ = primary_state.viewportMask | primary_state.viewportWithCountMask;
         scissor_mask_ = primary_state.scissorMask | primary_state.scissorWithCountMask;
 
-        for (uint32_t n = 0; n < primary_state.dynamic_state_value.viewports.size(); ++n) {
+        for (uint32_t n = 0; n < kMaxViewports; ++n) {
             uint32_t bit = uint32_t(1) << n;
             viewport_trashed_by_[n] = primary_state.trashedViewportMask & bit ? kTrashedByPrimary : kNotTrashed;
             scissor_trashed_by_[n] = primary_state.trashedScissorMask & bit ? kTrashedByPrimary : kNotTrashed;
-            if (viewport_mask_ & bit) {
+            if (n < primary_state.dynamic_state_value.viewports.size() && viewport_mask_ & bit) {
                 viewports_to_inherit_[n] = primary_state.dynamic_state_value.viewports[n];
             }
         }
@@ -630,10 +630,12 @@ class CoreChecks::ViewportScissorInheritanceTracker {
         viewport_mask_ |= secondary_state.viewportMask | secondary_state.viewportWithCountMask;
         scissor_mask_ |= secondary_state.scissorMask | secondary_state.scissorWithCountMask;
 
-        for (uint32_t n = 0; n < secondary_state.dynamic_state_value.viewports.size(); ++n) {
+        for (uint32_t n = 0; n < kMaxViewports; ++n) {
             uint32_t bit = uint32_t(1) << n;
             if ((secondary_state.viewportMask | secondary_state.viewportWithCountMask) & bit) {
-                viewports_to_inherit_[n] = secondary_state.dynamic_state_value.viewports[n];
+                if (n < secondary_state.dynamic_state_value.viewports.size()) {
+                    viewports_to_inherit_[n] = secondary_state.dynamic_state_value.viewports[n];
+                }
                 viewport_trashed_by_[n] = kNotTrashed;
             }
             if ((secondary_state.scissorMask | secondary_state.scissorWithCountMask) & bit) {
