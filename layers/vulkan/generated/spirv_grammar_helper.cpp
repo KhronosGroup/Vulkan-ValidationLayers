ea9b02ca43f371fcde34491d938993390feb1440
Juan Ramos <juan@lunarg.com>
2023-09-29 10:27:28 -0600

    cmake: Begin to allow clang on Windows
    Forwards the CMAKE_LANG_COMPILER variables in order to build
    everything properly
    
    Sets WIN32 defines properly. Since you can use clang on Windows.
    
    Fix warning found by clang on Windows regarding unused variable.

--- layers/vulkan/generated/spirv_grammar_helper.cpp
+++ layers/vulkan/generated/spirv_grammar_helper.cpp
@@ -1436,13 +1436,11 @@ static const char* string_SpvCooperativeMatrixOperandsMask(spv::CooperativeMatri
 
 std::string string_SpvCooperativeMatrixOperands(uint32_t mask) {
     std::string ret;
-    int index = 0;
     while (mask) {
         if (mask & 1) {
             if (!ret.empty()) ret.append("|");
             ret.append(string_SpvCooperativeMatrixOperandsMask(static_cast<spv::CooperativeMatrixOperandsMask>(1U << mask)));
         }
-        ++index;
         mask >>= 1;
     }
     if (ret.empty()) ret.append("CooperativeMatrixOperandsMask(0)");
