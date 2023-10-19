b3b70e9a7117170bb994f0087ea90f8acdcad92e
Juan Ramos <juan@lunarg.com>
2023-10-19 15:34:19 -0600

    iOS: Add iOS build to CI

--- layers/vk_layer_config.cpp
+++ layers/vk_layer_config.cpp
@@ -415,6 +415,33 @@ void PrintMessageType(VkFlags vk_flags, char *msg_flags) {
     }
 }
 
+// Ensure we are properly setting VK_USE_PLATFORM_METAL_EXT, VK_USE_PLATFORM_IOS_MVK, and VK_USE_PLATFORM_MACOS_MVK.
+#if __APPLE__
+
+#ifndef VK_USE_PLATFORM_METAL_EXT
+#error "VK_USE_PLATFORM_METAL_EXT not defined!"
+#endif
+
+#include <TargetConditionals.h>
+
+#if TARGET_OS_IOS
+
+#ifndef VK_USE_PLATFORM_IOS_MVK
+#error "VK_USE_PLATFORM_IOS_MVK not defined!"
+#endif
+
+#endif  //  TARGET_OS_IOS
+
+#if TARGET_OS_OSX
+
+#ifndef VK_USE_PLATFORM_MACOS_MVK
+#error "VK_USE_PLATFORM_MACOS_MVK not defined!"
+#endif
+
+#endif  // TARGET_OS_OSX
+
+#endif  // __APPLE__
+
 #ifdef VK_USE_PLATFORM_ANDROID_KHR
 
 // Require at least NDK 20 to build Validation Layers. Makes everything simpler to just have people building the layers to use a
