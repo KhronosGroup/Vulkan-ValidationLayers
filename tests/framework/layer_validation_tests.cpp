dd03f4d63e5410d15ec8049f07141f9b9c0f8136
Artem Kharytoniuk <artem@lunarg.com>
2023-11-23 16:13:25 +0100

    tests: Cleanup MSVC c-runtime tweaks
    Removes c-runtime configuration that is already handled by gtest.
    
    gtest internally disables MSVC assert message boxes and tweaks
    confirugation of c-runtime. Check UnitTest::Run() in gtest.cc.
    
    _CRT_WARN - is not handled by gtest, but _CRT_WARN does not create
    message boxes in case of warnings (it logs debug messages), that's why
    it was removed too.
    
    One part that has to be explicitly disabled by the VVL code is
    _CRT_ERROR, to not show message boxes in case of crashes.

--- tests/framework/layer_validation_tests.cpp
+++ tests/framework/layer_validation_tests.cpp
@@ -965,16 +965,8 @@ int main(int argc, char **argv) {
     int result;
 
 #if defined(_WIN32)
-#if !defined(NDEBUG)
-    _CrtSetReportMode(_CRT_WARN, _CRTDBG_MODE_FILE);
-    _CrtSetReportFile(_CRT_WARN, _CRTDBG_FILE_STDERR);
-    _CrtSetReportMode(_CRT_ASSERT, _CRTDBG_MODE_FILE);
-    _CrtSetReportFile(_CRT_ASSERT, _CRTDBG_FILE_STDERR);
-#endif
-    // Avoid "Abort, Retry, Ignore" dialog boxes
-    _set_error_mode(_OUT_TO_STDERR);
-    _set_abort_behavior(0, _WRITE_ABORT_MSG | _CALL_REPORTFAULT);
-    SetErrorMode(SEM_FAILCRITICALERRORS | SEM_NOGPFAULTERRORBOX | SEM_NOOPENFILEERRORBOX);
+    // Disable message box for: "Errors, unrecoverable problems, and issues that require immediate attention."
+    // This does not include asserts. GTest does similar configuration for asserts.
     _CrtSetReportMode(_CRT_ERROR, _CRTDBG_MODE_FILE);
     _CrtSetReportFile(_CRT_ERROR, _CRTDBG_FILE_STDERR);
 #endif
