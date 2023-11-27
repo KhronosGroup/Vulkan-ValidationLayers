2ca4dd0ad97a9c5f186b37936d43a0e462c95c89
Juan Ramos <juan@lunarg.com>
2023-11-27 13:27:04 -0700

    android: Remove redundant --tests argument from android.py
    Currently it's possible to build an APK with no testing.
    
    Despite this being invalid/pointless.

--- scripts/android.py
+++ scripts/android.py
@@ -84,7 +84,6 @@ def main():
     parser.add_argument('--app-abi', dest='android_abi', type=str, default="arm64-v8a")
     parser.add_argument('--app-stl', dest='android_stl', type=str, choices=["c++_static", "c++_shared"], default="c++_static")
     parser.add_argument('--apk', action='store_true', help='Generate an APK as a post build step.')
-    parser.add_argument('--tests', action='store_true', help='Build tests.')
     parser.add_argument('--clean', action='store_true', help='Cleans CMake build artifacts')
     args = parser.parse_args()
 
@@ -92,7 +91,6 @@ def main():
     android_abis = args.android_abi.split(" ")
     android_stl = args.android_stl
     create_apk = args.apk
-    build_tests = args.tests
     clean = args.clean
 
     if "ANDROID_NDK_HOME" not in os.environ:
@@ -157,7 +155,7 @@ def main():
         cmake_cmd += f' -D CMAKE_TOOLCHAIN_FILE={android_toolchain}'
         cmake_cmd += f' -D CMAKE_ANDROID_ARCH_ABI={abi}'
         cmake_cmd += f' -D CMAKE_INSTALL_LIBDIR={lib_dir}'
-        cmake_cmd += f' -D BUILD_TESTS={build_tests}'
+        cmake_cmd += f' -D BUILD_TESTS={create_apk}'
         cmake_cmd += f' -D CMAKE_ANDROID_STL_TYPE={android_stl}'
 
         cmake_cmd += ' -D ANDROID_PLATFORM=26'
