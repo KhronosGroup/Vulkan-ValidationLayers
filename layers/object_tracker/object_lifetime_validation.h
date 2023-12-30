24cc6aa8e73d641f32a0fb425ec49e9dc4708ed0
spencer-lunarg <spencer@lunarg.com>
2023-12-30 17:33:52 +0900

    layers: Improve Location for Object Tracker Utils

--- layers/object_tracker/object_lifetime_validation.h
+++ layers/object_tracker/object_lifetime_validation.h
@@ -115,7 +115,7 @@ class ObjectLifetimes : public ValidationObject {
     bool ValidateDescriptorWrite(VkWriteDescriptorSet const *desc, bool isPush, const Location &loc) const;
     bool ValidateAnonymousObject(uint64_t object, VkObjectType core_object_type, const char *invalid_handle_vuid,
                                  const char *wrong_parent_vuid, const Location &loc) const;
-    bool ValidateAccelerationStructures(const char *dst_handle_vuid, uint32_t count,
+    bool ValidateAccelerationStructures(const char *src_handle_vuid, const char *dst_handle_vuid, uint32_t count,
                                         const VkAccelerationStructureBuildGeometryInfoKHR *infos, const Location &loc) const;
 
     bool TracksObject(uint64_t object_handle, VulkanObjectType object_type) const;
