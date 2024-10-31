5f9bbe150137e246140233291ab9c3ab80fefac2
spencer-lunarg <spencer@lunarg.com>
2024-10-31 13:24:47 -0400

    gpu: Add PostProcessDescriptorIndexSlot

--- layers/gpu/resources/gpuav_shader_resources.h
+++ layers/gpu/resources/gpuav_shader_resources.h
@@ -70,6 +70,12 @@ struct BindingLayout {
     uint32_t state_start;
 };
 
+// Each indexing into a descriptor we have a 32-bit slot to mark what happend on the GPU
+// Since most devices can only support 32 descriptor sets, we can start to be clever and compress info into these 32-bits by
+// exploiting the fact certain data we are saving doesn't need a full 32-bit to save. GLSL doesn't have bitfields and don't want to
+// make this structs until we can't fit everything in 32-bits anymore.
+typedef uint32_t PostProcessDescriptorIndexSlot;
+
 // Represented as a uvec2 in the shader
 struct DescriptorState {
     DescriptorState() : id(0), extra_data(0) {}
--- layers/gpu/shaders/gpuav_shaders_constants.h
+++ layers/gpu/shaders/gpuav_shaders_constants.h
@@ -122,6 +122,9 @@ const int kDebugInputBuffAddrPtrOffset = 1;
 // not a valid buffer, the length associated with the 0x0 address is zero.
 const int kDebugInputBuffAddrLengthOffset = 0;
 
+// We store which set in last 5-bits because it can only be 0 to 31 (since devices don't support more than 32 sets and we have
+// warnings/assert if they start)
+const uint kDescriptorSetSelectionMask = 0x1F;
 // We use "0" as an initialization value, but the set could be "0" in SPIR-V
 // This mask lets know the descriptor was written to while saving the set value used.
 const uint kDescriptorSetWrittenMask = 1u << 31;
