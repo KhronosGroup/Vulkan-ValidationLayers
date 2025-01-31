3d7c347e03e5d7376cb369f2543b1a3b623f9562
spencer-lunarg <spencer@lunarg.com>
2025-01-31 08:47:33 -0500

    layers: Add Dedicated Buffer bind check

--- tests/unit/memory.cpp
+++ tests/unit/memory.cpp
@@ -2346,6 +2346,59 @@ TEST_F(NegativeMemory, MaxMemoryAllocationSize) {
     m_errorMonitor->VerifyFound();
 }
 
+TEST_F(NegativeMemory, RequiredDedicatedAllocationBuffer) {
+    SetTargetApiVersion(VK_API_VERSION_1_1);
+    RETURN_IF_SKIP(Init());
+
+    auto buffer_info = vkt::Buffer::CreateInfo(4096, VK_BUFFER_USAGE_VERTEX_BUFFER_BIT);
+    vkt::Buffer buffer(*m_device, buffer_info, vkt::no_mem);
+    vkt::Buffer buffer2(*m_device, buffer_info, VK_BUFFER_USAGE_TRANSFER_SRC_BIT | VK_BUFFER_USAGE_TRANSFER_DST_BIT);
+
+    VkBufferMemoryRequirementsInfo2 buffer_memory_requirements_info = vku::InitStructHelper();
+    buffer_memory_requirements_info.buffer = buffer;
+
+    VkMemoryDedicatedRequirements memory_dedicated_requirements = vku::InitStructHelper();
+    VkMemoryRequirements2 memory_requirements = vku::InitStructHelper(&memory_dedicated_requirements);
+    vk::GetBufferMemoryRequirements2(device(), &buffer_memory_requirements_info, &memory_requirements);
+    // TODO - hard to get this required for buffer, would be good to have way for TestICD to force it on for this test
+    if (!memory_dedicated_requirements.requiresDedicatedAllocation) {
+        GTEST_SKIP() << "requiresDedicatedAllocation is false";
+    }
+
+    {
+        VkMemoryAllocateInfo memory_info = vku::InitStructHelper();
+        memory_info.allocationSize = memory_requirements.memoryRequirements.size;
+        bool pass = m_device->Physical().SetMemoryType(memory_requirements.memoryRequirements.memoryTypeBits, &memory_info,
+                                                       VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT);
+        ASSERT_TRUE(pass);
+        vkt::DeviceMemory device_memory(*m_device, memory_info);
+
+        VkBindBufferMemoryInfo bind_buffer_memory_info = vku::InitStructHelper();
+        bind_buffer_memory_info.buffer = buffer;
+        bind_buffer_memory_info.memory = device_memory;
+        bind_buffer_memory_info.memoryOffset = 0u;
+        m_errorMonitor->SetDesiredError("VUID-VkBindBufferMemoryInfo-buffer-01444");
+        vk::BindBufferMemory2(device(), 1u, &bind_buffer_memory_info);
+        m_errorMonitor->VerifyFound();
+    }
+
+    {
+        VkMemoryDedicatedAllocateInfo memory_dedicated_allocate_info = vku::InitStructHelper();
+        memory_dedicated_allocate_info.buffer = buffer2;
+        VkMemoryAllocateInfo memory_info = vku::InitStructHelper(&memory_dedicated_allocate_info);
+        memory_info.allocationSize = memory_requirements.memoryRequirements.size;
+        bool pass = m_device->Physical().SetMemoryType(memory_requirements.memoryRequirements.memoryTypeBits, &memory_info,
+                                                       VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT);
+        ASSERT_TRUE(pass);
+        vkt::DeviceMemory device_memory(*m_device, memory_info);
+
+        m_errorMonitor->SetDesiredError("VUID-vkBindBufferMemory-memory-01508");
+        m_errorMonitor->SetDesiredError("VUID-vkBindBufferMemory-buffer-01444");
+        vk::BindBufferMemory(device(), buffer, device_memory, 0u);
+        m_errorMonitor->VerifyFound();
+    }
+}
+
 TEST_F(NegativeMemory, MapMemoryWithMapPlacedFlag) {
     TEST_DESCRIPTION("Test vkCmdUpdateBuffer");
 
