b256782810c54e15209b64476823010f57f10b2a
Artem Kharytoniuk <artem@lunarg.com>
2025-02-11 18:30:54 +0100

    sync: Test destination acceleration structure build

--- tests/unit/sync_val_ray_tracing_positive.cpp
+++ tests/unit/sync_val_ray_tracing_positive.cpp
@@ -30,3 +30,55 @@ TEST_F(PositiveSyncValRayTracing, BuildAccelerationStructure) {
     blas.BuildCmdBuffer(m_command_buffer);
     m_command_buffer.End();
 }
+
+TEST_F(PositiveSyncValRayTracing, WriteToAccelerationStructureBuffer) {
+    TEST_DESCRIPTION("Write to a vacant region of acceleration structure buffer during acceleration structure build");
+    RETURN_IF_SKIP(InitRayTracing());
+
+    vkt::as::BuildGeometryInfoKHR blas = vkt::as::blueprint::BuildGeometryInfoSimpleOnDeviceBottomLevel(*m_device);
+    blas.GetDstAS()->SetBufferUsageFlags(VK_BUFFER_USAGE_ACCELERATION_STRUCTURE_STORAGE_BIT_KHR | VK_BUFFER_USAGE_TRANSFER_DST_BIT);
+    // Reserve initial buffer region for non-acceleration-structure business.
+    // Acceleration structure contents start after it.
+    blas.GetDstAS()->SetOffset(256);
+    blas.SetupBuild(true);
+    const vkt::Buffer& accel_buffer = blas.GetDstAS()->GetBuffer();
+
+    m_command_buffer.Begin();
+    blas.VkCmdBuildAccelerationStructuresKHR(m_command_buffer);
+    // Write the answer into the first 4 bytes.
+    // This should not interfere with acceleration structure build accesses
+    vk::CmdFillBuffer(m_command_buffer, accel_buffer, 0, 4, 0x42);
+    m_command_buffer.End();
+}
+
+TEST_F(PositiveSyncValRayTracing, BuildAccelerationStructureThenBarrier) {
+    TEST_DESCRIPTION("Use barrier to wait for acceleration structure accesses");
+    SetTargetApiVersion(VK_API_VERSION_1_3);
+    AddRequiredFeature(vkt::Feature::synchronization2);
+    RETURN_IF_SKIP(InitRayTracing());
+
+    vkt::as::BuildGeometryInfoKHR blas = vkt::as::blueprint::BuildGeometryInfoSimpleOnDeviceBottomLevel(*m_device);
+    blas.GetDstAS()->SetBufferUsageFlags(VK_BUFFER_USAGE_ACCELERATION_STRUCTURE_STORAGE_BIT_KHR | VK_BUFFER_USAGE_TRANSFER_DST_BIT);
+    blas.SetupBuild(true);
+
+    const vkt::Buffer& accel_buffer = blas.GetDstAS()->GetBuffer();
+
+    VkBufferMemoryBarrier2 barrier = vku::InitStructHelper();
+    barrier.srcStageMask = VK_PIPELINE_STAGE_2_ACCELERATION_STRUCTURE_BUILD_BIT_KHR;
+    barrier.srcAccessMask = VK_ACCESS_2_ACCELERATION_STRUCTURE_READ_BIT_KHR | VK_ACCESS_2_ACCELERATION_STRUCTURE_WRITE_BIT_KHR;
+    barrier.dstStageMask = VK_PIPELINE_STAGE_2_CLEAR_BIT;
+    barrier.dstAccessMask = VK_ACCESS_2_TRANSFER_WRITE_BIT;
+    barrier.buffer = accel_buffer;
+    barrier.offset = 0;
+    barrier.size = 4;
+
+    VkDependencyInfo dep_info = vku::InitStructHelper();
+    dep_info.bufferMemoryBarrierCount = 1;
+    dep_info.pBufferMemoryBarriers = &barrier;
+
+    m_command_buffer.Begin();
+    blas.VkCmdBuildAccelerationStructuresKHR(m_command_buffer);
+    vk::CmdPipelineBarrier2(m_command_buffer, &dep_info);
+    vk::CmdFillBuffer(m_command_buffer, accel_buffer, 0, 4, 0x80386);
+    m_command_buffer.End();
+}
