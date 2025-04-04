e9c77bde5df0b87304d9c1b00477c1442a87e40f
ziga-lunarg <ziga@lunarg.com>
2025-04-04 17:14:14 +0300

    tests: Add missing unordered map include

--- tests/framework/sync_helper.h
+++ tests/framework/sync_helper.h
@@ -13,6 +13,7 @@
 
 #include "layer_validation_tests.h"
 #include "containers/limits.h"
+#include <unordered_map>
 
 VkDependencyInfo DependencyInfo(const VkMemoryBarrier2 &barrier, VkDependencyFlags dependency_flags = 0);
 VkDependencyInfo DependencyInfo(const VkBufferMemoryBarrier2 &buffer_barrier, VkDependencyFlags dependency_flags = 0);
