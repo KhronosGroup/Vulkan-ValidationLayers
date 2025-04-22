9cbcd17739b6932938d21ae7a0f1881187b3e14f
spencer-lunarg <spencer@lunarg.com>
2025-04-22 12:31:12 -0400

    gpuav: Move VertexAttributeFetchOff to shared resource

--- layers/gpuav/spirv/vertex_attribute_fetch_oob.h
+++ layers/gpuav/spirv/vertex_attribute_fetch_oob.h
@@ -20,7 +20,40 @@
 #include <stdint.h>
 #include "pass.h"
 
+#include <vulkan/utility/vk_struct_helper.hpp>
+#include "gpuav/resources/gpuav_vulkan_objects.h"
+
 namespace gpuav {
+
+// Vertex Attribute Fetch OOB checks are for Indexed draws, so when using another draw, we set the values of the small buffer to
+// zero to indicate the instrumented vertex shader to skip validating the limits. We have a single global buffer that we can have
+// all non-index draws point at.
+struct VertexAttributeFetchOff {
+    vko::Buffer buffer;
+    bool valid = false;
+
+    VertexAttributeFetchOff(Validator &gpuav, const Location &loc) : buffer(gpuav) {
+        VkBufferCreateInfo buffer_info = vku::InitStructHelper();
+        buffer_info.size = 4 * sizeof(uint32_t);
+        buffer_info.usage = VK_BUFFER_USAGE_STORAGE_BUFFER_BIT;
+        VmaAllocationCreateInfo alloc_info = {};
+        alloc_info.requiredFlags = VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT;
+        alloc_info.preferredFlags = VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT;
+        const bool success = buffer.Create(loc, &buffer_info, &alloc_info);
+        if (!success) {
+            valid = false;
+            return;
+        }
+        valid = true;
+
+        auto vertex_attribute_fetch_limits_buffer_ptr = (uint32_t *)buffer.GetMappedPtr();
+        vertex_attribute_fetch_limits_buffer_ptr[0] = 0u;  // has_max_vbb_vertex_input_rate
+        vertex_attribute_fetch_limits_buffer_ptr[2] = 0u;  // has_max_vbb_instance_input_rate
+    }
+
+    ~VertexAttributeFetchOff() { buffer.Destroy(); }
+};
+
 namespace spirv {
 
 // Validating validating that gl_VertexID is an index within bound vertex buffers
