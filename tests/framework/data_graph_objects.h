102c30690b1e84e58ad659a249dbb819dcc7f926
spencer-lunarg <spencer@lunarg.com>
2025-09-25 14:40:10 -0400

    layers: Nit fixes for data graphs

--- tests/framework/data_graph_objects.h
+++ tests/framework/data_graph_objects.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015-2024 The Khronos Group Inc.
+ * Copyright (c) 2025 The Khronos Group Inc.
  * Copyright (C) 2025 Arm Limited.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
