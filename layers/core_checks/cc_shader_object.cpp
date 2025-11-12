248f71b76dc5cd3ffaa29cbc813a93f5db3ab1e7
spencer-lunarg <spencer@lunarg.com>
2025-11-12 14:26:43 -0500

    layers: Fix Mesh ShaderObject with DGC

--- layers/core_checks/cc_shader_object.cpp
+++ layers/core_checks/cc_shader_object.cpp
@@ -848,7 +848,9 @@ bool CoreChecks::ValidateDrawShaderObjectMesh(const LastBound& last_bound_state,
     const bool has_task_shader = task_shader_handle != VK_NULL_HANDLE;
     const bool has_mesh_shader = mesh_shader_handle != VK_NULL_HANDLE;
 
-    const bool is_mesh_command = vvl::IsCommandDrawMesh(vuid.function);
+    // TODO - We could check (and report) if vkCmdExecuteGeneratedCommandsEXT has a mesh token actually
+    const bool is_mesh_command =
+        vvl::IsCommandDrawMesh(vuid.function) || vuid.function == vvl::Func::vkCmdExecuteGeneratedCommandsEXT;
 
     if (has_task_shader || has_mesh_shader) {
         auto print_mesh_task = [this, has_task_shader, has_mesh_shader, mesh_shader_handle, task_shader_handle]() {
