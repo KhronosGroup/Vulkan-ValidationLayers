6c9f83e16f207d9ee135f85a30752c17d840f6c1
spencer-lunarg <spencer@lunarg.com>
2025-11-21 13:31:42 -0500

    gpuav: Add rest of Divide By Zero types

--- layers/gpuav/spirv/sanitizer_pass.cpp
+++ layers/gpuav/spirv/sanitizer_pass.cpp
@@ -34,25 +34,72 @@ SanitizerPass::SanitizerPass(Module& module) : Pass(module, kOfflineModule) {}
 // By appending the LinkInfo, it will attempt at linking stage to add the function.
 uint32_t SanitizerPass::GetLinkFunctionId() { return GetLinkFunction(link_function_id_, kOfflineFunction); }
 
+// We do the check for zero in C++ to make it easier to handle the various cases of signed/unsigned/64bit/etc
+// Returns an ID of type OpTypeBool
+uint32_t SanitizerPass::DivideByZeroCheck(BasicBlock& block, InstructionIt* inst_it, const InstructionMeta& meta) {
+    const Type& bool_type = type_manager_.GetTypeBool();
+    const uint32_t vector_size = meta.result_type->VectorSize();
+    const uint32_t divisor_id = meta.target_instruction->Word(4);
+
+    if (vector_size == 0) {
+        const uint32_t null_type_id = type_manager_.GetConstantNull(*meta.result_type).Id();
+        const uint32_t compare_id = module_.TakeNextId();
+        block.CreateInstruction(spv::OpIEqual, {bool_type.Id(), compare_id, null_type_id, divisor_id}, inst_it);
+        return compare_id;
+    } else {
+        const Type& bool_vector_type = type_manager_.GetTypeVector(bool_type, vector_size);
+        const uint32_t zero_id = type_manager_.GetConstantZeroVector(*meta.result_type).Id();
+
+        const uint32_t compare_id = module_.TakeNextId();
+        block.CreateInstruction(spv::OpIEqual, {bool_vector_type.Id(), compare_id, zero_id, divisor_id}, inst_it);
+
+        const uint32_t any_id = module_.TakeNextId();
+        block.CreateInstruction(spv::OpAny, {bool_type.Id(), any_id, compare_id}, inst_it);
+        return any_id;
+    }
+}
+
 uint32_t SanitizerPass::CreateFunctionCall(BasicBlock& block, InstructionIt* inst_it, const InstructionMeta& meta) {
     const uint32_t function_result = module_.TakeNextId();
     const uint32_t function_def = GetLinkFunctionId();
-    const uint32_t bool_type = module_.type_manager_.GetTypeBool().Id();
+    const uint32_t bool_type = type_manager_.GetTypeBool().Id();
 
     const uint32_t inst_position = meta.target_instruction->GetPositionOffset();
-    const uint32_t inst_position_id = module_.type_manager_.CreateConstantUInt32(inst_position).Id();
+    const uint32_t inst_position_id = type_manager_.CreateConstantUInt32(inst_position).Id();
 
-    uint32_t divisor_id = meta.target_instruction->Word(4);
-    if (meta.target_instruction->Opcode() == spv::OpSDiv) {
-        const Type& uint32_type = module_.type_manager_.GetTypeInt(32, false);
-        const uint32_t new_id = module_.TakeNextId();
-        block.CreateInstruction(spv::OpBitcast, {uint32_type.Id(), new_id, divisor_id}, inst_it);
-        divisor_id = new_id;
-    }
+    uint32_t is_valid_id = DivideByZeroCheck(block, inst_it, meta);
 
-    block.CreateInstruction(spv::OpFunctionCall, {bool_type, function_result, function_def, inst_position_id, divisor_id}, inst_it);
+    block.CreateInstruction(spv::OpFunctionCall, {bool_type, function_result, function_def, is_valid_id, inst_position_id},
+                            inst_it);
     module_.need_log_error_ = true;
     return function_result;
+}
+
+bool SanitizerPass::IsConstantZero(const Constant& constant) const {
+    if (constant.is_spec_constant_) {
+        // TODO - We have the spec constants information, we just need to pipe it into the passes
+        return false;
+    }
+    const spv::Op opcode = (spv::Op)constant.inst_.Opcode();
+    if (opcode == spv::OpConstantNull || opcode == spv::OpConstantFalse) {
+        return true;
+    } else if (opcode == spv::OpConstant) {
+        // This works for signed ints and floats because zero is same for all
+        if (constant.type_.Is64Bit()) {
+            return constant.inst_.Word(3) == 0 && constant.inst_.Word(4) == 0;
+        } else {
+            return constant.inst_.Word(3) == 0;
+        }
+    } else if (opcode == spv::OpConstantComposite) {
+        const size_t constituent_count = constant.inst_.Length() - 3;
+        for (uint32_t i = 0; i < constituent_count; i++) {
+            const Constant* component_constant = type_manager_.FindConstantById(constant.inst_.Operand(i));
+            if (component_constant && IsConstantZero(*component_constant)) {
+                return true;
+            }
+        }
+    }
+    return false;
 }
 
 bool SanitizerPass::RequiresInstrumentation(const Instruction& inst, InstructionMeta& meta) {
@@ -60,21 +107,15 @@ bool SanitizerPass::RequiresInstrumentation(const Instruction& inst, Instruction
 
     if (IsValueIn(opcode, {spv::OpUDiv, spv::OpSDiv})) {
         // Note - It is valid to divide by zero for a float (you get NaN), but invalid for an int.
-        //
-        // If its a constant, no reason to instrument, unless its a constant value of zero,
-        // then it is only invalid if executed on the GPU.
-        if (const Constant* constant = module_.type_manager_.FindConstantById(inst.Word(4))) {
-            if (constant->GetValueUint32() != 0) {
+        if (const Constant* constant = type_manager_.FindConstantById(inst.Word(4))) {
+            // If its a constant, no reason to instrument, unless its a constant value of zero,
+            // then it is only invalid if executed on the GPU.
+            // (Tried to add to spirv-val, decided it should be a warning at most in VVL)
+            if (!IsConstantZero(*constant)) {
                 return false;
             }
         }
-        meta.result_type = module_.type_manager_.FindTypeById(inst.TypeId());
-        if (meta.result_type->VectorSize() > 0) {
-            // TODO - Figure out how dividing by a vector of zero is suppose to work
-            // https://gitlab.khronos.org/spirv/SPIR-V/-/issues/900
-            return false;
-        }
-
+        meta.result_type = type_manager_.FindTypeById(inst.TypeId());
         meta.target_instruction = &inst;
         return true;
     }
