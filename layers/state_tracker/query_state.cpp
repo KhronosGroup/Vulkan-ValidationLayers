39d5a1018f4d38724d4698e0d4942c65729a1002
ziga-lunarg <ziga@lunarg.com>
2026-01-16 22:18:53 +0200

    layers: Fix setting subpass in queries

--- layers/state_tracker/query_state.cpp
+++ layers/state_tracker/query_state.cpp
@@ -1,6 +1,6 @@
-/* Copyright (c) 2015-2025 The Khronos Group Inc.
- * Copyright (c) 2015-2025 Valve Corporation
- * Copyright (c) 2015-2025 LunarG, Inc.
+/* Copyright (c) 2015-2026 The Khronos Group Inc.
+ * Copyright (c) 2015-2026 Valve Corporation
+ * Copyright (c) 2015-2026 LunarG, Inc.
  * Copyright (C) 2015-2025 Google Inc.
  * Modifications Copyright (C) 2020 Advanced Micro Devices, Inc. All rights reserved.
  * Modifications Copyright (C) 2022 RasterGrid Kft.
@@ -103,7 +103,7 @@ QueryResultType QueryPool::GetQueryResultType(QueryState state, VkQueryResultFla
 
 QueryCount::QueryCount(vvl::CommandBuffer &cb_state) {
     count = 1;
-    subpass = 0;
+    subpass = cb_state.GetActiveSubpass();
     inside_render_pass = cb_state.active_render_pass != nullptr;
     // If render pass instance has multiview enabled, query uses N consecutive query indices
     if (inside_render_pass) {
