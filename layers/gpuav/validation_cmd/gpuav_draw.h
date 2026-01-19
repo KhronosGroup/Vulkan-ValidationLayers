3e456194f8b4c2187e6d99d12e147b76a8d01a79
arno-lunarg <111733687+arno-lunarg@users.noreply.github.com>
2026-01-19 16:06:37 +0100

    gpuav: Add more firstIntance validation (#11488)

--- layers/gpuav/validation_cmd/gpuav_draw.h
+++ layers/gpuav/validation_cmd/gpuav_draw.h
@@ -1,6 +1,6 @@
-/* Copyright (c) 2018-2025 The Khronos Group Inc.
- * Copyright (c) 2018-2025 Valve Corporation
- * Copyright (c) 2018-2025 LunarG, Inc.
+/* Copyright (c) 2018-2026 The Khronos Group Inc.
+ * Copyright (c) 2018-2026 Valve Corporation
+ * Copyright (c) 2018-2026 LunarG, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -20,6 +20,7 @@
 #include <vulkan/vulkan.h>
 
 #include "generated/error_location_helper.h"
+#include "external/inplace_function.h"
 
 struct Location;
 struct LastBound;
@@ -32,25 +33,31 @@ namespace valcmd {
 
 void FlushValidationCmds(Validator &gpuav, CommandBufferSubState &cb_state);
 
+// Note: For lifetime reasons, `vuid` *has* to be statically allocated.
 template <typename IndirectCommand>
 void FirstInstance(Validator &gpuav, CommandBufferSubState &cb_state, const Location &loc, const LastBound &last_bound,
                    VkBuffer buffer, VkDeviceSize offset, uint32_t draw_count, VkBuffer count_buffer,
-                   VkDeviceSize count_buffer_offset, const char *vuid);
+                   VkDeviceSize count_buffer_offset);
 
 template <>
 void FirstInstance<VkDrawIndirectCommand>(Validator &gpuav, CommandBufferSubState &cb_state, const Location &loc,
                                           const LastBound &last_bound, VkBuffer buffer, VkDeviceSize offset, uint32_t draw_count,
-                                          VkBuffer count_buffer, VkDeviceSize count_buffer_offset, const char *vuid);
+                                          VkBuffer count_buffer, VkDeviceSize count_buffer_offset);
 template <>
 void FirstInstance<VkDrawIndexedIndirectCommand>(Validator &gpuav, CommandBufferSubState &cb_state, const Location &loc,
                                                  const LastBound &last_bound, VkBuffer buffer, VkDeviceSize offset,
-                                                 uint32_t draw_count, VkBuffer count_buffer, VkDeviceSize count_buffer_offset,
-                                                 const char *vuid);
+                                                 uint32_t draw_count, VkBuffer count_buffer, VkDeviceSize count_buffer_offset);
 
+// Use "api_" prefix to make it clear which buffer/offset/etc we are talking about
+// "api" helps to distinguish it is input from the user at the API level
+
+// Given rendering context, select the right "firstInstance VUID" to validate. Returns nullptr if there is nothing to validate.
+using FirstInstanceValidationVuidSelector =
+    stdext::inplace_function<const char *(Validator &gpuav, CommandBufferSubState &cb_state, const LastBound &last_bound)>;
 void FirstInstance(Validator &gpuav, CommandBufferSubState &cb_state, const Location &loc, const LastBound &last_bound,
                    VkBuffer api_buffer, VkDeviceSize api_offset, uint32_t api_stride, vvl::Struct api_struct_name,
                    uint32_t first_instance_member_pos, uint32_t api_draw_count, VkBuffer api_count_buffer,
-                   VkDeviceSize api_count_buffer_offset, const char *vuid);
+                   VkDeviceSize api_count_buffer_offset, const FirstInstanceValidationVuidSelector &vuid_selector);
 
 void CountBuffer(Validator &gpuav, CommandBufferSubState &cb_state, const Location &loc, const LastBound &last_bound,
                  VkBuffer api_buffer, VkDeviceSize api_offset, uint32_t api_struct_size_byte, vvl::Struct api_struct_name,
