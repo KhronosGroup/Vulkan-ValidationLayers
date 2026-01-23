c5978c0ee66180c2f9d7a58ffae13bac697beb1e
ziga-lunarg <ziga@lunarg.com>
2026-01-23 14:31:33 +0200

    tests: Test internally synchronized queues

--- tests/unit/device_queue_positive.cpp
+++ tests/unit/device_queue_positive.cpp
@@ -1,6 +1,6 @@
 /*
- * Copyright (c) 2023-2025 Valve Corporation
- * Copyright (c) 2023-2025 LunarG, Inc.
+ * Copyright (c) 2023-2026 Valve Corporation
+ * Copyright (c) 2023-2026 LunarG, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -41,4 +41,54 @@ TEST_F(PositiveDeviceQueue, NoQueues) {
     VkPipeline pipeline;
     vk::CreateComputePipelines(device, VK_NULL_HANDLE, 1, &compute_create_info, nullptr, &pipeline);
     vk::DestroyPipeline(device, pipeline, nullptr);
+}
+
+TEST_F(PositiveDeviceQueue, QueueFamilyIndexDifferentCreateFlags) {
+    SetTargetApiVersion(VK_API_VERSION_1_1);
+    AddRequiredExtensions(VK_KHR_INTERNALLY_SYNCHRONIZED_QUEUES_EXTENSION_NAME);
+    AddRequiredFeature(vkt::Feature::internallySynchronizedQueues);
+    RETURN_IF_SKIP(InitFramework());
+
+    bool found_queue = false;
+    VkQueueFamilyProperties queue_properties;
+    uint32_t queue_family_index = 0u;
+    uint32_t queue_family_count = 0u;
+    vk::GetPhysicalDeviceQueueFamilyProperties(Gpu(), &queue_family_count, nullptr);
+    std::vector<VkQueueFamilyProperties> queue_families(queue_family_count);
+    vk::GetPhysicalDeviceQueueFamilyProperties(Gpu(), &queue_family_count, queue_families.data());
+
+    for (size_t i = 0; i < queue_families.size(); i++) {
+        if (queue_families[i].queueCount > 1) {
+            found_queue = true;
+            queue_family_index = i;
+            queue_properties = queue_families[i];
+            break;
+        }
+    }
+
+    if (found_queue == false) {
+        GTEST_SKIP() << "test requires queue family with 2 queues, not available";
+    }
+
+    float queue_priority = 1.0f;
+    VkDeviceQueueCreateInfo device_queue_ci[2];
+    device_queue_ci[0] = vku::InitStructHelper();
+    device_queue_ci[0].flags = 0u;
+    device_queue_ci[0].queueFamilyIndex = queue_family_index;
+    device_queue_ci[0].queueCount = 1u;
+    device_queue_ci[0].pQueuePriorities = &queue_priority;
+    device_queue_ci[1] = vku::InitStructHelper();
+    device_queue_ci[1].flags = VK_DEVICE_QUEUE_CREATE_INTERNALLY_SYNCHRONIZED_BIT_KHR;
+    device_queue_ci[1].queueFamilyIndex = queue_family_index;
+    device_queue_ci[1].queueCount = 1u;
+    device_queue_ci[1].pQueuePriorities = &queue_priority;
+
+    VkDevice test_device = VK_NULL_HANDLE;
+    VkDeviceCreateInfo device_ci = vku::InitStructHelper();
+    device_ci.queueCreateInfoCount = 2u;
+    device_ci.pQueueCreateInfos = device_queue_ci;
+    device_ci.enabledExtensionCount = static_cast<uint32_t>(m_device_extension_names.size());
+    device_ci.ppEnabledExtensionNames = m_device_extension_names.data();
+    vk::CreateDevice(Gpu(), &device_ci, nullptr, &test_device);
+    vk::DestroyDevice(test_device, nullptr);
 }
