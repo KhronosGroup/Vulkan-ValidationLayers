e5df0db3c7cd385bb683cc907aef95ba90b5f125
ziga-lunarg <ziga@lunarg.com>
2026-03-07 16:43:56 +0200

    tests: Test missing ISQ feature

--- tests/unit/device_queue.cpp
+++ tests/unit/device_queue.cpp
@@ -554,3 +554,53 @@ TEST_F(NegativeDeviceQueue, DISABLED_DeviceCreateEnabledExtensionNamesPointer) {
     vk::CreateDevice(Gpu(), &device_ci, nullptr, &device);
     m_errorMonitor->VerifyFound();
 }
+
+TEST_F(NegativeDeviceQueue, MissingInternallySynchronizedQueuesFeature) {
+    SetTargetApiVersion(VK_API_VERSION_1_1);
+    AddRequiredExtensions(VK_KHR_INTERNALLY_SYNCHRONIZED_QUEUES_EXTENSION_NAME);
+    RETURN_IF_SKIP(InitFramework());
+
+    bool found_queue = false;
+    VkQueueFamilyProperties queue_properties;
+    uint32_t queue_family_index = 0u;
+    uint32_t queue_family_count = 0u;
+    vk::GetPhysicalDeviceQueueFamilyProperties(Gpu(), &queue_family_count, nullptr);
+    std::vector<VkQueueFamilyProperties> queue_families(queue_family_count);
+    vk::GetPhysicalDeviceQueueFamilyProperties(Gpu(), &queue_family_count, queue_families.data());
+
+    for (size_t i = 0; i < queue_families.size(); i++) {
+        if (queue_families[i].queueCount > 1) {
+            found_queue = true;
+            queue_family_index = i;
+            queue_properties = queue_families[i];
+            break;
+        }
+    }
+
+    if (found_queue == false) {
+        GTEST_SKIP() << "test requires queue family with 2 queues, not available";
+    }
+
+    float queue_priority = 1.0f;
+    VkDeviceQueueCreateInfo device_queue_ci[2];
+    device_queue_ci[0] = vku::InitStructHelper();
+    device_queue_ci[0].flags = 0u;
+    device_queue_ci[0].queueFamilyIndex = queue_family_index;
+    device_queue_ci[0].queueCount = 1u;
+    device_queue_ci[0].pQueuePriorities = &queue_priority;
+    device_queue_ci[1] = vku::InitStructHelper();
+    device_queue_ci[1].flags = VK_DEVICE_QUEUE_CREATE_INTERNALLY_SYNCHRONIZED_BIT_KHR;
+    device_queue_ci[1].queueFamilyIndex = queue_family_index;
+    device_queue_ci[1].queueCount = 1u;
+    device_queue_ci[1].pQueuePriorities = &queue_priority;
+
+    VkDevice test_device = VK_NULL_HANDLE;
+    VkDeviceCreateInfo device_ci = vku::InitStructHelper();
+    device_ci.queueCreateInfoCount = 2u;
+    device_ci.pQueueCreateInfos = device_queue_ci;
+    device_ci.enabledExtensionCount = static_cast<uint32_t>(m_device_extension_names.size());
+    device_ci.ppEnabledExtensionNames = m_device_extension_names.data();
+    m_errorMonitor->SetDesiredError("VUID-VkDeviceQueueCreateInfo-internallySynchronizedQueues-12348");
+    vk::CreateDevice(Gpu(), &device_ci, nullptr, &test_device);
+    m_errorMonitor->VerifyFound();
+}
