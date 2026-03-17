ceee69e3d9252c172d1a2bdb1bb3fa8bff3b72e7
Stefano Bucciarelli <stefano.bucciarelli@arm.com>
2026-03-17 15:37:30 +0000

    tests: Add <cmath> in RT test files
    - on some systems the missing header was causing a build error:
    error: 'NAN' was not declared in this scope

--- tests/unit/gpu_av_ray_tracing_positive.cpp
+++ tests/unit/gpu_av_ray_tracing_positive.cpp
@@ -11,6 +11,7 @@
  *     http://www.apache.org/licenses/LICENSE-2.0
  */
 
+#include <cmath>
 #include "../framework/layer_validation_tests.h"
 #include "../framework/ray_tracing_objects.h"
 #include "../framework/descriptor_helper.h"
