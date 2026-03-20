fb11604fa3bec438c2532c371464c2d5c0b27317
Jeff Bolz <jbolz@nvidia.com>
2026-03-20 13:26:09 -0500

    gpuav: Test fixes, don't disable datarace

--- tests/unit/gpu_av_shared_memory_oob_positive.cpp
+++ tests/unit/gpu_av_shared_memory_oob_positive.cpp
@@ -15,8 +15,7 @@
 
 void GpuAVSharedMemoryOobTest::InitSharedMemoryOob() {
     SetTargetApiVersion(VK_API_VERSION_1_2);
-    RETURN_IF_SKIP(InitGpuAvFramework(
-        {{OBJECT_LAYER_NAME, "gpuav_shared_memory_data_race", VK_LAYER_SETTING_TYPE_BOOL32_EXT, 1, &kVkFalse}}, false));
+    RETURN_IF_SKIP(InitGpuAvFramework());
     RETURN_IF_SKIP(InitState());
 }
 
@@ -238,17 +237,63 @@ TEST_F(PositiveGpuAVSharedMemoryOob, SlangNestedStruct) {
 }
 
 TEST_F(PositiveGpuAVSharedMemoryOob, VectorInsertInBounds) {
-    const char* shader_source = R"glsl(
-        #version 450
-        layout(local_size_x = 4) in;
-        layout(set = 0, binding = 0) buffer StorageBuffer { uint data[]; } ssbo;
-        shared vec4 v;
-        void main() {
-            v[gl_LocalInvocationIndex % 4] = float(gl_LocalInvocationIndex);
-            barrier();
-            ssbo.data[gl_LocalInvocationIndex] = floatBitsToUint(v[gl_LocalInvocationIndex % 4]);
-        }
-    )glsl";
+    const char* shader_source = R"spirv(
+               OpCapability Shader
+               OpMemoryModel Logical GLSL450
+               OpEntryPoint GLCompute %main "main" %ssbo %v
+               OpExecutionMode %main LocalSize 1 1 1
+               OpDecorate %runtime_arr ArrayStride 4
+               OpMemberDecorate %ssbo_struct 0 Offset 0
+               OpDecorate %ssbo_struct Block
+               OpDecorate %ssbo DescriptorSet 0
+               OpDecorate %ssbo Binding 0
+       %void = OpTypeVoid
+    %void_fn = OpTypeFunction %void
+       %uint = OpTypeInt 32 0
+      %float = OpTypeFloat 32
+    %v4float = OpTypeVector %float 4
+     %uint_0 = OpConstant %uint 0
+     %uint_3 = OpConstant %uint 3
+    %float_1 = OpConstant %float 1
+    %float_2 = OpConstant %float 2
+%runtime_arr = OpTypeRuntimeArray %uint
+%ssbo_struct = OpTypeStruct %runtime_arr
+   %ssbo_ptr = OpTypePointer StorageBuffer %ssbo_struct
+   %elem_ptr = OpTypePointer StorageBuffer %uint
+       %ssbo = OpVariable %ssbo_ptr StorageBuffer
+     %wg_ptr = OpTypePointer Workgroup %v4float
+          %v = OpVariable %wg_ptr Workgroup
+       %main = OpFunction %void None %void_fn
+      %entry = OpLabel
+   %init_vec = OpCompositeConstruct %v4float %float_1 %float_1 %float_1 %float_1
+               OpStore %v %init_vec
+   %loaded_v = OpLoad %v4float %v
+   %inserted = OpVectorInsertDynamic %v4float %loaded_v %float_2 %uint_3
+               OpStore %v %inserted
+      %chain = OpAccessChain %elem_ptr %ssbo %uint_0 %uint_0
+       %cast = OpBitcast %uint %float_2
+               OpStore %chain %cast
+               OpReturn
+               OpFunctionEnd
+    )spirv";
 
-    TestHelper(shader_source);
+    RETURN_IF_SKIP(InitSharedMemoryOob());
+
+    CreateComputePipelineHelper pipe(*this);
+    pipe.dsl_bindings_[0] = {0, VK_DESCRIPTOR_TYPE_STORAGE_BUFFER, 1, VK_SHADER_STAGE_ALL, nullptr};
+    pipe.cs_ = VkShaderObj(*m_device, shader_source, VK_SHADER_STAGE_COMPUTE_BIT, SPV_ENV_VULKAN_1_2, SPV_SOURCE_ASM);
+    pipe.CreateComputePipeline();
+
+    vkt::Buffer in_buffer(*m_device, 32, VK_BUFFER_USAGE_STORAGE_BUFFER_BIT);
+    pipe.descriptor_set_.WriteDescriptorBufferInfo(0, in_buffer, 0, VK_WHOLE_SIZE, VK_DESCRIPTOR_TYPE_STORAGE_BUFFER);
+    pipe.descriptor_set_.UpdateDescriptorSets();
+
+    m_command_buffer.Begin();
+    vk::CmdBindPipeline(m_command_buffer, VK_PIPELINE_BIND_POINT_COMPUTE, pipe);
+    vk::CmdBindDescriptorSets(m_command_buffer, VK_PIPELINE_BIND_POINT_COMPUTE, pipe.pipeline_layout_, 0, 1,
+                              &pipe.descriptor_set_.set_, 0, nullptr);
+    vk::CmdDispatch(m_command_buffer, 1, 1, 1);
+    m_command_buffer.End();
+
+    m_default_queue->SubmitAndWait(m_command_buffer);
 }
