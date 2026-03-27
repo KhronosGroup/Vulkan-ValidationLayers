a1a142a93c22e234410e73b17650b8c921c9d713
arno-lunarg <arno@lunarg.com>
2026-03-27 13:04:55 +0100

    gpuav: Improve GetASByAddress
    Remove needless reserve

--- layers/state_tracker/state_tracker.cpp
+++ layers/state_tracker/state_tracker.cpp
@@ -6560,7 +6560,6 @@ small_vector<const vvl::AccelerationStructureKHR*, 2> DeviceState::GetAccelerati
     if (address == 0) {
         return as_vector;
     }
-    as_vector.reserve(static_cast<uint32_t>(as_with_addresses.array.size()));
 
     ReadLockGuard lock(as_with_addresses.array_mutex);
     auto as_found_it =
