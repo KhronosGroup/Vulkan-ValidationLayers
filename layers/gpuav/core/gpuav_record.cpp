dee3be1dde1a4f66251b13d57fe99e64cd671403
spencer-lunarg <spencer@lunarg.com>
2026-06-02 17:31:49 -0400

    gpuav: Remove DebugDescriptor idea

--- layers/gpuav/core/gpuav_record.cpp
+++ layers/gpuav/core/gpuav_record.cpp
@@ -20,7 +20,6 @@
 #include "generated/dispatch_functions.h"
 #include "gpuav/core/gpuav.h"
 #include "gpuav/core/gpuav_constants.h"
-#include "gpuav/debug_descriptor/debug_descriptor.h"
 #include "gpuav/debug_printf/debug_printf.h"
 #include "gpuav/descriptor_validation/gpuav_descriptor_validation.h"
 #include "gpuav/instrumentation/descriptor_checks_classic.h"
@@ -262,7 +261,6 @@ void Validator::PreCallRecordBeginCommandBuffer(VkCommandBuffer commandBuffer, c
     RegisterSanitizer(*this, gpuav_cb_state);
     RegisterTraceRayValidation(*this, gpuav_cb_state);
     debug_printf::RegisterDebugPrintf(*this, gpuav_cb_state);
-    debug_descriptor::RegisterDebugDescriptor(*this, gpuav_cb_state);
 }
 
 // Dedicated warning VUID that likely can be ignored.
