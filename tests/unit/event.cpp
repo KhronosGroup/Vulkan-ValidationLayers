7417d3e5da29d91a3d7c51e4eef96311dff9bc8d
Artem Kharytoniuk <artem@lunarg.com>
2026-06-24 15:39:57 +0200

    layers: Fix wait event dependency flags validation

--- tests/unit/event.cpp
+++ tests/unit/event.cpp
@@ -770,7 +770,7 @@ TEST_F(NegativeEvent, DISABLED_WaitEventThenSet) {
     m_errorMonitor->VerifyFound();
 }
 
-TEST_F(NegativeEvent, EventOwnershipTransferUseAllStagesNoFeature) {
+TEST_F(NegativeEvent, OwnershipTransferUseAllStagesNoFeature) {
     SetTargetApiVersion(VK_API_VERSION_1_3);
     AddRequiredFeature(vkt::Feature::synchronization2);
     // Enable extension but do not enable maintenance8 feature
@@ -780,25 +780,6 @@ TEST_F(NegativeEvent, EventOwnershipTransferUseAllStagesNoFeature) {
     vkt::Event event(*m_device);
     VkDependencyInfo dependency_info = vku::InitStructHelper();
     dependency_info.dependencyFlags = VK_DEPENDENCY_QUEUE_FAMILY_OWNERSHIP_TRANSFER_USE_ALL_STAGES_BIT_KHR;
-
-    m_command_buffer.Begin();
-    m_errorMonitor->SetDesiredError("VUID-vkCmdWaitEvents2-dependencyFlags-10394");
-    vk::CmdWaitEvents2(m_command_buffer, 1, &event.handle(), &dependency_info);
-    m_errorMonitor->VerifyFound();
-    m_command_buffer.End();
-}
-
-TEST_F(NegativeEvent, EventOwnershipTransferUseAllStages) {
-    SetTargetApiVersion(VK_API_VERSION_1_3);
-    AddRequiredFeature(vkt::Feature::synchronization2);
-    AddRequiredExtensions(VK_KHR_MAINTENANCE_8_EXTENSION_NAME);
-    AddRequiredFeature(vkt::Feature::maintenance8);
-    RETURN_IF_SKIP(Init());
-
-    vkt::Event event(*m_device);
-    VkDependencyInfo dependency_info = vku::InitStructHelper();
-    dependency_info.dependencyFlags =
-        VK_DEPENDENCY_QUEUE_FAMILY_OWNERSHIP_TRANSFER_USE_ALL_STAGES_BIT_KHR | VK_DEPENDENCY_VIEW_LOCAL_BIT;
 
     m_command_buffer.Begin();
     m_errorMonitor->SetDesiredError("VUID-vkCmdWaitEvents2-maintenance8-10205");
