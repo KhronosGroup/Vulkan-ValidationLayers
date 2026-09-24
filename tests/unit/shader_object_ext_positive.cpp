ef7f68c78b12f8bf432bd2d7a9e7de08e84db6a9
ziga-lunarg <ziga@lunarg.com>
2026-09-24 16:08:55 +0300

    tests: Fix local_size

--- tests/unit/shader_object_ext_positive.cpp
+++ tests/unit/shader_object_ext_positive.cpp
@@ -405,7 +405,7 @@ TEST_F(PositiveShaderObjectEXT, ComputeShader) {
 
     const char comp_src[] = R"glsl(
         #version 450
-        layout(local_size_x=16, local_size_x=1, local_size_x=1) in;
+        layout(local_size_x=16, local_size_y=1, local_size_z=1) in;
         layout(binding = 0) buffer Output {
             uint values[16];
         } buffer_out;
@@ -1775,7 +1775,7 @@ TEST_F(PositiveShaderObjectEXT, DescriptorHeapStorageBuffer) {
 
     const char comp_src[] = R"glsl(
         #version 450
-        layout(local_size_x=16, local_size_x=1, local_size_x=1) in;
+        layout(local_size_x=16, local_size_y=1, local_size_z=1) in;
         layout(binding = 0) buffer Output {
             uint values[16];
         } buffer_out;
